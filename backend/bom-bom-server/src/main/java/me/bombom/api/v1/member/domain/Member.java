52fb437ef66844f4b266dab688aa28b3e7e50908
ChoiDongjun <adong0808@naver.com>
2025-07-10 22:37:25 +0900

    feat: 도메인 엔티티 생성

---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/member/domain/Member.java
@@ -0,0 +1,44 @@
+package me.bombom.api.v1.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.bombom.api.v1.common.BaseEntity;
+import me.bombom.api.v1.member.enums.Gender;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    @Column(length = 512)
+    private String profileUrl;
+
+    private LocalDateTime birthDate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Long roleId;
+}

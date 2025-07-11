d66295b96e2f276e95eb8317993c3c6b29dc862f
guesung <gueit214@naver.com>
2025-07-11 16:04:24 +0900

    chore: 불필요한 파일 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,5 @@
-import React from "react";
-import { emojis } from "./emoji";
-import logo from "./assets/logo.svg";
-import "./style.css";
-
 const App = () => {
-  const [selectedEmoji, setSelectedEmoji] = React.useState(emojis[0]);
-
-  // main.ts에 있던 showRandomEmoji 함수를 가져오되, React 상태에 저장하도록 수정했어요
-  const showRandomEmoji = () => {
-    const randomIndex = Math.floor(Math.random() * emojis.length);
-    setSelectedEmoji(emojis[randomIndex]);
-  };
-
-  return (
-    <div className="container">
-      <img src={logo} alt="Logo" className="logo"></img>
-      <h1>Emoji of the Day</h1>
-      <div className="emoji-container">
-        <div className="emoji">{selectedEmoji.icon}</div>
-        <div className="emoji-name">{selectedEmoji.name}</div>
-        {/* 다른 이모지 보기 기능을 추가했어요 */}
-        <button onClick={showRandomEmoji}>See other emoji</button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;

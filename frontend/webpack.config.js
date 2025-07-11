b41f2d18d6bdaf35f6b9fa50ae151b497855149c
guesung <gueit214@naver.com>
2025-07-11 15:52:56 +0900

    feat: webpack devServer 설정

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -27,10 +27,10 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.css$/, // .css 파일을 처리해요
+        test: /\.css$/, // .css 파일을 처리
         use: [
-          "style-loader", // CSS를 <style> 태그로 주입해요
-          "css-loader", // CSS를 JavaScript 모듈로 변환해요
+          "style-loader", // CSS를 <style> 태그로 주입
+          "css-loader", // CSS를 JavaScript 모듈로 변환
         ],
       },
       {
@@ -56,4 +56,16 @@ module.exports = {
       inject: true, // <script> 태그 자동 삽입
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"), // 빌드된 파일을 이 경로에서 서빙
+    },
+    port: 3000, // localhost:3000에서 실행
+    open: true, // 서버 실행 시 브라우저 자동 열기
+    hot: true, // HMR 사용
+    historyApiFallback: true, // SPA 라우팅 지원
+    client: {
+      overlay: true, // 에러 발생 시 브라우저에 띄워줘요
+    },
+  },
 };

80db1d741edd991953bf6cfb1f87736d186e21e9
guesung <gueit214@naver.com>
2025-07-13 15:15:42 +0900

    chore: CJS 파일을 .cjs확장자로 수정

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,5 +1,3 @@
-const { rules } = require('eslint-config-prettier');
-
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
--- frontend/webpack.config.cjs
+++ frontend/webpack.config.cjs
@@ -1,12 +1,12 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/main.tsx",
+  mode: 'development',
+  entry: './src/main.tsx',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -14,12 +14,12 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         use: [
           {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
               presets: [
-                "@babel/preset-env",
-                ["@babel/preset-react", { runtime: "automatic" }],
-                "@babel/preset-typescript",
+                '@babel/preset-env',
+                ['@babel/preset-react', { runtime: 'automatic' }],
+                '@babel/preset-typescript',
               ],
             },
           },
@@ -29,36 +29,36 @@ module.exports = {
       {
         test: /\.css$/, // .css 파일을 처리
         use: [
-          "style-loader", // CSS를 <style> 태그로 주입
-          "css-loader", // CSS를 JavaScript 모듈로 변환
+          'style-loader', // CSS를 <style> 태그로 주입
+          'css-loader', // CSS를 JavaScript 모듈로 변환
         ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset",
+        type: 'asset',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "assets/[name][ext]",
+          filename: 'assets/[name][ext]',
         },
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html", // 템플릿 HTML
-      filename: "index.html", // 출력될 HTML 파일 이름
+      template: './index.html', // 템플릿 HTML
+      filename: 'index.html', // 출력될 HTML 파일 이름
       inject: true, // <script> 태그 자동 삽입
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"), // 빌드된 파일을 이 경로에서 서빙
+      directory: path.join(__dirname, 'dist'), // 빌드된 파일을 이 경로에서 서빙
     },
     port: 3000, // localhost:3000에서 실행
     open: true, // 서버 실행 시 브라우저 자동 열기

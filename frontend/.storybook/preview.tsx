abe9bc638bef84261774fd2672fdfe709598b9d7
Jeongeun Lee <leeju1009@gmail.com>
2025-07-17 00:02:05 +0900

    feat: 진행률 바 UI 스토리북 제작

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -1,4 +1,7 @@
 import type { Preview } from '@storybook/react-webpack5';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from '../src/styles/theme';
+import React from 'react';
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +13,13 @@ const preview: Preview = {
     },
   },
   tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
 };
 
 export default preview;
---
+++ frontend/src/stories/ProgressBar.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+import ProgressBar from '../components/ProgressBar';
+
+const meta = {
+  title: 'ProgressBar',
+  component: ProgressBar,
+  parameters: {
+    layout: 'padded',
+  },
+} satisfies Meta<typeof ProgressBar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    progressRate: 80,
+  },
+};
+
+export const Zero: Story = {
+  args: {
+    progressRate: 0,
+  },
+};
+
+export const Fulfill: Story = {
+  args: {
+    progressRate: 100,
+  },
+};

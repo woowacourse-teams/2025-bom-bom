6b7ec463139fd3ccc93a3112f8c02e3d6a2e79e1
Jeongeun Lee <leeju1009@gmail.com>
2025-07-17 04:10:43 +0900

    feat: 읽기 현황 스토리북 제작

---
+++ frontend/src/stories/ReadingStatusCard.stories.ts
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+import ReadingStatusCard from '../pages/today/components/ReadingStatusCard';
+
+const meta = {
+  title: 'ReadingStatusCard',
+  component: ReadingStatusCard,
+} satisfies Meta<typeof ReadingStatusCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    streakReadDay: 7,
+    todayReadCount: 3,
+    totalCount: 4,
+    weeklyReadCount: 4,
+    goalCount: 5,
+  },
+};
+
+export const ReachedDailyGoal: Story = {
+  args: {
+    streakReadDay: 7,
+    todayReadCount: 4,
+    totalCount: 4,
+    weeklyReadCount: 4,
+    goalCount: 5,
+  },
+};
+
+export const ReachedWeeklyGoal: Story = {
+  args: {
+    streakReadDay: 7,
+    todayReadCount: 3,
+    totalCount: 4,
+    weeklyReadCount: 5,
+    goalCount: 5,
+  },
+};
+
+export const ReachedWholeGoal: Story = {
+  args: {
+    streakReadDay: 7,
+    todayReadCount: 4,
+    totalCount: 4,
+    weeklyReadCount: 5,
+    goalCount: 5,
+  },
+};

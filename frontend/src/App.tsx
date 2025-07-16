6a94134efd0493d649ed442465f24cfa01a20b14
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-07-16 11:24:21 +0900

    chore: stylelint 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import styled from '@emotion/styled';
+
 const App = () => {
-  return <div></div>;
+  return <Container></Container>;
 };
+
+const Container = styled.div`
+  position: relative;
+  z-index: 0;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin: 0;
+  padding: 20px;
+
+  background-color: #f0f0f0;
+  font-family: Arial, sans-serif;
+`;
 
 export default App;

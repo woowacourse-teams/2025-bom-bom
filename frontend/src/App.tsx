e738566471668312caeff76c4f23373963ab286c
guesung <gueit214@naver.com>
2025-07-16 22:03:04 +0900

    chore: Container 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,18 @@
 import styled from '@emotion/styled';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Container>Hello World</Container>
+      <Container>
+        <Header />
+      </Container>
     </ThemeProvider>
   );
 };
 
-const Container = styled.div`
-  position: relative;
-  z-index: 0;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  ${({ theme }) => theme.fonts.heading1};
-
-  margin: 0;
-  padding: 20px;
-
-  background-color: #f0f0f0;
-  font-family: Arial, sans-serif;
-`;
+const Container = styled.div``;
 
 export default App;

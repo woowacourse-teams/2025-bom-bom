d450486cb652eb4be9574918d0dce349e7cf0647
Kim Eunsu <88280787+rladmstn@users.noreply.github.com>
2025-07-17 11:39:19 +0900

    [BE][BOM-17] feat: 연속 읽기 & 오늘의 진행 현황 & 주간 목표 조회 API 구현 (#14)
    * feat: 연속 읽기 & 오늘의 진행 현황 & 주간 목표 조회 API 구현
    
    * test: 연속 읽기 & 오늘의 진행 현황 & 주간 목표 조회 API 테스트 작성
    
    * fix: 오늘 읽기 현황 entity에 BaseEntity 상속 추가
    
    * style: dto 정적 팩토리 메서드 네이밍 수정

--- backend/bom-bom-server/src/test/java/me/bombom/api/v1/member/service/MemberServiceTest.java
+++ backend/bom-bom-server/src/test/java/me/bombom/api/v1/member/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package me.bombom.api.v1.member.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import me.bombom.api.v1.TestFixture;
 import me.bombom.api.v1.common.exception.CIllegalArgumentException;
@@ -10,9 +11,12 @@ import me.bombom.api.v1.member.domain.Member;
 import me.bombom.api.v1.member.domain.WeeklyReading;
 import me.bombom.api.v1.member.dto.request.UpdateWeeklyCurrentCountRequest;
 import me.bombom.api.v1.member.dto.request.UpdateWeeklyGoalCountRequest;
+import me.bombom.api.v1.member.dto.response.ReadingInformationResponse;
 import me.bombom.api.v1.member.dto.response.WeeklyCurrentCountResponse;
 import me.bombom.api.v1.member.dto.response.WeeklyGoalCountResponse;
+import me.bombom.api.v1.member.repository.ContinueReadingRepository;
 import me.bombom.api.v1.member.repository.MemberRepository;
+import me.bombom.api.v1.member.repository.TodayReadingRepository;
 import me.bombom.api.v1.member.repository.WeeklyReadingRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +35,12 @@ class MemberServiceTest {
 
     @Autowired
     private WeeklyReadingRepository weeklyReadingRepository;
+
+    @Autowired
+    private ContinueReadingRepository continueReadingRepository;
+
+    @Autowired
+    private TodayReadingRepository todayReadingRepository;
 
     @Test
     void 주간_목표를_수정할_수_있다(){
@@ -130,6 +140,71 @@ class MemberServiceTest {
 
         // when & then
         assertThatThrownBy(() -> memberService.updateWeeklyCurrentCount(request))
+                .isInstanceOf(CIllegalArgumentException.class)
+                .hasFieldOrPropertyWithValue("errorDetail", ErrorDetail.ENTITY_NOT_FOUND);
+    }
+
+    @Test
+    void 읽기_현황_종합_정보를_조회할_수_있다() {
+        // given
+        Member savedMember = memberRepository.save(TestFixture.normalMemberFixture());
+        continueReadingRepository.save(TestFixture.continueReadingFixture(savedMember));
+        todayReadingRepository.save(TestFixture.todayReadingFixture(savedMember));
+        weeklyReadingRepository.save(TestFixture.weeklyReadingFixture(savedMember));
+
+        // when
+        ReadingInformationResponse response = memberService.getReadingInformation(savedMember.getId());
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(response.streakReadDay()).isEqualTo(10);
+            softly.assertThat(response.today().readCount()).isEqualTo(1);
+            softly.assertThat(response.today().totalCount()).isEqualTo(3);
+            softly.assertThat(response.weekly().readCount()).isEqualTo(3);
+            softly.assertThat(response.weekly().goalCount()).isEqualTo(5);
+        });
+    }
+
+    @Test
+    void 읽기_현황_종합_정보_조회에서_회원_정보가_존재하지_않을_경우_예외가_발생한다() {
+        // when & then
+        assertThatThrownBy(() -> memberService.getReadingInformation(1L))
+                .isInstanceOf(CIllegalArgumentException.class)
+                .hasFieldOrPropertyWithValue("errorDetail", ErrorDetail.ENTITY_NOT_FOUND);
+    }
+
+    @Test
+    void 읽기_현황_종합_정보_조회에서_연속_읽기가_존재하지_않을_경우_예외가_발생한다() {
+        // given
+        Member savedMember = memberRepository.save(TestFixture.normalMemberFixture());
+
+        // when & then
+        assertThatThrownBy(() -> memberService.getReadingInformation(savedMember.getId()))
+                .isInstanceOf(CIllegalArgumentException.class)
+                .hasFieldOrPropertyWithValue("errorDetail", ErrorDetail.ENTITY_NOT_FOUND);
+    }
+
+    @Test
+    void 읽기_현황_종합_정보_조회에서_일간_읽기_정보가_존재하지_않을_경우_예외가_발생한다() {
+        // given
+        Member savedMember = memberRepository.save(TestFixture.normalMemberFixture());
+        continueReadingRepository.save(TestFixture.continueReadingFixture(savedMember));
+
+        // when & then
+        assertThatThrownBy(() -> memberService.getReadingInformation(savedMember.getId()))
+                .isInstanceOf(CIllegalArgumentException.class)
+                .hasFieldOrPropertyWithValue("errorDetail", ErrorDetail.ENTITY_NOT_FOUND);
+    }
+
+    @Test
+    void 읽기_현황_종합_정보_조회에서_주간_목표_정보가_존재하지_않을_경우_예외가_발생한다() {
+        // given
+        Member savedMember = memberRepository.save(TestFixture.normalMemberFixture());
+        continueReadingRepository.save(TestFixture.continueReadingFixture(savedMember));
+        todayReadingRepository.save(TestFixture.todayReadingFixture(savedMember));
+
+        // when & then
+        assertThatThrownBy(() -> memberService.getReadingInformation(savedMember.getId()))
                 .isInstanceOf(CIllegalArgumentException.class)
                 .hasFieldOrPropertyWithValue("errorDetail", ErrorDetail.ENTITY_NOT_FOUND);
     }

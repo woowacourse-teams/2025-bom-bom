c4b15b5dea4954a8573a33ede279c78fe43fd7cf
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-07-17 16:34:55 +0900

    feat: Chip 컴포넌트 제작

---
+++ frontend/src/components/Chip/Chip.tsx
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+
+interface ChipProps {
+  text: string;
+  selected?: boolean;
+  onSelect?: () => void;
+}
+
+function Chip({ text, selected = false, onSelect }: ChipProps) {
+  return (
+    <Container selected={selected} onClick={onSelect}>
+      {text}
+    </Container>
+  );
+}
+
+export default Chip;
+
+const Container = styled.button<{
+  selected: boolean;
+}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: fit-content;
+  padding: 8px 16px;
+
+  ${({ theme }) => theme.fonts.caption};
+
+  color: ${({ theme, selected }) =>
+    selected ? theme.colors.white : theme.colors.textPrimary};
+  background-color: ${({ theme, selected }) =>
+    selected ? theme.colors.black : theme.colors.dividers};
+  border-radius: 16px;
+`;

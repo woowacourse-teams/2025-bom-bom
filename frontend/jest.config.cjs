8a978fe2dee3445f426c3eca6d19959f482861a3
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-07-17 14:33:01 +0900

    fix: jest config 파일 js로 변경

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -3,9 +3,8 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -198,4 +197,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+module.exports = config;

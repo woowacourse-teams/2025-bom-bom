5ee2bba3fa1c01614d9cb87fe4554076bc9f1ffc
Kuesung Park <gueit214@gachon.ac.kr>
2025-07-17 16:35:17 +0900

    [FE][BOM-42] feat: header component 제작 (#17)
    * feat: 헤더 퍼블리싱
    
    * feat: 헤더 스토리북
    
    * feat: activeTab에 따라 분기 처리
    
    * refactor: `theme`로 교체
    
    * chore: Container 제거
    
    * fix: 기존 헤더 제거
    
    * chore: 공통 컴포넌트 그룹을 common으로 수정
    
    * chore: `.cjs` 파일 확장자로 수정
    
    * chore: decorator 추가
    
    * chore: App 파일 변경사항 제거
    
    * feat: 복사 기능
    
    * refactor: 역할에 맞는 스타일명
    
    * refactor: 부모에서 폰트 지정
    
    * chore: 색상 동적 조정
    
    * chore: `components` depth 추가
    
    * chore: 줄바꿈으로 각 책임 분리

---
+++ frontend/src/components/Header/Header.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+import Header from './Header';
+
+const meta: Meta<typeof Header> = {
+  title: 'components/common/Header',
+  component: Header,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Header>;
+
+export const Default: Story = {
+  render: () => <Header activeNav="home" />,
+};
+
+export const Recommend: Story = {
+  render: () => <Header activeNav="recommend" />,
+};
---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,177 @@
+import styled from '@emotion/styled';
+import compassIcon from '../../../public/assets/compass.svg';
+import copyIcon from '../../../public/assets/copy.svg';
+import CompassIcon from '../icons/CompassIcon';
+import HomeIcon from '../icons/HomeIcon';
+
+type NavType = 'home' | 'recommend';
+
+interface HeaderProps {
+  activeNav: NavType;
+}
+
+export default function Header({ activeNav = 'home' }: HeaderProps) {
+  const handleCopyEmail = () => {
+    navigator.clipboard.writeText('test@bombom.news');
+  };
+
+  return (
+    <HeaderContainer>
+      <HeaderInner>
+        <LogoWrapper>
+          <LogoBox>
+            <HomeIcon />
+          </LogoBox>
+          <TitleBox>
+            <Title>봄봄</Title>
+            <SubTitle>당신의 하루에 찾아오는 작은 설렘</SubTitle>
+          </TitleBox>
+        </LogoWrapper>
+
+        <Nav>
+          <NavButton active={activeNav === 'home'}>
+            <HomeIcon color={activeNav === 'home' ? 'white' : 'black'} />
+            <p>오늘의 뉴스레터</p>
+          </NavButton>
+          <NavButton active={activeNav === 'recommend'}>
+            <CompassIcon
+              color={activeNav === 'recommend' ? 'white' : 'black'}
+            />
+            <p>뉴스레터 추천</p>
+          </NavButton>
+        </Nav>
+
+        <ProfileBox>
+          <ProfileImg src={compassIcon} alt="profile" />
+          <ProfileInfo>
+            <ProfileName>김봄봄</ProfileName>
+            <ProfileEmail onClick={handleCopyEmail}>
+              <EmailText>test@bombom.news</EmailText>
+              <img src={copyIcon} alt="copy" width={16} height={16} />
+            </ProfileEmail>
+          </ProfileInfo>
+        </ProfileBox>
+      </HeaderInner>
+    </HeaderContainer>
+  );
+}
+
+const HeaderContainer = styled.header`
+  background: ${({ theme }) => theme.colors.white};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow:
+    0px 10px 15px -3px rgba(0, 0, 0, 0.1),
+    0px 4px 6px -4px rgba(0, 0, 0, 0.1);
+  border-radius: 0 0 8px 8px;
+  padding: 8px 16px;
+  height: 64px;
+`;
+
+const HeaderInner = styled.div`
+  width: 100%;
+  max-width: 1280px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LogoBox = styled.div`
+  width: 40px;
+  height: 40px;
+  background: ${({ theme }) => theme.colors.primary};
+  border-radius: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow:
+    0px 10px 15px -3px rgba(0, 0, 0, 0.1),
+    0px 4px 6px -4px rgba(0, 0, 0, 0.1);
+  margin-right: 12px;
+`;
+
+const TitleBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Title = styled.div`
+  ${({ theme }) => theme.fonts.heading4};
+  background: linear-gradient(
+    144.324deg,
+    #4b5563 0%,
+    #ff9966 50%,
+    #ffd700 100%
+  );
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const SubTitle = styled.div`
+  ${({ theme }) => theme.fonts.body2};
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  background: ${({ theme }) => theme.colors.white};
+  border-radius: 14px;
+  padding: 4px;
+`;
+
+const NavButton = styled.button<{ active?: boolean }>`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  padding: 10px 12px;
+  border-radius: 12px;
+  background: ${({ active, theme }) =>
+    active ? theme.colors.primary : 'transparent'};
+  color: ${({ active, theme }) =>
+    active ? theme.colors.white : theme.colors.black};
+  ${({ theme }) => theme.fonts.body2};
+`;
+
+const ProfileBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 8px 12px;
+  border-radius: 12px;
+  background: ${({ theme }) => theme.colors.white};
+`;
+
+const ProfileImg = styled.img`
+  width: 28px;
+  height: 28px;
+  border-radius: 50%;
+`;
+
+const ProfileInfo = styled.div`
+  ${({ theme }) => theme.fonts.caption};
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const ProfileName = styled.div``;
+
+const ProfileEmail = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  gap: 4px;
+`;
+
+const EmailText = styled.div`
+  ${({ theme }) => theme.fonts.caption};
+`;

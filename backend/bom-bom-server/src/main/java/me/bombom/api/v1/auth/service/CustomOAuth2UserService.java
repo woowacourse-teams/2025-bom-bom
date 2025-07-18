e1ad27a08834bb3fe68aac1c3e676ad94bf7c4ee
ChoiDongjun <adong0808@naver.com>
2025-07-18 19:34:24 +0900

    fix: Security 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/auth/service/CustomOAuth2UserService.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/auth/service/CustomOAuth2UserService.java
@@ -1,29 +1,29 @@
-//package me.bombom.api.v1.auth.service;
-//
-//import java.util.Optional;
-//import lombok.RequiredArgsConstructor;
-//import me.bombom.api.v1.auth.dto.CustomOAuth2User;
-//import me.bombom.api.v1.member.domain.Member;
-//import me.bombom.api.v1.member.repository.MemberRepository;
-//import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-//import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-//import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class CustomOAuth2UserService extends DefaultOAuth2UserService {
-//
-//    private final MemberRepository memberRepository;
-//
-//    @Override
-//    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-//        OAuth2User oAuth2User = super.loadUser(userRequest);
-//
-//        String provider = userRequest.getClientRegistration().getRegistrationId();
-//        String providerId = oAuth2User.getAttribute("sub");
-//        Optional<Member> member = memberRepository.findByProviderAndProviderId(provider, providerId);
-//        return new CustomOAuth2User(oAuth2User.getAttributes(), member.orElse(null));
-//    }
-//}
+package me.bombom.api.v1.auth.service;
+
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import me.bombom.api.v1.auth.dto.CustomOAuth2User;
+import me.bombom.api.v1.member.domain.Member;
+import me.bombom.api.v1.member.repository.MemberRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        String providerId = oAuth2User.getAttribute("sub");
+        Optional<Member> member = memberRepository.findByProviderAndProviderId(provider, providerId);
+        return new CustomOAuth2User(oAuth2User.getAttributes(), member.orElse(null));
+    }
+}

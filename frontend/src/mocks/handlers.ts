74b015df88b8ca6259be17d99761ca5b958ef08c
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-07-19 02:46:30 +0900

    feat: msw handler 설정

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,3 +1,37 @@
+import { http, HttpResponse } from 'msw';
+import { ARTICLES } from './data/mock-articles';
+
+const baseURL = process.env.API_BASE_URL;
+
 export const handlers = [
-  // Define your request handlers here
+  http.get(`${baseURL}/articles`, () => {
+    return HttpResponse.json({
+      totalPages: 0,
+      totalElements: 0,
+      first: true,
+      last: true,
+      size: 0,
+      content: ARTICLES,
+      number: 0,
+      sort: {
+        empty: true,
+        unsorted: true,
+        sorted: true,
+      },
+      pageable: {
+        offset: 0,
+        sort: {
+          empty: true,
+          unsorted: true,
+          sorted: true,
+        },
+        unpaged: true,
+        paged: true,
+        pageNumber: 0,
+        pageSize: 0,
+      },
+      numberOfElements: 0,
+      empty: true,
+    });
+  }),
 ];

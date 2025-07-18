894ef9aad135c7639fba9b1adebf2c721c6e78b6
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-07-18 11:44:58 +0900

    [FE][BOM-43] feat: ImageInfoCard Component 제작 (#24)
    * feat: ImageInfoCard 컴포넌트 제작
    
    * refactor: description 옵셔널로 변경
    
    * refactor: image alt 속성 추가
    
    * refactor: description props 필수로 변경

---
+++ frontend/src/components/ImageInfoCard/ImageInfoCard.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+import ImageInfoCard from './ImageInfoCard';
+
+const meta: Meta<typeof ImageInfoCard> = {
+  title: 'components/common/ImageInfoCard',
+  component: ImageInfoCard,
+};
+export default meta;
+
+type Story = StoryObj<typeof ImageInfoCard>;
+
+export const Default: Story = {
+  args: {
+    imageUrl:
+      'https://img.freepik.com/free-vector/illustration-south-korea-flag_53876-27132.jpg',
+    title: '타이틀',
+    description:
+      '시간이 없어도 세상을 궁금하니까! 세상 돌아가는 소식을 쉽고 재밌게 알아보세요',
+  },
+};

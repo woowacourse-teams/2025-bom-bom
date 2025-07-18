00d9e6a3b19f638a0391e6fde314d3362d4af0b5
ryan-dia <cjfdnjs158@naver.com>
2025-07-18 21:48:46 +0900

    feat: member 도메인 추가

---
+++ backend/mail-server/src/main/java/news/bombomemail/member/Member.java
@@ -0,0 +1,63 @@
+package news.bombomemail.member;
+
+import emailtest.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(length = 512)
+    private String profileImageUrl;
+
+    private LocalDateTime birthDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(nullable = false, columnDefinition = "BIGINT DEFAULT 0")
+    private Long roleId = 0L;
+
+    @Builder
+    public Member(
+            Long id,
+            @NonNull String email,
+            @NonNull String nickname,
+            String profileImageUrl,
+            LocalDateTime birthDate,
+            @NonNull Gender gender,
+            @NonNull Long roleId
+    ) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.roleId = roleId;
+    }
+}

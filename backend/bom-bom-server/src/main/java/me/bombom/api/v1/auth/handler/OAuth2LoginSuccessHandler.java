4dbcc59f969ff8486f814a08073bfb193cf021dc
ChoiDongjun <adong0808@naver.com>
2025-07-18 17:07:43 +0900

    security 주석 처리

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/auth/handler/OAuth2LoginSuccessHandler.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/auth/handler/OAuth2LoginSuccessHandler.java
@@ -1,31 +1,31 @@
-package me.bombom.api.v1.auth.handler;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import me.bombom.api.v1.auth.dto.CustomOAuth2User;
-import me.bombom.api.v1.member.domain.Member;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
-
-    @Override
-    public void onAuthenticationSuccess(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Authentication authentication
-    ) throws IOException, ServletException {
-        CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
-        Member member = (Member) oAuth2User.getMember();
-
-        if (member == null) {
-            // 추가 정보 입력
-            response.sendRedirect("/api/v1/auth/signup");
-        } else {
-            // 로그인 성공
-            response.sendRedirect("/");
-        }
-    }
-}
+//package me.bombom.api.v1.auth.handler;
+//
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import me.bombom.api.v1.auth.dto.CustomOAuth2User;
+//import me.bombom.api.v1.member.domain.Member;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+//
+//public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+//
+//    @Override
+//    public void onAuthenticationSuccess(
+//            HttpServletRequest request,
+//            HttpServletResponse response,
+//            Authentication authentication
+//    ) throws IOException, ServletException {
+//        CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+//        Member member = (Member) oAuth2User.getMember();
+//
+//        if (member == null) {
+//            // 추가 정보 입력
+//            response.sendRedirect("/api/v1/auth/signup");
+//        } else {
+//            // 로그인 성공
+//            response.sendRedirect("/");
+//        }
+//    }
+//}

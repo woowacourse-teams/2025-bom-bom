68fdaca7200d4b8a2f46ce77bc66a789fffb5b97
ChoiDongjun <adong0808@naver.com>
2025-07-18 21:18:47 +0900

    feat: 모든 요청에 로깅 추가

---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/logging/LoggingInterceptor.java
@@ -0,0 +1,25 @@
+package me.bombom.api.v1.logging;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        log.info("Request: [{}] {}", request.getMethod(), request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                Exception ex) {
+        log.info("Response [{}] {} completed with status {}", request.getMethod(), request.getRequestURI(),
+                response.getStatus());
+    }
+}

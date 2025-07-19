92aee7e651949d655c503309bd9865f7efd1fd6b
geonwoo <kysub99@naver.com>
2025-07-19 20:36:23 +0900

    feat: 로그아웃 기능 구현

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/auth/controller/AuthController.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/auth/controller/AuthController.java
@@ -1,11 +1,13 @@
 package me.bombom.api.v1.auth.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import me.bombom.api.v1.member.service.MemberService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,12 @@ public class AuthController {
     @GetMapping("/login/{provider}")
     public void login(@PathVariable("provider") String provider, HttpServletResponse response) throws IOException {
         response.sendRedirect("/oauth2/authorization/" + provider);
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request) {
+        if(request.getSession(false) != null) {
+            request.getSession(false).invalidate();
+        }
     }
 }

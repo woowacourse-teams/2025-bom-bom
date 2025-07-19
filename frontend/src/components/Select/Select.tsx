c903e3425dcf55dc28df7da1cd580df08f6dc0a8
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-07-19 16:59:38 +0900

    feat: Select 컴포넌트 제작

---
+++ frontend/src/components/Select/Select.tsx
@@ -0,0 +1,148 @@
+import styled from '@emotion/styled';
+import { ComponentProps, useState } from 'react';
+import { useClickOutsideRef } from '../../hooks/useClickOutsideRef';
+import ChevronIcon from '../icons/ChevronIcon';
+
+interface SelectProps<T extends { label: string; value: string }>
+  extends ComponentProps<'div'> {
+  options: T[];
+  selectedValue: string | null;
+  onSelectOption: (optionValue: string) => void;
+  width?: number;
+  placeholder?: string;
+}
+
+function Select<T extends { label: string; value: string }>({
+  options,
+  selectedValue,
+  onSelectOption,
+  width = 132,
+  placeholder,
+  ...props
+}: SelectProps<T>) {
+  const [open, setOpen] = useState(false);
+  const SelectRef = useClickOutsideRef<HTMLDivElement>(() => setOpen(false));
+
+  const toggle = () => setOpen((prev) => !prev);
+
+  const selectedLabel = options.find(
+    (option) => option.value === selectedValue,
+  )?.label;
+
+  const onOptionSelected = (value: string) => {
+    setOpen(false);
+    onSelectOption(value);
+  };
+
+  return (
+    <Container ref={SelectRef} {...props} width={width}>
+      <SelectToggle onClick={toggle}>
+        <SelectText selected={selectedValue !== null}>
+          {selectedLabel || (placeholder ?? '')}
+        </SelectText>
+        <ChevronIcon width={16} direction={open ? 'up' : 'down'} />
+      </SelectToggle>
+      <SelectMenu open={open}>
+        <SelectMenuWrapper>
+          {options.map((option) => (
+            <SelectMenuItem
+              key={option.value}
+              data-value={option.value}
+              onClick={() => onOptionSelected(option.value)}
+            >
+              {option.label}
+            </SelectMenuItem>
+          ))}
+        </SelectMenuWrapper>
+      </SelectMenu>
+    </Container>
+  );
+}
+
+const Container = styled.div<{ width: number }>`
+  position: relative;
+
+  width: ${({ width }) => `${width}px`};
+  height: 36px;
+`;
+
+const SelectToggle = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  width: 100%;
+  padding: 8px 12px;
+  border: 1px solid ${({ theme }) => theme.colors.stroke};
+  border-radius: 6px;
+
+  background-color: white;
+
+  box-sizing: border-box;
+  cursor: pointer;
+  user-select: none;
+`;
+
+const SelectText = styled.p<{ selected: boolean }>`
+  overflow: hidden;
+
+  flex: 1;
+
+  color: ${({ theme, selected }) =>
+    selected ? theme.colors.textPrimary : theme.colors.textTertiary};
+  font: ${({ theme }) => theme.fonts.caption};
+  white-space: nowrap;
+
+  text-overflow: ellipsis;
+`;
+
+const SelectMenu = styled.div<{ open: boolean }>`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  z-index: 10;
+
+  display: ${({ open }) => (open ? 'block' : 'none')};
+
+  width: 100%;
+  margin-top: 6px;
+  border: 1px solid ${({ theme }) => theme.colors.stroke};
+  border-radius: 6px;
+
+  /* box-shadow: 0 4px 6px 0 rgb(0 0 0 / 9%); */
+
+  background: white;
+
+  box-sizing: border-box;
+  list-style: none;
+`;
+
+const SelectMenuWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  align-self: stretch;
+
+  width: 100%;
+  padding: 0 4px;
+  border-radius: 6px;
+
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const SelectMenuItem = styled.li`
+  width: 100%;
+  padding: 6px 8px;
+  border-radius: 6px;
+
+  font-weight: 400;
+  font-size: 12px;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.primaryLight};
+  }
+`;
+
+export default Select;

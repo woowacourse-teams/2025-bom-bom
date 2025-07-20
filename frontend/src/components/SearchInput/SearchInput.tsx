7d58c1193f4de5afff8f663ef6c87763bd1eeb2c
Kuesung Park <gueit214@gachon.ac.kr>
2025-07-20 16:31:03 +0900

    [FE][BOM-75] feat: SearchInput component 제작 (#32)
    * feat: `SearchInput` 컴포넌트 구현
    
    * refactor: theme 활용 및 불필요한 속성 제거
    
    * chore: 한글로 수정
    
    * chore: 접근성 - `aria-label`을 props로 받기
    
    * chore: 중복 표현 제거
    
    * refactor: theme 활용
    
    * chore: 중복 속성 제거
    
    * chore: `forwardRef` 제거
    
    * refactor: 컴포넌트 컨벤션 통일
    
    * refactor: `theme` 활용 및 4단위로 맞추기
    
    * design: `textTertiary'로 색 변경
    
    * refactor: `ComponentProps`로 교체
    
    * chore: 돋보기 아이콘 추가
    
    * chore: 루트 태그를 `form` 태그로 수정
    
    * chore: search type으로 수정

---
+++ frontend/src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,92 @@
+import styled from '@emotion/styled';
+import { ChangeEvent, ComponentProps } from 'react';
+import ReadingGlassesIcon from '../icons/ReadingGlassesIcon';
+import { theme } from '../../styles/theme';
+
+interface SearchInputProps extends Omit<ComponentProps<'input'>, 'onChange'> {
+  value?: string;
+  onChange?: (value: string) => void;
+}
+
+export default function SearchInput({
+  value,
+  onChange,
+  ...props
+}: SearchInputProps) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange?.(e.target.value);
+  };
+
+  return (
+    <Container>
+      <ReadingGlassesIconWrapper>
+        <ReadingGlassesIcon color={theme.colors.textTertiary} />
+      </ReadingGlassesIconWrapper>
+      <StyledInput
+        type="search"
+        value={value}
+        onChange={handleChange}
+        aria-label={props['aria-label'] || '검색'}
+        {...props}
+      />
+    </Container>
+  );
+}
+
+const Container = styled.form`
+  position: relative;
+
+  width: 100%;
+  height: 42px;
+`;
+
+const ReadingGlassesIconWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 12px;
+
+  transform: translateY(-50%);
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  height: 100%;
+  padding: 12px 42px;
+  outline: none;
+  border: 1px solid ${({ theme }) => theme.colors.stroke};
+  border-radius: 14px;
+  box-shadow:
+    0 1px 3px 0 rgb(0 0 0 / 10%),
+    0 1px 2px -1px rgb(0 0 0 / 10%);
+
+  background-color: ${({ theme }) => theme.colors.white};
+
+  color: ${({ theme }) => theme.colors.textPrimary};
+  font: ${({ theme }) => theme.fonts.body1};
+
+  transition: all 0.2s ease-in-out;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.textTertiary};
+  }
+
+  &:focus {
+    box-shadow:
+      0 1px 3px 0 rgb(0 0 0 / 10%),
+      0 1px 2px -1px rgb(0 0 0 / 10%),
+      0 0 0 2px ${({ theme }) => theme.colors.primary}20;
+    border-color: ${({ theme }) => theme.colors.primary};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.disabledBackground};
+
+    color: ${({ theme }) => theme.colors.textTertiary};
+
+    cursor: not-allowed;
+  }
+
+  &:hover:not(:disabled) {
+    border-color: ${({ theme }) => theme.colors.textTertiary};
+  }
+`;

1a9b94c8b7e741690c756b54e021afa901471e93
guesung <gueit214@naver.com>
2025-07-20 11:55:53 +0900

    chore: title ellipsis지정

--- frontend/src/components/NewsletterItemCard/NewsletterItemCard.tsx
+++ frontend/src/components/NewsletterItemCard/NewsletterItemCard.tsx
@@ -98,6 +98,12 @@ const Title = styled.h3`
 
   color: ${({ theme }) => theme.colors.textPrimary};
   font: ${({ theme }) => theme.fonts.heading5};
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
 `;
 
 const Description = styled.p`

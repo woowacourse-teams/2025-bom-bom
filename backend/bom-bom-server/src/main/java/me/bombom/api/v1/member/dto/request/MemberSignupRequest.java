53d2c5965da5d194a60efe0396213017c7c2ade1
geonwoo <kysub99@naver.com>
2025-07-20 16:19:28 +0900

    test: 회원가입 관련 테스트 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/member/dto/request/MemberSignupRequest.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/member/dto/request/MemberSignupRequest.java
@@ -1,8 +1,10 @@
 package me.bombom.api.v1.member.dto.request;
 
 import java.time.LocalDateTime;
+import lombok.Builder;
 import me.bombom.api.v1.member.enums.Gender;
 
+@Builder
 public record MemberSignupRequest(
         String nickname,
         LocalDateTime birthDate,

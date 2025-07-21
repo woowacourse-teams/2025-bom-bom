b4ac1b57065eddfc1a81f6bf11215d8ea3de202a
ChoiDongjun <adong0808@naver.com>
2025-07-21 17:55:20 +0900

    fix: Security 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/auth/dto/CustomOAuth2User.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/auth/dto/CustomOAuth2User.java
@@ -1,34 +1,34 @@
-//package me.bombom.api.v1.auth.dto;
-//
-//import java.util.Collection;
-//import java.util.Collections;
-//import java.util.Map;
-//import lombok.Getter;
-//import lombok.RequiredArgsConstructor;
-//import me.bombom.api.v1.member.domain.Member;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
-//
-//@Getter
-//@RequiredArgsConstructor
-//public class CustomOAuth2User implements OAuth2User {
-//
-//    private final Map<String, Object> attributes;
-//    private final Member member;
-//
-//    @Override
-//    public Map<String, Object> getAttributes() {
-//        return attributes;
-//    }
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        // TODO: 권한 처리 필요시 구현
-//        return Collections.emptyList();
-//    }
-//
-//    @Override
-//    public String getName() {
-//        return attributes.get("name").toString();
-//    }
-//}
+package me.bombom.api.v1.auth.dto;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import me.bombom.api.v1.member.domain.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomOAuth2User implements OAuth2User {
+
+    private final Map<String, Object> attributes;
+    private final Member member;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO: 권한 처리 필요시 구현
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getName() {
+        return attributes.get("name").toString();
+    }
+}
--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/config/SecurityConfig.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/config/SecurityConfig.java
@@ -1,55 +1,55 @@
-//package me.bombom.api.v1.common.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import me.bombom.api.v1.auth.handler.OAuth2LoginSuccessHandler;
-//import me.bombom.api.v1.auth.service.CustomOAuth2UserService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//public class SecurityConfig {
-//
-//    private final CustomOAuth2UserService customOAuth2UserService;
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                // 모든 설정 열어둠
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
-//                .cors(configurer -> configurer.configurationSource(corsConfigurationSource()))
-//                .formLogin(AbstractHttpConfigurer::disable)
-//                .httpBasic(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(auth -> auth.anyRequest()
-//                        .permitAll())
-//                .oauth2Login(oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo
-//                                .userService(customOAuth2UserService))
-//                        .successHandler(new OAuth2LoginSuccessHandler())
-//                );
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.addAllowedOriginPattern("*"); // 모든 origin 허용, 필요에 따라 특정 origin으로 변경 가능
-//        configuration.addAllowedMethod("*");
-//        configuration.addAllowedHeader("*"); // 모든 헤더 허용
-//        configuration.setAllowCredentials(true);
-//        configuration.setMaxAge(3600L);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-//}
+package me.bombom.api.v1.common.config;
+
+import lombok.RequiredArgsConstructor;
+import me.bombom.api.v1.auth.handler.OAuth2LoginSuccessHandler;
+import me.bombom.api.v1.auth.service.CustomOAuth2UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // 모든 설정 열어둠
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
+                .cors(configurer -> configurer.configurationSource(corsConfigurationSource()))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest()
+                        .permitAll())
+                .oauth2Login(oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService))
+                        .successHandler(new OAuth2LoginSuccessHandler())
+                );
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*"); // 모든 origin 허용, 필요에 따라 특정 origin으로 변경 가능
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*"); // 모든 헤더 허용
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}

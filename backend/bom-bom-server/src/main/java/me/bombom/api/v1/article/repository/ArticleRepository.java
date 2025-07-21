44475a5148dee2e79c069966b045557bf4046213
Kim Eunsu <88280787+rladmstn@users.noreply.github.com>
2025-07-21 15:52:53 +0900

    [BE][BOM-87] feat: 카테고리 별 아티클 개수 조회 API 구현 (#38)
    * feat: 카테고리 별 아티클 개수 조회 API 구현
    
    * test: 카테고리 별 아티클 개수 조회 API 테스트 작성
    
    * fix: 카테고리로 분류할 아티클 대상 오류 수정

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/repository/ArticleRepository.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/repository/ArticleRepository.java
@@ -1,7 +1,22 @@
 package me.bombom.api.v1.article.repository;
 
+import java.util.List;
 import me.bombom.api.v1.article.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ArticleRepository extends JpaRepository<Article, Long>, CustomArticleRepository {
+
+    List<Article> findAllByMemberId(Long memberId);
+    int countAllByMemberId(Long memberId);
+
+    @Query("""
+        SELECT count(*) 
+        FROM Article AS a 
+        INNER JOIN Newsletter AS n 
+        ON a.newsletterId = n.id 
+        WHERE n.categoryId = :categoryId 
+            AND a.memberId = :memberId 
+    """)
+    int countAllByCategoryIdAndMemberId(Long categoryId, Long memberId);
 }

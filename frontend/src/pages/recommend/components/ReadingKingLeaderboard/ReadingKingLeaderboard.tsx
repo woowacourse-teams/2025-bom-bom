361025ce9f78f508cd25e3567e24e14cfcf2412f
guesung <gueit214@naver.com>
2025-07-21 14:48:20 +0900

    fix: 타입 이슈 해결

--- frontend/src/pages/recommend/components/ReadingKingLeaderboard/ReadingKingLeaderboard.tsx
+++ frontend/src/pages/recommend/components/ReadingKingLeaderboard/ReadingKingLeaderboard.tsx
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import BookIcon from '../../../../components/icons/BookIcon';
 import AvatarIcon from '../../../../components/icons/AvatarIcon';
 import TopRightArrowIcon from '../../../../components/icons/TopRightArrowIcon';
 
@@ -83,7 +82,6 @@ interface ReadingKingLeaderboardProps {
 const LeaderboardItem = ({
   rank,
   name,
-  avatar,
   readCount,
   increment,
   isCrown,
@@ -140,7 +138,7 @@ export default function ReadingKingLeaderboard({
 
       <LeaderboardList>
         {data.map((item) => (
-          <LeaderboardItem key={item.id} {...item} />
+          <LeaderboardItem key={item.rank} {...item} />
         ))}
       </LeaderboardList>
 

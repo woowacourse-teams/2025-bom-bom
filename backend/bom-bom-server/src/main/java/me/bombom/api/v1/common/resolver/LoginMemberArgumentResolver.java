eb3c8347f3da01dc3ed2b8608afb6a674dfdada9
geonwoo <kysub99@naver.com>
2025-07-22 16:12:32 +0900

    feat: argument resolver 추가

---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/resolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,40 @@
+package me.bombom.api.v1.common.resolver;
+
+import me.bombom.api.v1.auth.dto.CustomOAuth2User;
+import me.bombom.api.v1.common.exception.ErrorDetail;
+import me.bombom.api.v1.common.exception.UnauthorizedException;
+import me.bombom.api.v1.member.domain.Member;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginMember.class)
+                && parameter.getParameterType().equals(Member.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null ||  !authentication.isAuthenticated()) {
+            throw new UnauthorizedException(ErrorDetail.UNAUTHORIZED);
+        }
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof CustomOAuth2User) {
+            return ((CustomOAuth2User) principal).getMember();
+        }
+        throw new  UnauthorizedException(ErrorDetail.INVALID_TOKEN);
+    }
+}

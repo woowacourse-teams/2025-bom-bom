c19d729c31bc2ffb234f5de8443f0a2612afc9e9
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-07-22 16:54:56 +0900

    fix: ArticleCard 스토리북 에러 해결

--- frontend/src/pages/today/components/ArticleCard/ArticleCard.stories.tsx
+++ frontend/src/pages/today/components/ArticleCard/ArticleCard.stories.tsx
@@ -16,7 +16,7 @@ export const Default: Story = {
       title: '폭염에도 전력난 없는 이유는?',
       contentsSummary:
         '자동차 사이버보안 소프트웨어 솔루션 기업인 아우토크립트가 코스닥에 새로 상장해요. 15일 오늘까지 폴더블 스마트폰에 적용되는 초박형 강화유리를 주력사업으로 하는 도우인시스의 코스닥 공모주 청약이 있어요. 15~16일 양일간 자동차 사이버보안 소프트웨어 솔루션 기업인 아우토크립트가 코스닥에 새로 상장해요. 15일 오늘까지 폴더블 스마트폰에 적용되는 초박형 강화유리를 주력사업으로 하는 도우인시스의 코스닥 공모주 청약이 있어요. 15~16일 양일간',
-      arrivedDateTime: new Date('2025.07.01'),
+      arrivedDateTime: '2025.07.01',
       thumbnailUrl:
         'https://img.freepik.com/free-vector/illustration-south-korea-flag_53876-27132.jpg',
       expectedReadTime: 5,
@@ -37,7 +37,7 @@ export const Read: Story = {
       title: '폭염에도 전력난 없는 이유는?',
       contentsSummary:
         '자동차 사이버보안 소프트웨어 솔루션 기업인 아우토크립트가 코스닥에 새로 상장해요. 15일 오늘까지 폴더블 스마트폰에 적용되는 초박형 강화유리를 주력사업으로 하는 도우인시스의 코스닥 공모주 청약이 있어요. 15~16일 양일간 자동차 사이버보안 소프트웨어 솔루션 기업인 아우토크립트가 코스닥에 새로 상장해요. 15일 오늘까지 폴더블 스마트폰에 적용되는 초박형 강화유리를 주력사업으로 하는 도우인시스의 코스닥 공모주 청약이 있어요. 15~16일 양일간',
-      arrivedDateTime: new Date('2025.07.01'),
+      arrivedDateTime: '2025.07.01',
       thumbnailUrl:
         'https://img.freepik.com/free-vector/illustration-south-korea-flag_53876-27132.jpg',
       expectedReadTime: 5,

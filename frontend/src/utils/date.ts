ffb7f9a5422fe8b35f518ce48f0f2eb013edf3d9
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-07-22 21:28:39 +0900

    refactor: formatDateToDotString separator 추가

--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -1,7 +1,7 @@
-export function formatDateToDotString(date: Date) {
+export function formatDateToDotString(date: Date, separator: string = '.') {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
 
-  return `${year}.${month}.${day}`;
+  return year + separator + month + separator + day;
 }

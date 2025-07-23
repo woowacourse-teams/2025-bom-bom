4ca8c168e240b5a7dc4cfc7a094fcbb54814b394
Jeongeun Lee <leeju1009@gmail.com>
2025-07-23 14:19:22 +0900

    refactor: alt를 props 값을 고정으로 사용하도록 수정
    - 이미지 에러에 따라 alt를 변경할 필요가 없다는 결론을 내려 수정

--- frontend/src/components/ImageWithFallback/ImageWithFallback.tsx
+++ frontend/src/components/ImageWithFallback/ImageWithFallback.tsx
@@ -10,13 +10,12 @@ function ImageWithFallback({ src, alt, ...props }: ImageWithFallbackProps) {
   const handleImageError = (event: React.SyntheticEvent<HTMLImageElement>) => {
     const image = event.currentTarget;
     image.src = defaultImage;
-    image.alt = '기본 이미지';
   };
 
   return (
     <img
       src={src.length > 0 ? src : defaultImage}
-      alt={src.length > 0 ? alt : '기본 이미지'}
+      alt={alt}
       onError={handleImageError}
       {...props}
     />

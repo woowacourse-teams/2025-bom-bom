e8e891f2245fe56a168dcc0b6bb5e08ed7ad7dd6
guesung <gueit214@naver.com>
2025-07-24 15:24:55 +0900

    chore: enableMsw 추가

--- frontend/webpack.config.cjs
+++ frontend/webpack.config.cjs
@@ -72,7 +72,7 @@ module.exports = (env, argv) => {
       }),
       new webpack.DefinePlugin({
         'process.env': JSON.stringify(process.env),
-        'process.env.ENABLE_MSW': JSON.stringify(env.MSW),
+        'process.env.ENABLE_MSW': JSON.stringify(env.ENABLE_MSW),
       }),
     ],
     devServer: {

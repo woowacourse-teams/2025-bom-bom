b39fdc8e4117b21b0015186d0aca51192b29c8a6
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-07-24 20:46:48 +0900

    회원가입 테스트용으로 머지합니다. (#87)
    * feat: 회원가입 페이지
    
    * temp
    
    * feat: credentials 헤더 추가
    
    ---------
    
    Co-authored-by: guesung <gueit214@naver.com>

---
+++ frontend/src/apis/auth.ts
@@ -0,0 +1,22 @@
+import { fetcher } from './fetcher';
+
+// interface PostSignupParams {
+//   nickname: string;
+//   birthdate: string;
+//   gender: string;
+// }
+
+export const postSignup = async () => {
+  return await fetcher.post({
+    path: '/auth/signup',
+    body: {
+      nickname: '피터',
+      // birthdate: '2025-07-23',
+      gender: 'MALE',
+      // email: 'test@bombom.news',
+    },
+    headers: {
+      credentials: 'include',
+    },
+  });
+};
---
+++ frontend/src/pages/signup/components/SignupCard.tsx
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled';
+import { postSignup } from '@/apis/auth';
+
+export default function SignupCard() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    postSignup();
+  };
+
+  return (
+    <Container onSubmit={handleSubmit}>
+      <EmailInput type="email" placeholder="이메일" />
+      <SubmitButton type="submit">회원가입</SubmitButton>
+    </Container>
+  );
+}
+
+const Container = styled.form`
+  display: flex;
+  gap: 22px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 410px;
+  height: 540px;
+  padding: 28px;
+  border-radius: 20px;
+  box-shadow: 0 25px 50px -12px rgb(0 0 0 / 25%);
+
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const EmailInput = styled.input`
+  width: 100%;
+  height: 48px;
+  padding: 12px 16px;
+  border: 1px solid ${({ theme }) => theme.colors.black};
+  border-radius: 12px;
+`;
+
+const SubmitButton = styled.button`
+  width: 100%;
+  height: 48px;
+  border-radius: 12px;
+
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  color: ${({ theme }) => theme.colors.white};
+`;

b15906110c85b03eb805b67c21be888a816cb296
Jeongeun Lee <leeju1009@gmail.com>
2025-07-24 14:17:53 +0900

    test: 테스트가 깨지는 현상 수정

--- frontend/src/utils/date.test.ts
+++ frontend/src/utils/date.test.ts
@@ -2,7 +2,7 @@ import { formatDate } from './date';
 
 describe('formatDate', () => {
   it('Date 정보를 점(.)으로 구분된 문자열로 변환한다.', () => {
-    const date = new Date('2025-07-01T12:00:00Z');
+    const date = new Date('2025-07-01T12:00:00');
     expect(formatDate(date)).toBe('2025.07.01');
   });
 });

9ec72a647707034fee9e71f4513aede5598c9d8c
ryan-dia <cjfdnjs158@naver.com>
2025-07-27 05:12:51 +0900

    fix: 의존성 추가

--- backend/mail-server/src/main/java/news/bombomemail/member/repository/MemberRepository.java
+++ backend/mail-server/src/main/java/news/bombomemail/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package news.bombomemail.member.repository;
 
 import java.util.Optional;
+import news.bombomemail.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {

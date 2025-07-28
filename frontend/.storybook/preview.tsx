a389ef150aa207989ff952c368d32f85acd47f8e
Jeongeun Lee <leeju1009@gmail.com>
2025-07-29 02:28:11 +0900

    fix: 스토리북이 router를 사용하는 컴포넌트 스토리를 렌더링 못하는 문제 수정

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -2,6 +2,10 @@ import { Global, ThemeProvider } from '@emotion/react';
 import type { Preview } from '@storybook/react-webpack5';
 import { theme } from '@/styles/theme';
 import reset from '@/styles/reset.ts';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { routeTree } from '@/routeTree.gen';
+
+const router = createRouter({ routeTree });
 
 const preview: Preview = {
   parameters: {
@@ -18,6 +22,7 @@ const preview: Preview = {
     (Story) => (
       <ThemeProvider theme={theme}>
         <Global styles={reset} />
+        <RouterProvider router={router} />
         <Story />
       </ThemeProvider>
     ),

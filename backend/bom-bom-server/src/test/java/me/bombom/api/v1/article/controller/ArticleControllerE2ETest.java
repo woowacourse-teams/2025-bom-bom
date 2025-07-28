d561474006ecb247694de9dea9d83a52e3fe4228
ChoiDongjun <adong0808@naver.com>
2025-07-29 01:32:30 +0900

    test: 불필요한 라인 제거

--- backend/bom-bom-server/src/test/java/me/bombom/api/v1/article/controller/ArticleControllerE2ETest.java
+++ backend/bom-bom-server/src/test/java/me/bombom/api/v1/article/controller/ArticleControllerE2ETest.java
@@ -35,14 +35,12 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 class ArticleControllerE2ETest {
 
     @Autowired

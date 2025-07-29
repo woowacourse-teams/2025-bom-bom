71e77b1c9395be71b325b1ddef94d67b315b698b
ChoiDongjun <adong0808@naver.com>
2025-07-29 13:28:22 +0900

    build: 보안 취약점으로 인해 Querydsl 버전 변경

--- backend/bom-bom-server/build.gradle.kts
+++ backend/bom-bom-server/build.gradle.kts
@@ -32,6 +32,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.security:spring-security-test")
 
+    // querydsl (using 5.0.0 to avoid CVE-2024-49203 in 5.1.0)
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+
     // lombok
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
@@ -54,4 +60,9 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+// Querydsl 생성된 파일 정리
+tasks.named<Delete>("clean") {
+    delete("src/main/generated")
 }

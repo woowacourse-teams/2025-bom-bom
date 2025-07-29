f1f0199084fa98e699677392272dbf7a3c9adeaa
geonwoo <kysub99@naver.com>
2025-07-29 11:52:11 +0900

    test: profile 추가

--- backend/bom-bom-server/src/test/java/me/bombom/api/v1/member/event/MemberSignupListenerTest.java
+++ backend/bom-bom-server/src/test/java/me/bombom/api/v1/member/event/MemberSignupListenerTest.java
@@ -11,12 +11,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.transaction.TestTransaction;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @SpringBootTest
+@ActiveProfiles("test")
 class MemberSignupListenerTest {
 
     @Autowired

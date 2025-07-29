cac62820fe18ce53889c169d6dba413bb81e090c
ChoiDongjun <adong0808@naver.com>
2025-07-29 21:30:40 +0900

    fix: 디폴트 정렬 기준 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/controller/ArticleController.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/controller/ArticleController.java
@@ -11,6 +11,7 @@ import me.bombom.api.v1.common.resolver.LoginMember;
 import me.bombom.api.v1.member.domain.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
@@ -33,10 +34,11 @@ public class ArticleController {
     @GetMapping
     public Page<ArticleResponse> getArticles(
             @LoginMember Member member,
+//            @ModelAttribute GetArticlesOptions getArticlesOptions,
             @RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE) LocalDate date,
             @RequestParam(required = false) String category,
             @RequestParam(required = false) String keyword,
-            @PageableDefault Pageable pageable
+            @PageableDefault(sort = "arrivedDateTime", direction = Direction.DESC) Pageable pageable
     ) {
         return articleService.getArticles(
                 member,

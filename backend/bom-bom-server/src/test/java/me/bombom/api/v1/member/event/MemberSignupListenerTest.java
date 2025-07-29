1d700d92397043f05241577d856e24cb29f245ae
geonwoo <kysub99@naver.com>
2025-07-29 10:08:11 +0900

    test: 이벤트 리스너 호출 테스트

---
+++ backend/bom-bom-server/src/test/java/me/bombom/api/v1/member/event/MemberSignupListenerTest.java
@@ -0,0 +1,44 @@
+package me.bombom.api.v1.member.event;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import me.bombom.api.v1.TestFixture;
+import me.bombom.api.v1.member.domain.Member;
+import me.bombom.api.v1.member.repository.MemberRepository;
+import me.bombom.api.v1.reading.service.ReadingService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.transaction.TestTransaction;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+class MemberSignupListenerTest {
+
+    @Autowired
+    ApplicationEventPublisher publisher;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @MockitoBean
+    ReadingService readingService;
+
+    @Test
+    void 회원가입_이벤트_발행_시_읽기정보_초기화_메서드가_호출된다() {
+        // given
+        Member member = memberRepository.save(TestFixture.normalMemberFixture());
+
+        // when
+        publisher.publishEvent(new MemberSignupEvent(this, member.getId()));
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
+
+        // then
+        verify(readingService, times(1)).initializeReadingInformation(member.getId());
+    }
+}

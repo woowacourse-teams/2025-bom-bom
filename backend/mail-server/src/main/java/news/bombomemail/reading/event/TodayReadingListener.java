5e2b4613c3b9713998893a9a13d9e9de81aacbc2
ryan-dia <cjfdnjs158@naver.com>
2025-07-30 20:25:29 +0900

    fix: 임시로깅 추가

--- backend/mail-server/src/main/java/news/bombomemail/reading/event/TodayReadingListener.java
+++ backend/mail-server/src/main/java/news/bombomemail/reading/event/TodayReadingListener.java
@@ -1,10 +1,12 @@
 package news.bombomemail.reading.event;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import news.bombomemail.reading.service.TodayReadingService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class TodayReadingListener {
@@ -16,7 +18,8 @@ public class TodayReadingListener {
         try {
             todayReadingService.updateTodayTotalCount(event.memberId());
         } catch (Exception e) {
-
+            // FIXME :: 로깅 시스템 구축후 추가될 예정
+            log.error("오늘 읽기 totoal count 업데이트 실패");
         }
     }
 }
--- backend/mail-server/src/main/java/news/bombomemail/subscribe/event/SubscribeListener.java
+++ backend/mail-server/src/main/java/news/bombomemail/subscribe/event/SubscribeListener.java
@@ -1,10 +1,12 @@
 package news.bombomemail.subscribe.event;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import news.bombomemail.subscribe.service.SubscribeService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SubscribeListener {
@@ -16,7 +18,8 @@ public class SubscribeListener {
         try {
             subscribeService.save(event.newsletterId(), event.memberId());
         } catch (Exception e) {
-
+            // FIXME :: 로깅 시스템 구축후 추가될 예정
+            log.error("구독 리스트 저장 실패");
         }
     }
 }

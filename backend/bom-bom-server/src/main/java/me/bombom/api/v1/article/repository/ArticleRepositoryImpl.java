3dba2786c06ecc9062149cbb2092b8987baa8b11
ChoiDongjun <adong0808@naver.com>
2025-07-30 17:34:59 +0900

    feat: 정렬 기준 컬럼 화이트리스트 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/repository/ArticleRepositoryImpl.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/repository/ArticleRepositoryImpl.java
@@ -9,16 +9,18 @@ import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Path;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import me.bombom.api.v1.article.dto.ArticleResponse;
 import me.bombom.api.v1.article.dto.GetArticlesOptions;
 import me.bombom.api.v1.article.dto.QArticleResponse;
+import me.bombom.api.v1.common.exception.CIllegalArgumentException;
+import me.bombom.api.v1.common.exception.ErrorDetail;
 import me.bombom.api.v1.newsletter.dto.QNewsletterSummaryResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -28,6 +30,13 @@ import org.springframework.util.StringUtils;
 @RequiredArgsConstructor
 public class ArticleRepositoryImpl implements CustomArticleRepository{
 
+    private static final Map<String, Path<?>> SORT_FIELD_WHITELIST_MAP = Map.of(
+            "title", article.title,
+            "createdAt", article.createdAt,
+            "arrivedDateTime", article.arrivedDateTime,
+            "expectedReadTime", article.expectedReadTime
+    );
+
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
@@ -36,7 +45,36 @@ public class ArticleRepositoryImpl implements CustomArticleRepository{
             GetArticlesOptions options,
             Pageable pageable
     ) {
-        Long total = jpaQueryFactory.select(article.count())
+        Long total = getTotal(memberId, options);
+        List<ArticleResponse> content = getContent(memberId, options, pageable);
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public int countAllByMemberId(Long memberId, String keyword) {
+        return jpaQueryFactory.select(article.count())
+                .from(article)
+                .where(createMemberWhereClause(memberId))
+                .where(createKeywordWhereClause(keyword))
+                .fetchOne()
+                .intValue();
+    }
+
+    @Override
+    public int countAllByCategoryIdAndMemberId(Long memberId, Long categoryId, String keyword) {
+        return jpaQueryFactory.select(article.count())
+                .from(article)
+                .join(newsletter).on(article.newsletterId.eq(newsletter.id))
+                .join(category).on(newsletter.categoryId.eq(category.id))
+                .where(createMemberWhereClause(memberId))
+                .where(createKeywordWhereClause(keyword))
+                .where(createCategoryIdWhereClause(categoryId))
+                .fetchOne()
+                .intValue();
+    }
+
+    private Long getTotal(Long memberId, GetArticlesOptions options) {
+        return jpaQueryFactory.select(article.count())
                 .from(article)
                 .join(newsletter).on(article.newsletterId.eq(newsletter.id))
                 .join(category).on(newsletter.categoryId.eq(category.id))
@@ -44,10 +82,10 @@ public class ArticleRepositoryImpl implements CustomArticleRepository{
                 .where(createDateWhereClause(options.date()))
                 .where(createKeywordWhereClause(options.keyword()))
                 .fetchOne();
+    }
 
-        List<OrderSpecifier<?>> orderSpecifiers = getOrderSpecifiers(pageable);
-
-        List<ArticleResponse> content = jpaQueryFactory.select(new QArticleResponse(
+    private List<ArticleResponse> getContent(Long memberId, GetArticlesOptions options, Pageable pageable) {
+        return jpaQueryFactory.select(new QArticleResponse(
                         article.id,
                         article.title,
                         article.contentsSummary,
@@ -64,36 +102,14 @@ public class ArticleRepositoryImpl implements CustomArticleRepository{
                 .where(createDateWhereClause(options.date()))
                 .where(createKeywordWhereClause(options.keyword()))
                 .where(createCategoryNameWhereClause(options.category()))
-                .orderBy(orderSpecifiers.stream().toArray(OrderSpecifier[]::new))
+                .orderBy(getOrderSpecifiers(pageable).toArray(OrderSpecifier[]::new))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
-
-
-        return new PageImpl<>(content, pageable, total);
     }
 
-    @Override
-    public int countAllByMemberId(Long memberId, String keyword) {
-        return jpaQueryFactory.select(article.count())
-                .from(article)
-                .where(createMemberWhereClause(memberId))
-                .where(StringUtils.hasText(keyword) ? article.title.like("%" + keyword + "%") : null)
-                .fetchOne()
-                .intValue();
-    }
-
-    @Override
-    public int countAllByCategoryIdAndMemberId(Long memberId, Long categoryId, String keyword) {
-        return jpaQueryFactory.select(article.count())
-                .from(article)
-                .join(newsletter).on(article.newsletterId.eq(newsletter.id))
-                .join(category).on(newsletter.categoryId.eq(category.id))
-                .where(createMemberWhereClause(memberId))
-                .where(StringUtils.hasText(keyword) ? article.title.like("%" + keyword.trim() + "%") : null)
-                .where(createCategoryIdWhereClause(categoryId))
-                .fetchOne()
-                .intValue();
+    private BooleanExpression createMemberWhereClause(Long memberId) {
+        return article.memberId.eq(memberId);
     }
 
     private Predicate createCategoryNameWhereClause(String categoryName) {
@@ -104,10 +120,6 @@ public class ArticleRepositoryImpl implements CustomArticleRepository{
         return categoryId != null ? category.id.eq(categoryId) : null;
     }
 
-    private BooleanExpression createMemberWhereClause(Long memberId) {
-        return article.memberId.eq(memberId);
-    }
-
     private Predicate createDateWhereClause(LocalDate date) {
         return date != null ? article.arrivedDateTime.between(
                 date.atTime(LocalTime.MIN),
@@ -116,7 +128,7 @@ public class ArticleRepositoryImpl implements CustomArticleRepository{
     }
 
     private Predicate createKeywordWhereClause(String keyword) {
-        return StringUtils.hasText(keyword) ? article.title.like("%" + keyword + "%") : null;
+        return StringUtils.hasText(keyword) ? article.title.like("%" + keyword.trim() + "%") : null;
     }
 
     private List<OrderSpecifier<?>> getOrderSpecifiers(Pageable pageable) {
@@ -124,10 +136,18 @@ public class ArticleRepositoryImpl implements CustomArticleRepository{
         pageable.getSort().stream().forEach(sort -> {
             Order order = sort.isAscending() ? Order.ASC : Order.DESC;
             String property = sort.getProperty();
-            Path<Object> target = Expressions.path(Object.class, article, property);
+            Path<?> target = resolveSortProperty(property);
             OrderSpecifier<?> orderSpecifier = new OrderSpecifier(order, target);
             orderSpecifiers.add(orderSpecifier);
         });
         return orderSpecifiers;
+    }
+
+    private static Path<?> resolveSortProperty(String property) {
+        Path<?> sortPath = SORT_FIELD_WHITELIST_MAP.get(property);
+        if (sortPath == null) {
+            throw new CIllegalArgumentException(ErrorDetail.INVALID_REQUEST_PARAMETER_VALIDATION);
+        }
+        return sortPath;
     }
 }

2fff3f3b091d86f590745ef7a221141c8c085084
ryan-dia <cjfdnjs158@naver.com>
2025-07-30 20:26:21 +0900

    style: 불필요한 공백 제거

--- backend/mail-server/src/main/java/news/bombomemail/reading/event/TodayReadingEvent.java
+++ backend/mail-server/src/main/java/news/bombomemail/reading/event/TodayReadingEvent.java
@@ -2,7 +2,7 @@ package news.bombomemail.reading.event;
 
 public record TodayReadingEvent(Long memberId) {
 
-    public static TodayReadingEvent from (Long memberId) {
+    public static TodayReadingEvent from(Long memberId) {
         return new TodayReadingEvent(memberId);
     }
 }

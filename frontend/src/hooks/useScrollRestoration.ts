db1b8ff2c89cec5bd829c75b13c7c2eba16f8057
Jeongeun Lee <leeju1009@gmail.com>
2025-07-31 17:21:46 +0900

    refactor: 디바운스 커스텀 훅을 사용하도록 리팩토링

--- frontend/src/hooks/useScrollRestoration.ts
+++ frontend/src/hooks/useScrollRestoration.ts
@@ -1,5 +1,6 @@
 import { useLocation } from '@tanstack/react-router';
 import { useLayoutEffect } from 'react';
+import { useDebounce } from './useDebounce';
 import useLocalStorage from './useLocalStorage';
 
 const useScrollRestoration = (path: string) => {
@@ -8,29 +9,31 @@ const useScrollRestoration = (path: string) => {
   const { get: getScrollLocation, set: setScrollLocation } =
     useLocalStorage<number>(storageKey, 0);
 
+  const restoreScroll = useDebounce((scrollLocation: number) => {
+    window.scroll({
+      top: scrollLocation,
+      behavior: 'smooth',
+    });
+  }, 400);
+
   useLayoutEffect(() => {
     if (path !== location.pathname) return;
 
     const scrollLocation = getScrollLocation();
     if (scrollLocation) {
-      setTimeout(() => {
-        window.scroll({
-          top: scrollLocation,
-          behavior: 'smooth',
-        });
-      }, 400);
+      restoreScroll(scrollLocation);
     }
 
-    const handleScrollLocation = () => {
+    const saveLocation = () => {
       setScrollLocation(window.scrollY);
     };
-    window.addEventListener('beforeunload', handleScrollLocation);
+    window.addEventListener('beforeunload', saveLocation);
 
     return () => {
       setScrollLocation(window.scrollY);
-      window.removeEventListener('beforeunload', handleScrollLocation);
+      window.removeEventListener('beforeunload', saveLocation);
     };
-  }, [getScrollLocation, setScrollLocation]);
+  }, [restoreScroll, getScrollLocation, setScrollLocation]);
 };
 
 export default useScrollRestoration;

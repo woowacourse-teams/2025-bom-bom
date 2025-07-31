e6ad6df8e669cb8b892b65e149d0828d74f6bdfa
guesung <gueit214@naver.com>
2025-07-31 17:36:57 +0900

    chore: 복사

--- frontend/src/constants/category.ts
+++ frontend/src/constants/category.ts
@@ -1,17 +1,14 @@
 export const CATEGORIES = [
   '전체',
   '트렌드/라이프',
-  '엔터테인먼트',
   '비즈/재테크',
   '지역/여행',
   '푸드',
   'IT/테크',
-  '디자인',
   '시사/사회',
   '취미/자기개발',
   '문화/예술',
   '리빙/인테리어',
-  '건강/의학',
 ] as const;
 
 export type CategoryType = (typeof CATEGORIES)[number];

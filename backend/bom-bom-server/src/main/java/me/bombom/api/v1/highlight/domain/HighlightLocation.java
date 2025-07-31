4c982818169d49cedbef1ed01e40fcdc46852ea4
ChoiDongjun <adong0808@naver.com>
2025-07-31 21:02:04 +0900

    fix: setter 제거

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/highlight/domain/HighlightLocation.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/highlight/domain/HighlightLocation.java
@@ -6,10 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Embeddable
 @EqualsAndHashCode
 @AllArgsConstructor

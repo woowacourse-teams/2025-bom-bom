a1bf7939e4d99624f0774cef72c5146a8eb56bd3
ChoiDongjun <adong0808@naver.com>
2025-08-01 13:24:42 +0900

    fix: color 요청값을 dto로 받도록 수정

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/highlight/controller/HighlightController.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/highlight/controller/HighlightController.java
@@ -5,12 +5,13 @@ import jakarta.validation.constraints.Positive;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.bombom.api.v1.common.resolver.LoginMember;
-import me.bombom.api.v1.highlight.dto.request.HighlightColorChangeRequest;
+import me.bombom.api.v1.highlight.dto.request.ChangeHighlightColorRequest;
 import me.bombom.api.v1.highlight.dto.request.HighlightCreateRequest;
 import me.bombom.api.v1.highlight.dto.response.HighlightResponse;
 import me.bombom.api.v1.highlight.service.HighlightService;
 import me.bombom.api.v1.member.domain.Member;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/highlights")
@@ -58,7 +60,7 @@ public class HighlightController {
     public void changeHighlightColor(
             @LoginMember Member member,
             @PathVariable @Positive(message = "id는 1 이상의 값이어야 합니다.") Long id,
-            @Valid @RequestBody HighlightColorChangeRequest request
+            @Valid @RequestBody ChangeHighlightColorRequest request
     ) {
         highlightService.changeColor(id, request.color(), member);
     }

ba919a4adb608082bf796dbb6dac4635703670ac
rladmstn <oz33177351@gmail.com>
2025-08-01 10:51:27 +0900

    feat: 펫, 성장 단계 entity 추가

---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/pet/domain/Pet.java
@@ -0,0 +1,44 @@
+package me.bombom.api.v1.pet.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.bombom.api.v1.common.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Pet extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private Long stageId;
+
+    private int currentScore;
+
+    @Builder
+    public Pet(
+            Long id,
+            @NotNull Long memberId,
+            @NotNull Long stageId,
+            int currentScore
+    ) {
+        this.id = id;
+        this.memberId = memberId;
+        this.stageId = stageId;
+        this.currentScore = currentScore;
+    }
+}
---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/pet/domain/Stage.java
@@ -0,0 +1,36 @@
+package me.bombom.api.v1.pet.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.bombom.api.v1.common.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Stage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int level;
+    private int totalScore;
+
+    @Builder
+    public Stage(
+            Long id,
+            int level,
+            int totalScore
+    ) {
+        this.id = id;
+        this.level = level;
+        this.totalScore = totalScore;
+    }
+}

80cbcaa672cfdd074d2290da940aeb7b50b43650
ryan-dia <cjfdnjs158@naver.com>
2025-08-03 20:49:12 +0900

    chore: prometheus 의존성 추가

--- backend/bom-bom-server/build.gradle.kts
+++ backend/bom-bom-server/build.gradle.kts
@@ -46,6 +46,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-security")
 
+    // prometheus
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")

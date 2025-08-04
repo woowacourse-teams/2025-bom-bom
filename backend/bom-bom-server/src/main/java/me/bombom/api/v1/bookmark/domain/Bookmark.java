fb8f4e7aa5c4fc57b8581494b150ace46905246f
ryan-dia <cjfdnjs158@naver.com>
2025-08-04 21:42:03 +0900

    feat: 북마크 도메인 추가

---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/bookmark/domain/Bookmark.java
@@ -0,0 +1,42 @@
+package me.bombom.api.v1.bookmark.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.bombom.api.v1.common.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "bookmark",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"member_id", "article_id"})}
+)
+public class Bookmark extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long articleId;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Builder
+    public Bookmark(Long id, @NotNull Long articleId, @NotNull Long memberId) {
+        this.id = id;
+        this.articleId = articleId;
+        this.memberId = memberId;
+    }
+}

62ea0016846bf1917775a96c24736c54f2038502
ChoiDongjun <adong0808@naver.com>
2025-08-05 01:02:42 +0900

    fix: Color 제거

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/highlight/dto/request/UpdateHighlightRequest.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/highlight/dto/request/UpdateHighlightRequest.java
@@ -1,10 +1,9 @@
 package me.bombom.api.v1.highlight.dto.request;
 
 import jakarta.validation.constraints.Size;
-import me.bombom.api.v1.highlight.domain.Color;
 
 public record UpdateHighlightRequest(
-    Color color,
+    String color,
     @Size(max = 500) String memo
 ) {
 }

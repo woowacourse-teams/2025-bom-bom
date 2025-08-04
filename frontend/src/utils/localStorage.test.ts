159a29db1ce54d8e8987f77b6f4f26e48e07819c
Jeongeun Lee <leeju1009@gmail.com>
2025-08-04 20:14:51 +0900

    test: 반복되는 코드를 재사용하도록 리팩토링

--- frontend/src/utils/localStorage.test.ts
+++ frontend/src/utils/localStorage.test.ts
@@ -2,42 +2,38 @@ import { createStorage } from './localStorage';
 
 describe('createStorage', () => {
   const KEY = 'test';
+  const DEFAULT_DATA = { count: 0 };
+  let storage: ReturnType<typeof createStorage<{ count: number }>>;
 
   beforeEach(() => {
     window.localStorage.clear();
     jest.spyOn(Storage.prototype, 'getItem');
     jest.spyOn(Storage.prototype, 'setItem');
+    jest.spyOn(Storage.prototype, 'removeItem');
+
+    storage = createStorage(KEY, DEFAULT_DATA);
   });
 
   afterEach(() => {
     jest.restoreAllMocks();
   });
 
-  it('local storage에 key가 없으면 defaultData를 반환한다.', () => {
-    const DEFAULT_DATA = 'data';
-
-    const storage = createStorage<string>(KEY, DEFAULT_DATA);
+  it('local storage에 key 값이 없으면 defaultData를 반환한다.', () => {
     const result = storage.get();
 
     expect(window.localStorage.getItem).toHaveBeenCalledWith(KEY);
     expect(result).toEqual(DEFAULT_DATA);
   });
 
-  it('local storage에 key 값이 존재하면 해당 데이터를 파싱하여 반환한다.', () => {
-    const DEFAULT_DATA = { count: 0 };
+  it('get: local storage에 key 값이 존재하면 해당 데이터를 파싱하여 반환한다.', () => {
     const STORED_DATA = { count: 42 };
-
     window.localStorage.setItem(KEY, JSON.stringify(STORED_DATA));
-    const storage = createStorage<{ count: number }>(KEY, DEFAULT_DATA);
 
     expect(storage.get()).toEqual(STORED_DATA);
   });
 
-  it('local storage에 데이터가 정상적으로 저장된다.', () => {
-    const DEFAULT_DATA = { count: 0 };
+  it('set: local storage에 데이터가 정상적으로 저장된다.', () => {
     const NEW_DATA = { count: 42 };
-    const storage = createStorage<{ count: number }>(KEY, DEFAULT_DATA);
-
     storage.set(NEW_DATA);
 
     expect(window.localStorage.setItem).toHaveBeenCalledWith(

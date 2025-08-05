955ae7c32444b236d0e859a7c9bbf9fb2ef5cb75
Lee Jeongeun <106021313+JeLee-river@users.noreply.github.com>
2025-08-05 20:11:01 +0900

    hotfix: 본 서버에서 로그인 시, 개발 서버에서 로그인되는 문제 수정 (#156)

--- frontend/src/apis/env.ts
+++ frontend/src/apis/env.ts
@@ -2,4 +2,6 @@ export const ENV = {
   baseUrl: `${process.env.API_BASE_URL}`,
   token: `${process.env.API_TOKEN}`,
   enableMsw: `${process.env.ENABLE_MSW}`,
+  sentryDsn: `${process.env.SENTRY_DSN}`,
+  nodeEnv: `${process.env.NODE_ENV}`,
 } as const;

435ec0ab05c66425da3e0385283c4553bc837e37
ryan-dia <cjfdnjs158@naver.com>
2025-08-05 15:50:29 +0900

    test: eol준수

--- backend/bom-bom-server/src/test/java/me/bombom/api/v1/TestJpaAuditingConfig.java
+++ backend/bom-bom-server/src/test/java/me/bombom/api/v1/TestJpaAuditingConfig.java
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
-public class TestJpaAuditingConfig {} 
+public class TestJpaAuditingConfig {}

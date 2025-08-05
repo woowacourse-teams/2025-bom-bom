5097292a55f2487b4bf40d0c449cf418b2c36763
Cheol Won <76567238+Ryan-Dia@users.noreply.github.com>
2025-08-05 21:27:45 +0900

    [BE][BOM-222] feat: 북마크 기능 구현 (#145)
    * feat: 북마크 도메인 추가
    
    * feat: 중복 데이터 예외 추가
    
    * test: 북마크 서비스 테스트 추가
    
    * feat: 북마크 조회 기능 추가
    
    * feat: 북마크 상태 조회 기능 구현
    
    * feat: 북마크 저장 기능 구현
    
    * feat: 북마크 삭제 기능 구현
    
    * style: 컨벤션에 맞게 줄바꿈
    
    * fix: 멱등성 고려해서 예외 대신 스킵처리
    
    * style: 컨벤션에 맞게 줄바꿈
    
    * style: 상수 이름 변경
    
    * test: eol준수
    
    * test: assertSoftly 적용
    
    * style: 불필요한 대괄호 삭제
    
    * style: 불필요한 final 키워드 삭제
    
    * style: 메서드명 수정
    
    * style: 메서드 네이밍 수정
    
    * test: 북마크 중복 저장 스킵 테스트로 변경
    
    * fix: 아티클 주인 확인후 북마크 추가
    
    * fix: 북마크 삭제시 아티클 주인인지 확인하도록 로직 추가
    
    * fix: 북마크 url 수정
    
    * fix: bookmarkStatus를 dto에 담아서 반환하도록 수정

---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/bookmark/dto/response/BookmarkResponse.java
@@ -0,0 +1,10 @@
+package me.bombom.api.v1.bookmark.dto.response;
+
+import com.querydsl.core.annotations.QueryProjection;
+import me.bombom.api.v1.article.dto.ArticleResponse;
+
+public record BookmarkResponse(Long id, ArticleResponse articleResponse) {
+
+    @QueryProjection
+    public BookmarkResponse {}
+}

7be0844b12b5e1aef93488525a4ea869f075eec7
rladmstn <oz33177351@gmail.com>
2025-08-05 21:21:04 +0900

    feat: Swagger 접근 시 로그인 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/config/SecurityConfig.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/config/SecurityConfig.java
@@ -3,12 +3,20 @@ package me.bombom.api.v1.common.config;
 import lombok.RequiredArgsConstructor;
 import me.bombom.api.v1.auth.handler.OAuth2LoginSuccessHandler;
 import me.bombom.api.v1.auth.service.CustomOAuth2UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -22,21 +30,33 @@ public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
+    @Value("${swagger.admin.username}")
+    private String adminUsername;
+
+    @Value("${swagger.admin.password}")
+    private String adminPassword;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                // 모든 설정 열어둠
-                .csrf(AbstractHttpConfigurer::disable)
-                .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
-                .cors(configurer -> configurer.configurationSource(corsConfigurationSource()))
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest()
-                        .permitAll())
-                .oauth2Login(oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo
-                        .userService(customOAuth2UserService))
-                        .successHandler(oAuth2LoginSuccessHandler));
-        return http.build();
+            http
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
+                    .cors(configurer -> configurer.configurationSource(corsConfigurationSource()))
+                    .formLogin(Customizer.withDefaults())
+                    .httpBasic(Customizer.withDefaults())
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers(
+                                    "/swagger-ui/**",
+                                    "/v3/api-docs/**",
+                                    "/swagger-resources/**",
+                                    "/webjars/**",
+                                    "/swagger-ui.html"
+                            ).authenticated()
+                            .anyRequest().permitAll())
+                    .oauth2Login(oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo
+                                    .userService(customOAuth2UserService))
+                            .successHandler(oAuth2LoginSuccessHandler));
+            return http.build();
     }
 
     @Bean
@@ -51,5 +71,20 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username(adminUsername)
+                .password(passwordEncoder().encode(adminPassword))
+                .roles("DEVELOPER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }

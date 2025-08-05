ff4ba42e23f1219d44186fd486c1d196155c8662
geonwoo <kysub99@naver.com>
2025-08-05 22:13:41 +0900

    feat: 로깅 및 상수화

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/reading/scheduler/ReadingScheduler.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/reading/scheduler/ReadingScheduler.java
@@ -1,26 +1,32 @@
 package me.bombom.api.v1.reading.scheduler;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.bombom.api.v1.reading.service.ReadingService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ReadingScheduler {
 
     private static final String TIME_ZONE = "Asia/Seoul";
+    private static final String DAILY_CRON = "0 0 0 * * *";
+    private static final String WEEKLY_CRON = "0 0 0 * * MON";
 
     private final ReadingService readingService;
 
-    @Scheduled(cron="0 0 0 * * *", zone = TIME_ZONE)
+    @Scheduled(cron = DAILY_CRON, zone = TIME_ZONE)
     public void daily() {
+        log.info("오늘 읽기 초기화 실행");
         readingService.resetContinueReadingCount();
         readingService.resetTodayReadingCount();
     }
 
-    @Scheduled(cron="0 0 0 * * MON", zone = TIME_ZONE)
+    @Scheduled(cron = WEEKLY_CRON, zone = TIME_ZONE)
     public void weekly() {
+        log.info("주간 읽기 초기화 실행");
         readingService.resetWeeklyReadingCount();
     }
 }

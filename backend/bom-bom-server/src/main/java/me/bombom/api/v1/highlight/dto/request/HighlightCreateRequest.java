190421a9b8639592c5ce74d554d64c7a8d5af534
ChoiDongjun <adong0808@naver.com>
2025-08-06 15:06:09 +0900

    [BE][BOM-225] refactor: Color 값 객체로 수정 (#150)
    * refactor: Color 값 객체에서 정규식 검증하도록 통일
    
    * test: 입력값 검증 테스트 작성
    
    * refactor: Pattern 컴파일 상수화
    
    * refactor: 사용하지 않는 메서드 제거
    
    * [BE][BOM-191] chore: 운영 환경 CI/CD 구축 (#119)
    
    * chore: deploy 작성
    
    * chore: docker compose 작성
    
    * fix: 불필요한 이미지 pull 생략
    
    Co-authored-by: Cheol Won <76567238+Ryan-Dia@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Cheol Won <76567238+Ryan-Dia@users.noreply.github.com>
    
    * chore: docker volume 경로 설정
    
    * chore: 코드 간격 조정
    
    * [BE][BOM-202] feat: 키우기 관련 기능 구현 (#143)
    
    * feat: 펫, 성장 단계 entity 추가
    
    * feat: 키우기 정보 조회 API 구현
    
    * test: 키우기 정보 조회 API 테스트 작성
    
    * feat: 키우기 출석 점수 추가 API 추가
    
    * test: 키우기 출석 점수 추가 API 테스트 작성
    
    * refactor: 키우기 관련 점수 상수화
    
    * refactor: 키우기 점수 정책 상수 위치 변경
    
    * test: 서비스 레이어 테스트 import 오류 해결
    
    * feat: 아티클 읽음 처리 시 키우기 점수 추가 이벤트 등록
    
    * test: 아티클 읽음 처리 시 키우기 점수 추가 이벤트 테스트 작성
    
    * feat: 회원가입 이벤트에 키우기 추가
    
    * test: 회원가입 이벤트에 키우기 추가 테스트 추가
    
    * test: 이벤트 리스너 테스트에 트랜잭션 공통화
    
    * feat: 읽음 처리 API에서 이미 읽은 아티클 검증 추가
    
    * fix: 이벤트 필드 member id로 변경
    
    * feat: 키우기 단계 필드에 not null 제약 추가
    
    * fix: 키우기 점수 필드명 수정
    
    * fix: 키우기 출석 점수 endpoint 수정
    
    * test: 이벤트 관련 테스트 수정
    
    * fix: article의 count 쿼리 관련 NPE 방지
    
    * feat: 키우기의 현재 점수에 대한 column 제약 및 초기화 추가
    
    * refactor: 보너스 점수 가능 여부 메서드 추출
    
    * hotfix: 불필요한 로그인 리다이렉트 로직 수정
    
    * [BE][BOM-222] feat: 북마크 기능 구현 (#145)
    
    * feat: 북마크 도메인 추가
    
    * feat: 중복 데이터 예외 추가
    
    * test: 북마크 서비스 테스트 추가
    
    * feat: 북마크 조회 기능 추가
    
    * feat: 북마크 상태 조회 기능 구현
    
    * feat: 북마크 저장 기능 구현
    
    * feat: 북마크 삭제 기능 구현
    
    * style: 컨벤션에 맞게 줄바꿈
    
    * fix: 멱등성 고려해서 예외 대신 스킵처리
    
    * style: 컨벤션에 맞게 줄바꿈
    
    * style: 상수 이름 변경
    
    * test: eol준수
    
    * test: assertSoftly 적용
    
    * style: 불필요한 대괄호 삭제
    
    * style: 불필요한 final 키워드 삭제
    
    * style: 메서드명 수정
    
    * style: 메서드 네이밍 수정
    
    * test: 북마크 중복 저장 스킵 테스트로 변경
    
    * fix: 아티클 주인 확인후 북마크 추가
    
    * fix: 북마크 삭제시 아티클 주인인지 확인하도록 로직 추가
    
    * fix: 북마크 url 수정
    
    * fix: bookmarkStatus를 dto에 담아서 반환하도록 수정
    
    * chore: docker-compose-dev에서 DB 환경변수 제거
    
    * chore: 사용하지 않는 환경 변수 제거
    
    * chore: yml명 수정
    
    * chore: 사용하지 않는 환경 변수 제거
    
    * chore: 개발 서버용 환경 변수 추가
    
    * chore: 개발 서버용 환경 변수 추가
    
    * chore: pr workflow 따옴표 수정
    
    * chore: 서버 컨테이너 네트워크 설정 추가
    
    * [BE][BOM-244] refactor: 북마크 dto 평탄화 (#164)
    
    * refactor: 북마크 dto 평탄화
    
    * fix: compact 생성자 추가
    
    * fix: 불필요한 compact 생성자 제거
    
    * style: 불필요한 final 삭제
    
    * style: 주석 추가
    
    ---------
    
    Co-authored-by: geonwoo kim <115832836+kysub99@users.noreply.github.com>
    Co-authored-by: Cheol Won <76567238+Ryan-Dia@users.noreply.github.com>
    Co-authored-by: Kim Eunsu <88280787+rladmstn@users.noreply.github.com>
    Co-authored-by: rladmstn <oz33177351@gmail.com>

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/highlight/dto/request/HighlightCreateRequest.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/highlight/dto/request/HighlightCreateRequest.java
@@ -1,16 +1,14 @@
 package me.bombom.api.v1.highlight.dto.request;
 
-import static me.bombom.api.v1.highlight.controller.HighlightController.COLOR_HEX_PATTERN;
-
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import me.bombom.api.v1.highlight.domain.Color;
 
 public record HighlightCreateRequest(
         @NotNull HighlightLocationRequest location,
         @Positive(message = "id는 1 이상의 값이어야 합니다.") Long articleId,
-        @Pattern(regexp = COLOR_HEX_PATTERN) String color,
+        @NotNull Color color,
         @NotNull String text,
         @Size(max = 500) String memo
 ) {
--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/highlight/dto/request/UpdateHighlightRequest.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/highlight/dto/request/UpdateHighlightRequest.java
@@ -1,9 +1,10 @@
 package me.bombom.api.v1.highlight.dto.request;
 
 import jakarta.validation.constraints.Size;
+import me.bombom.api.v1.highlight.domain.Color;
 
 public record UpdateHighlightRequest(
-    String color,
+    Color color,
     @Size(max = 500) String memo
 ) {
 }

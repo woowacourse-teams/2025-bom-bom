bf4354a669ba0bda142e61a7052f35f653c3ad59
ChoiDongjun <adong0808@naver.com>
2025-08-06 17:21:10 +0900

    feat: EnableJpaAuditing 추가

--- backend/bom-bom-server/src/main/java/me/bombom/BomBomServerApplication.java
+++ backend/bom-bom-server/src/main/java/me/bombom/BomBomServerApplication.java
@@ -2,9 +2,11 @@ package me.bombom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 public class BomBomServerApplication {
 

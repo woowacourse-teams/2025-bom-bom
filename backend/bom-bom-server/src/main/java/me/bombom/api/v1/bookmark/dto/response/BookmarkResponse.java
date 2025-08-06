a2adbbc83dabe0f768b7dc5cc493da56b4628b1e
ryan-dia <cjfdnjs158@naver.com>
2025-08-06 13:52:22 +0900

    refactor: 북마크 dto 평탄화

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/bookmark/dto/response/BookmarkResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/bookmark/dto/response/BookmarkResponse.java
@@ -1,10 +1,33 @@
 package me.bombom.api.v1.bookmark.dto.response;
 
 import com.querydsl.core.annotations.QueryProjection;
+import java.time.LocalDateTime;
 import me.bombom.api.v1.article.dto.ArticleResponse;
+import me.bombom.api.v1.newsletter.dto.NewsletterSummaryResponse;
 
-public record BookmarkResponse(Long id, ArticleResponse articleResponse) {
-
+public record BookmarkResponse(
+        Long id,
+        Long articleId,
+        String title,
+        String contentsSummary,
+        LocalDateTime arrivedDateTime,
+        String thumbnailUrl,
+        int expectedReadTime,
+        boolean isRead,
+        NewsletterSummaryResponse newsletter
+) {
     @QueryProjection
-    public BookmarkResponse {}
+    public BookmarkResponse(final Long id, final ArticleResponse articleResponse) {
+        this(
+                id,
+                articleResponse.articleId(),
+                articleResponse.title(),
+                articleResponse.contentsSummary(),
+                articleResponse.arrivedDateTime(),
+                articleResponse.thumbnailUrl(),
+                articleResponse.expectedReadTime(),
+                articleResponse.isRead(),
+                articleResponse.newsletter()
+        );
+    }
 }

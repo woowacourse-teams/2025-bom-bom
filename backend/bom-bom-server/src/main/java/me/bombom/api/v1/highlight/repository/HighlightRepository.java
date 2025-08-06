6394dd7779f9b327f3c87d8cf48e38c7483f2def
ChoiDongjun <adong0808@naver.com>
2025-08-06 19:53:26 +0900

    feat: 동적 쿼리로 두 API 통합

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/highlight/repository/HighlightRepository.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/highlight/repository/HighlightRepository.java
@@ -3,31 +3,9 @@ package me.bombom.api.v1.highlight.repository;
 import java.util.Optional;
 import me.bombom.api.v1.highlight.domain.Highlight;
 import me.bombom.api.v1.highlight.domain.HighlightLocation;
-import me.bombom.api.v1.highlight.dto.response.HighlightResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface HighlightRepository extends JpaRepository<Highlight, Long>, CustomHighlightRepository {
 
     Optional<Highlight> findByArticleIdAndHighlightLocation(Long articleId, HighlightLocation highlightLocation);
-
-    @Query("""
-        SELECT new me.bombom.api.v1.highlight.dto.response.HighlightResponse(
-            h.id,
-            new me.bombom.api.v1.highlight.domain.HighlightLocation(
-                h.highlightLocation.startOffset,
-                h.highlightLocation.startXPath,
-                h.highlightLocation.endOffset,
-                h.highlightLocation.endXPath
-            ),
-            h.articleId,
-            h.color.value,
-            h.text,
-            h.memo
-        )
-        FROM Highlight h
-        JOIN Article a ON h.articleId = a.id
-        WHERE a.memberId = :memberId
-    """)
-    List<HighlightResponse> findByMemberId(Long memberId);
 }

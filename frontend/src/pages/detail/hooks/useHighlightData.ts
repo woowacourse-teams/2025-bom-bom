85f0429fd2ff7fb5cf5f4a3e394cf179114099b0
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-08-07 21:45:40 +0900

    fix: 안쓰는 로직 제거

--- frontend/src/pages/detail/hooks/useHighlightData.ts
+++ frontend/src/pages/detail/hooks/useHighlightData.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { HighlightType } from '../types/highlight';
-import { addHighlightToDOM, removeHighlightFromDOM } from '../utils/highlight';
+import { removeHighlightFromDOM } from '../utils/highlight';
 import {
   deleteHighlight,
   getHighlights,

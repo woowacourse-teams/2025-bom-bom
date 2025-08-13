b80648b763a0f819573b52fee2956f5720a0b861
guesung <gueit214@naver.com>
2025-08-13 15:30:26 +0900

    refactor: `infiniteQueryOptions`로 리팩토링

--- frontend/src/pages/storage/hooks/useInfiniteArticles.ts
+++ frontend/src/pages/storage/hooks/useInfiniteArticles.ts
@@ -1,5 +1,6 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { getArticles, GetArticlesParams } from '@/apis/articles';
+import { GetArticlesParams } from '@/apis/articles';
+import { queries } from '@/apis/queries';
 
 interface UseInfiniteArticlesParams {
   baseQueryParams: GetArticlesParams;
@@ -11,18 +12,7 @@ const useInfiniteArticles = ({
   isPc,
 }: UseInfiniteArticlesParams) => {
   return useInfiniteQuery({
-    queryKey: ['articles', 'infinite', baseQueryParams],
-    queryFn: ({ pageParam = 0 }) =>
-      getArticles({
-        ...baseQueryParams,
-        page: pageParam,
-      }),
-    getNextPageParam: (lastPage, allPages) => {
-      if (!lastPage?.totalPages) return undefined;
-      const nextPage = allPages.length;
-      return nextPage < lastPage.totalPages ? nextPage : undefined;
-    },
-    initialPageParam: 0,
+    ...queries.infiniteArticles(baseQueryParams),
     enabled: !isPc,
   });
 };

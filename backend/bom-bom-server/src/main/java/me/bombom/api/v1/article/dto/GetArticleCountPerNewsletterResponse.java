55455c35a94d5d802cc14e1411dc27c715ecf028
geonwoo <kysub99@naver.com>
2025-08-13 20:01:42 +0900

    feat: 아티클 보관함에서 뉴스레터 조회 응답 이미지 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/dto/GetArticleCountPerNewsletterResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/dto/GetArticleCountPerNewsletterResponse.java
@@ -4,10 +4,11 @@ import me.bombom.api.v1.newsletter.domain.Newsletter;
 
 public record GetArticleCountPerNewsletterResponse(
         String newsletter,
+        String imageUrl,
         long count
 ) {
 
     public static GetArticleCountPerNewsletterResponse of(Newsletter newsletter, long count) {
-        return new GetArticleCountPerNewsletterResponse(newsletter.getName(), count);
+        return new GetArticleCountPerNewsletterResponse(newsletter.getName(), newsletter.getImageUrl(), count);
     }
 }

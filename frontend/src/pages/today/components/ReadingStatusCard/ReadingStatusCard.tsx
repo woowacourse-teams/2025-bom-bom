33a8fbfa416141bcfa04e16b47a5b78a939871d7
Jeongeun Lee <leeju1009@gmail.com>
2025-08-13 21:08:32 +0900

    refactor: 모바일 환경도ProgressWithLabel 컴포넌트를 사용하도록 수정

--- frontend/src/pages/today/components/ReadingStatusCard/ReadingStatusCard.tsx
+++ frontend/src/pages/today/components/ReadingStatusCard/ReadingStatusCard.tsx
@@ -6,7 +6,6 @@ import { queries } from '@/apis/queries';
 import ProgressWithLabel from '@/components/ProgressWithLabel/ProgressWithLabel';
 import { DeviceType, useDeviceType } from '@/hooks/useDeviceType';
 import { theme } from '@/styles/theme';
-import { calculateRate } from '@/utils/math';
 import type { CSSObject, Theme } from '@emotion/react';
 import GoalIcon from '#/assets/goal.svg';
 import StatusIcon from '#/assets/reading-status.svg';
@@ -21,16 +20,10 @@ function ReadingStatusCard() {
 
   const { streakReadDay, today, weekly } = data;
   const todayProgressDescription =
-    today?.readCount &&
-    today?.totalCount &&
-    today?.readCount < today?.totalCount
-      ? '목표까지 조금 더!'
-      : '목표 달성!';
-  const weeklyProgressDescription =
-    weekly?.readCount &&
-    weekly?.goalCount &&
-    weekly?.readCount < weekly?.goalCount
-      ? `목표까지 ${weekly?.goalCount - weekly?.readCount}개 남음`
+    today.readCount < today.totalCount ? '목표까지 조금 더!' : '목표 달성!';
+  const weeklyGoalDescription =
+    weekly.readCount < weekly.goalCount
+      ? `목표까지 ${weekly.goalCount - weekly.readCount}개 남음`
       : '목표 달성!';
 
   return (
@@ -66,8 +59,8 @@ function ReadingStatusCard() {
             label="오늘의 진행률"
             Icon={GoalIcon}
             value={{
-              currentCount: today?.readCount ?? 0,
-              totalCount: today?.totalCount ?? 0,
+              currentCount: today.readCount,
+              totalCount: today.totalCount,
             }}
             {...(deviceType === 'pc'
               ? { description: todayProgressDescription }
@@ -77,11 +70,11 @@ function ReadingStatusCard() {
             label="주간 목표"
             Icon={GoalIcon}
             value={{
-              currentCount: weekly?.readCount ?? 0,
-              totalCount: weekly?.goalCount ?? 0,
+              currentCount: weekly.readCount,
+              totalCount: weekly.goalCount,
             }}
             {...(deviceType === 'pc'
-              ? { description: weeklyProgressDescription }
+              ? { description: weeklyGoalDescription }
               : {})}
             rateFormat="ratio"
           />
@@ -89,27 +82,30 @@ function ReadingStatusCard() {
       ) : (
         <StatisticWrapper>
           <StreakWrapper deviceType={deviceType}>
-            <StreakCounter streakReadDay={streakReadDay ?? 0} />
+            <StreakCounter streakReadDay={streakReadDay} />
             <StreakDescription deviceType={deviceType}>
               연속 읽기 중!
             </StreakDescription>
           </StreakWrapper>
 
           <GoalWrapper>
-            <Goal>
-              오늘의 진행률
-              <ReachedGoal>{`${calculateRate(
-                weekly?.readCount ?? 0,
-                weekly?.goalCount ?? 0,
-              )}%`}</ReachedGoal>
-            </Goal>
-            <Goal>
-              주간 목표
-              <Rate>
-                <ReachedGoal>{weekly?.readCount ?? 0}</ReachedGoal>
-                <TotalGoal>{` / ${today?.totalCount ?? 0}`}</TotalGoal>
-              </Rate>
-            </Goal>
+            <ProgressWithLabel
+              label="오늘의 진행률"
+              value={{
+                currentCount: today.readCount,
+                totalCount: today.totalCount,
+              }}
+              showGraph={false}
+            />
+            <ProgressWithLabel
+              label="주간 목표"
+              value={{
+                currentCount: weekly.readCount,
+                totalCount: weekly.goalCount,
+              }}
+              rateFormat="ratio"
+              showGraph={false}
+            />
           </GoalWrapper>
         </StatisticWrapper>
       )}
@@ -222,29 +218,6 @@ const GoalWrapper = styled.div`
   justify-content: center;
 
   text-align: center;
-`;
-
-const Goal = styled.p`
-  width: 100%;
-
-  display: flex;
-  justify-content: space-between;
-
-  color: ${({ theme }) => theme.colors.textSecondary};
-  font: ${({ theme }) => theme.fonts.body2};
-`;
-
-const Rate = styled.div`
-  display: flex;
-`;
-
-const ReachedGoal = styled.span`
-  color: ${({ theme }) => theme.colors.primary};
-  font: ${({ theme }) => theme.fonts.body2};
-`;
-
-const TotalGoal = styled.span`
-  font: ${({ theme }) => theme.fonts.body2};
 `;
 
 const containerStyles: Record<DeviceType, (theme: Theme) => CSSObject> = {

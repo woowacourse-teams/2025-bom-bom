1843caf0816c4a4ed0920009301594e1d34f9403
ryan-dia <cjfdnjs158@naver.com>
2025-08-13 21:43:29 +0900

    feat: 로깅 필터 추가

---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/log/MDCLoggingFilter.java
@@ -0,0 +1,37 @@
+package me.bombom.api.log;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@Component
+public class MDCLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+        if (requestURI.startsWith("/.well-known/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        UUID traceId = UUID.randomUUID();
+        MDC.put("traceId", traceId.toString());
+        MDC.put("API", request.getRequestURI());
+        MDC.put("method", request.getMethod());
+
+        filterChain.doFilter(request, response);
+
+        MDC.clear();
+    }
+}

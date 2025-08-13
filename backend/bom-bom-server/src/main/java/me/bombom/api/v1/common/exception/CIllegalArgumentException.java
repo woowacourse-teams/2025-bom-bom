81178b98d8eac0a13138f88c1425cbb15e48b3da
ryan-dia <cjfdnjs158@naver.com>
2025-08-13 21:39:23 +0900

    feat: 커스텀 예외 수정

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/exception/CIllegalArgumentException.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/exception/CIllegalArgumentException.java
@@ -1,14 +1,25 @@
 package me.bombom.api.v1.common.exception;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@RequiredArgsConstructor
 public class CIllegalArgumentException extends RuntimeException {
 
     private final ErrorDetail errorDetail;
+    private final Map<String, Object> context = new HashMap<>();
+
+    public CIllegalArgumentException(ErrorDetail errorDetail) {
+        super(errorDetail.getMessage());
+        this.errorDetail = errorDetail;
+    }
+
+    public CIllegalArgumentException addContext(String key, Object value) {
+        this.context.put(key, value);
+        return this;
+    }
 
     public HttpStatus getHttpStatus(){
       return errorDetail.getStatus();
--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/exception/CServerErrorException.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/exception/CServerErrorException.java
@@ -1,14 +1,25 @@
 package me.bombom.api.v1.common.exception;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@RequiredArgsConstructor
 public class CServerErrorException extends RuntimeException{
 
     private final ErrorDetail errorDetail;
+    private final Map<String, Object> context = new HashMap<>();
+
+    public CServerErrorException(ErrorDetail errorDetail) {
+        super(errorDetail.getMessage());
+        this.errorDetail = errorDetail;
+    }
+
+    public CServerErrorException addContext(String key, Object value) {
+        this.context.put(key, value);
+        return this;
+    }
 
     public HttpStatus getHttpStatus(){
         return errorDetail.getStatus();

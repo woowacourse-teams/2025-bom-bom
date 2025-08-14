3760364fd451d59efd8c3bf42ecc6d8b95ad3ab8
ryan-dia <cjfdnjs158@naver.com>
2025-08-14 17:30:29 +0900

    style: 불필요한 공백 제거

--- backend/bom-bom-server/src/main/java/me/bombom/api/log/MDCLoggingFilter.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/log/MDCLoggingFilter.java
@@ -18,7 +18,6 @@ public class MDCLoggingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-
         String requestURI = request.getRequestURI();
         if (requestURI.startsWith("/.well-known/")) {
             filterChain.doFilter(request, response);

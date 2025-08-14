1c8e0643e7f848c6c48934857ff9d53e6b59b0a0
geonwoo <kysub99@naver.com>
2025-08-14 19:34:12 +0900

    docs: shema api 설정 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/bookmark/dto/response/GetBookmarkNewsletterStatisticsResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/bookmark/dto/response/GetBookmarkNewsletterStatisticsResponse.java
@@ -1,9 +1,15 @@
 package me.bombom.api.v1.bookmark.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public record GetBookmarkNewsletterStatisticsResponse(
+        @Schema(type = "integer", format = "int32", description = "전체 북마크 수", required = true)
         int totalCount,
+
+        @NotNull
+        @Schema(type = "array", description = "뉴스레터별 통계", required = true)
         List<GetBookmarkCountPerNewsletterResponse> newsletters
 ) {
 

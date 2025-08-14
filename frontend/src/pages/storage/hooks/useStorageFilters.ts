d9bcc460b638ad5b4b9153031d8d4e4f3bf43291
guesung <gueit214@naver.com>
2025-08-15 07:56:55 +0900

    refactor: 미사용 필드 제거

--- frontend/src/pages/storage/hooks/useStorageFilters.ts
+++ frontend/src/pages/storage/hooks/useStorageFilters.ts
@@ -45,7 +45,6 @@ export const useStorageFilters = () => {
     selectedCategory,
     sortFilter,
     searchInput,
-    debouncedSearchInput,
     baseQueryParams,
     categoryCounts,
     existCategories,

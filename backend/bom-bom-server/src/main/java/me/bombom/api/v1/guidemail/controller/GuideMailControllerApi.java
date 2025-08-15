772b9f37ba97ef4eda925013e9e2409c52f6c300
ChoiDongjun <adong0808@naver.com>
2025-08-15 16:24:28 +0900

    docs: 스웨거 수정

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/guidemail/controller/GuideMailControllerApi.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/guidemail/controller/GuideMailControllerApi.java
@@ -15,7 +15,8 @@ public interface GuideMailControllerApi {
         description = "가이드 메일 읽었을 시, 키우기/읽기 점수 증가"
     )
     @ApiResponses({
-        @ApiResponse(responseCode = "204", description = "읽음 처리 성공")
+        @ApiResponse(responseCode = "204", description = "읽음 처리 성공"),
+        @ApiResponse(responseCode = "404", description = "멤버의 읽기 정보가 없을 경우")
     })
     void updateRead(
         @Parameter(hidden = true) Member member

148171e42bf5a675d8ecbd5c19ddc6f31cc8febf
Jeongeun Lee <leeju1009@gmail.com>
2025-08-18 05:24:14 +0900

    feat: 모달이 열렸을 때 외부 스크롤이 동작하지 않도록 기능 추가

--- frontend/src/components/Modal/useModal.ts
+++ frontend/src/components/Modal/useModal.ts
@@ -1,14 +1,25 @@
-import { MouseEvent, useRef, useCallback } from 'react';
+import {
+  MouseEvent,
+  useRef,
+  useCallback,
+  useState,
+  useEffect,
+  useMemo,
+} from 'react';
 
 const useModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const modalRef = useRef<HTMLDialogElement>(null);
+  const bodyScrollStatus = useMemo(() => document.body.style.overflow, []);
 
   const openModal = useCallback(() => {
     modalRef.current?.showModal();
+    setIsOpen(true);
   }, []);
 
   const closeModal = useCallback(() => {
     modalRef.current?.close();
+    setIsOpen(false);
   }, []);
 
   const clickOutsideModal = useCallback(
@@ -18,8 +29,20 @@ const useModal = () => {
         closeModal();
       }
     },
-    [],
+    [closeModal],
   );
+
+  const toggleScrollLock = useCallback(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = bodyScrollStatus;
+    }
+  }, [isOpen, bodyScrollStatus]);
+
+  useEffect(() => {
+    toggleScrollLock();
+  }, [isOpen, toggleScrollLock]);
 
   return {
     modalRef,

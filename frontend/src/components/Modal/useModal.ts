8e36f7ee57434b857139803ca048a69b171423a0
Jeongeun Lee <leeju1009@gmail.com>
2025-08-18 04:25:42 +0900

    refactor: 커스텀 훅의 함수에 useCallback 추가

--- frontend/src/components/Modal/useModal.ts
+++ frontend/src/components/Modal/useModal.ts
@@ -1,22 +1,25 @@
-import { MouseEvent, useRef } from 'react';
+import { MouseEvent, useRef, useCallback } from 'react';
 
 const useModal = () => {
   const modalRef = useRef<HTMLDialogElement>(null);
 
-  const openModal = () => {
+  const openModal = useCallback(() => {
     modalRef.current?.showModal();
-  };
+  }, []);
 
-  const closeModal = () => {
+  const closeModal = useCallback(() => {
     modalRef.current?.close();
-  };
+  }, []);
 
-  const clickOutsideModal = (event: MouseEvent<HTMLDialogElement>) => {
-    const { target, currentTarget } = event;
-    if (target === currentTarget) {
-      closeModal();
-    }
-  };
+  const clickOutsideModal = useCallback(
+    (event: MouseEvent<HTMLDialogElement>) => {
+      const { target, currentTarget } = event;
+      if (target === currentTarget) {
+        closeModal();
+      }
+    },
+    [],
+  );
 
   return {
     modalRef,

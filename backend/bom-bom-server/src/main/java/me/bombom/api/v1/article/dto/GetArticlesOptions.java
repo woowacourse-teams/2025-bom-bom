fd0e742c5cb24a0408bf46e4e83fa48b7dfe08f6
geonwoo <kysub99@naver.com>
2025-08-19 16:22:29 +0900

    feat: keyword 패턴 및 사이즈 검증

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/dto/GetArticlesOptions.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/dto/GetArticlesOptions.java
@@ -1,12 +1,21 @@
 package me.bombom.api.v1.article.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 public record GetArticlesOptions(
-        @DateTimeFormat(iso = ISO.DATE) LocalDate date,
+        @DateTimeFormat(iso = ISO.DATE)
+        LocalDate date,
+
+        @Positive(message = "id는 1 이상의 값이어야 합니다.")
         Long newsletterId,
+
+        @Size(min = 2, max = 100, message = "검색어는 2자 이상 100자 이하로 입력해야 합니다.")
+        @Pattern(regexp = ".*\\S.*", message = "검색어는 빈값 또는 공백만으로 입력할 수 없습니다.")
         String keyword
 ) {
 

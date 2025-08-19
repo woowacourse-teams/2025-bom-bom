3dfa696d0935bea90535c08f9fa821f5e486f14e
ChoiDongjun <adong0808@naver.com>
2025-08-19 17:16:06 +0900

    feat: limit 검증 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/reading/controller/ReadingController.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/reading/controller/ReadingController.java
@@ -1,6 +1,7 @@
 package me.bombom.api.v1.reading.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.bombom.api.v1.common.resolver.LoginMember;
@@ -10,6 +11,7 @@ import me.bombom.api.v1.reading.dto.response.MonthlyReadingRankResponse;
 import me.bombom.api.v1.reading.dto.response.ReadingInformationResponse;
 import me.bombom.api.v1.reading.dto.response.WeeklyGoalCountResponse;
 import me.bombom.api.v1.reading.service.ReadingService;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/members/me/reading")
@@ -38,7 +41,7 @@ public class ReadingController implements ReadingControllerApi{
 
     @Override
     @GetMapping("/month/rank")
-    public List<MonthlyReadingRankResponse> getMonthlyReadingRank(@RequestParam int limit) {
+    public List<MonthlyReadingRankResponse> getMonthlyReadingRank(@RequestParam @Positive(message = "limit는 1 이상의 값이어야 합니다.") int limit) {
         return readingService.getMonthlyReadingRank(limit);
     }
 }

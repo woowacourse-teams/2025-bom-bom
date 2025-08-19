51906040649aef81fe9a92674e24bb7912a5ed72
ChoiDongjun <adong0808@naver.com>
2025-08-19 13:22:34 +0900

    fix: API 메서드와 엔드포인트 변경

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/guidemail/controller/GuideMailController.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/guidemail/controller/GuideMailController.java
@@ -5,19 +5,19 @@ import me.bombom.api.v1.common.resolver.LoginMember;
 import me.bombom.api.v1.guidemail.service.GuideMailService;
 import me.bombom.api.v1.member.domain.Member;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/guide")
+@RequestMapping("/api/v1/guide")
 public class GuideMailController implements GuideMailControllerApi{
 
     private final GuideMailService guideMailService;
 
-    @PostMapping("/read")
+    @PatchMapping("/read")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateRead(@LoginMember Member member) {
         guideMailService.updateReadScore(member.getId());

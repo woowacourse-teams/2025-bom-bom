bdd6c205c8cfa973fc77763458617936f92afe38
geonwoo <kysub99@naver.com>
2025-08-20 14:35:38 +0900

    test: 회원 탈퇴 테스트

--- backend/bom-bom-server/src/test/java/me/bombom/api/v1/member/service/MemberServiceTest.java
+++ backend/bom-bom-server/src/test/java/me/bombom/api/v1/member/service/MemberServiceTest.java
@@ -1,8 +1,10 @@
 package me.bombom.api.v1.member.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
+import java.util.Optional;
 import me.bombom.api.v1.TestFixture;
 import me.bombom.api.v1.auth.dto.PendingOAuth2Member;
 import me.bombom.api.v1.common.config.QuerydslConfig;
@@ -15,6 +17,7 @@ import me.bombom.api.v1.member.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 
 @DataJpaTest
@@ -26,6 +29,9 @@ class MemberServiceTest {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Test
     void 회원가입_중_이미_존재하는_닉네임이면_예외_발생() {
@@ -77,5 +83,36 @@ class MemberServiceTest {
         assertThatThrownBy(() -> memberService.signup(oAuth2Member, memberSignupRequest))
                 .isInstanceOf(CIllegalArgumentException.class)
                 .hasFieldOrPropertyWithValue("errorDetail", ErrorDetail.DUPLICATE_EMAIL);
+    }
+
+    @Test
+    void 회원_탈퇴_성공() {
+        // given
+        Member member = TestFixture.normalMemberFixture();
+        memberRepository.save(member);
+        Long memberId = member.getId();
+
+        // when
+        memberService.withdraw(memberId);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        // then
+        Optional<Member> foundMemberOptional = memberRepository.findById(memberId);
+        assertThat(foundMemberOptional).isEmpty();
+    }
+
+    @Test
+    void 존재하지_않는_회원_탈퇴_시_예외_발생() {
+        // given
+        Long nonExistentMemberId = 0L;
+
+        // when & then
+        assertThatThrownBy(() -> memberService.withdraw(nonExistentMemberId))
+                .isInstanceOf(CIllegalArgumentException.class)
+                .hasFieldOrPropertyWithValue("errorDetail", ErrorDetail.ENTITY_NOT_FOUND)
+                .hasFieldOrPropertyWithValue("context.memberId", nonExistentMemberId)
+                .hasFieldOrPropertyWithValue("context.entityType", "member");
     }
 }

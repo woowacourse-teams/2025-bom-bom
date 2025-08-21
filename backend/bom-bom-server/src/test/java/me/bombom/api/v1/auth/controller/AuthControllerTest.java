5f62179a6b4c9906ac690286375a00f6271f05b2
ChoiDongjun <adong0808@naver.com>
2025-08-21 12:47:33 +0900

    test: 회원가입 입력 필드 중복 검사 API 테스트 작성

---
+++ backend/bom-bom-server/src/test/java/me/bombom/api/v1/auth/controller/AuthControllerTest.java
@@ -0,0 +1,79 @@
+package me.bombom.api.v1.auth.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import me.bombom.api.v1.TestFixture;
+import me.bombom.api.v1.member.domain.Member;
+import me.bombom.api.v1.member.repository.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private Member member;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository.deleteAllInBatch();
+        member = TestFixture.normalMemberFixture();
+        memberRepository.save(member);
+    }
+
+    @Test
+    void 닉네임_중복_체크_TRUE() throws Exception {
+        mockMvc.perform(get("/api/v1/auth/signup/check")
+                        .param("field", "NICKNAME")
+                        .param("value", member.getNickname())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+
+    @Test
+    void 닉네임_중복_체크_FALSE() throws Exception {
+        mockMvc.perform(get("/api/v1/auth/signup/check")
+                        .param("field", "nickname")
+                        .param("value", "anotherNickname")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("false"));
+    }
+
+    @Test
+    void 이메일_중복_체크_TRUE() throws Exception {
+        mockMvc.perform(get("/api/v1/auth/signup/check")
+                        .param("field", "EMAIL")
+                        .param("value", member.getEmail())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+
+    @Test
+    void 유효하지_않은_필드_입력_시_예외() throws Exception {
+        mockMvc.perform(get("/api/v1/auth/signup/check")
+                        .param("field", "INVALID")
+                        .param("value", member.getEmail())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+}

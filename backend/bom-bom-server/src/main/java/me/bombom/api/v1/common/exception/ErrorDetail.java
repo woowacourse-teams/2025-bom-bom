551c60db128e260f2c3193d1069dad61c2cc8abd
geonwoo kim <115832836+kysub99@users.noreply.github.com>
2025-08-21 11:19:22 +0900

    [BE][BOM-343] feat: 회원 탈퇴 및 soft delete 기능 구현 (#245)
    * feat: 회원 탈퇴 및 soft delete 기능
    
    * test: 회원 탈퇴 테스트
    
    * feat: 탈퇴 후 1년 지난 회원 삭제 스케줄러
    
    * refactor: SQLDelete 사용
    
    * fix: native query 사용
    
    * style: 컨벤션 적용

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/exception/ErrorDetail.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/exception/ErrorDetail.java
@@ -31,6 +31,7 @@ public enum ErrorDetail {
     DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "J005", "이미 사용 중인 닉네임입니다."),
     UNSUPPORTED_OAUTH2_PROVIDER(HttpStatus.BAD_REQUEST, "J006", "지원하지 않는 소셜 로그인 입니다."),
     DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "J007", "이미 사용 중인 이메일입니다."),
+    WITHDRAWN_MEMBER(HttpStatus.BAD_REQUEST, "J008", "이미 탈퇴한 회원입니다."),
 
     /*
     * G : 인가

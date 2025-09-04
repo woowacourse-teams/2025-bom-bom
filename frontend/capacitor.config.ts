7d52bb9fe998c38b4977f615a65234d2a48e73ed
guesung <gueit214@naver.com>
2025-09-04 17:12:55 +0900

    feat: andoid, ios 세팅

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'frontend-',
+  webDir: 'www'
+};
+
+export default config;

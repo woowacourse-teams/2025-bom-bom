47aba0872dc2eed8b6a615882cc8e8cc846fb272
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-09-05 09:51:01 +0900

    [FE][BOM-378] 토스트 제작 (#276)
    * feat: 토스트 구현
    
    * refactor: 토스트 디자인 수정
    
    * chore: 폴더 위치 이동
    
    * refactor: 사용성을 올려주는 토스트 유틸 함수 추가
    
    * story: 토스트 스토리북 추가
    
    * fix: 이름 충돌로 인한 네이밍 변경
    
    * fix: 스토리북 아이콘 오류 해결
    
    * refactor: props 인터페이스로 변경
    
    * refactor: 전역 스타일 변수 사용
    
    * fix: Toast role 변경
    
    * refactor: 중복 방지 로직 수정
    
    * refactor: 불필요한 getDistributedToasts 함수 삭제

---
+++ frontend/src/components/Toast/Toast.constants.ts
@@ -0,0 +1,5 @@
+import { ToastPosition } from './Toast.types';
+
+export const DEFAULT_LIMIT = 3;
+export const DEFAULT_DURATION = 5000;
+export const DEFAULT_POSITION: ToastPosition = 'bottom-right';

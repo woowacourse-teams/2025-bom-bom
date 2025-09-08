c93eb270ffa3a8b6e21c2e270282260a53d1fa43
Jeongeun Lee <leeju1009@gmail.com>
2025-09-08 17:27:06 +0900

    feat: expo prebuild에 클린 옵션을 추가하여 진행

--- app/android/app/src/main/java/com/antarctica/bombom/MainActivity.kt
+++ app/android/app/src/main/java/com/antarctica/bombom/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.app
+package com.antarctica.bombom
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- app/android/app/src/main/java/com/antarctica/bombom/MainApplication.kt
+++ app/android/app/src/main/java/com/antarctica/bombom/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.app
+package com.antarctica.bombom
 
 import android.app.Application
 import android.content.res.Configuration

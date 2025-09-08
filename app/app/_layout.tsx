d116d42273a2801ba509f032d34a1c842c24b560
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-09-08 21:30:45 +0900

    fix: 구글 로그인 오류 해결

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -3,10 +3,13 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 import WebView from 'react-native-webview';
 
+const CHROME_USER_AGENT =
+  "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/96.0.4664.116 Mobile/15E148 Safari/604.1";
+
 export default function RootLayout() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <WebView source={{ uri: 'https://dev.bombom.news' }} />
+      <WebView source={{ uri: 'https://dev.bombom.news' }} userAgent={CHROME_USER_AGENT} />
     </SafeAreaView>
   );
 }

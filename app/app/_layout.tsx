2f9e56053a65ed0298aabe8587b1015e56b6ced1
guesung <gueit214@naver.com>
2025-09-15 16:04:25 +0900

    chore: webviewDebuggingEnabled, pullToRefreshEnabled 활성화

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,7 +1,7 @@
-import 'react-native-reanimated';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import "react-native-reanimated";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import WebView from 'react-native-webview';
+import WebView from "react-native-webview";
 
 const CHROME_USER_AGENT =
   "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/96.0.4664.116 Mobile/15E148 Safari/604.1";
@@ -10,11 +10,13 @@ export default function RootLayout() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <WebView
-        source={{ uri: 'https://www.bombom.news' }}
+        source={{ uri: "https://dev.bombom.news" }}
         userAgent={CHROME_USER_AGENT}
-        allowsBackForwardNavigationGestures={true}
-        sharedCookiesEnabled={true}
-        thirdPartyCookiesEnabled={true}
+        allowsBackForwardNavigationGestures
+        sharedCookiesEnabled
+        thirdPartyCookiesEnabled
+        webviewDebuggingEnabled
+        pullToRefreshEnabled
       />
     </SafeAreaView>
   );

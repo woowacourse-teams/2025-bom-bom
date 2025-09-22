c086ba67535bc9dfef58679ed9ea43ee8bbd0502
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-09-22 13:20:35 +0900

    refactor: opened를 파생상태로 변경

--- frontend/src/pages/detail/hooks/useFloatingToolbarState.ts
+++ frontend/src/pages/detail/hooks/useFloatingToolbarState.ts
@@ -1,20 +1,19 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { FloatingToolbarMode } from './../components/FloatingToolbar/FloatingToolbar.types';
 import { Position } from '@/types/position';
 
 const DEFAULT_POSITION: Position = { x: 0, y: 0 };
-const DEFAULT_MODE: FloatingToolbarMode = 'new';
+const DEFAULT_MODE: FloatingToolbarMode = 'none';
 
 export const useFloatingToolbarState = () => {
-  const [opened, setOpened] = useState(false);
   const [position, setPosition] = useState<Position>(DEFAULT_POSITION);
   const [mode, setMode] = useState<FloatingToolbarMode>(DEFAULT_MODE);
+  const opened = useMemo(() => mode !== 'none', [mode]);
 
   const showToolbar = useCallback(
     ({ position, mode }: { position: Position; mode: FloatingToolbarMode }) => {
       setPosition(position);
       setMode(mode);
-      setOpened(true);
     },
     [],
   );
@@ -22,7 +21,6 @@ export const useFloatingToolbarState = () => {
   const hideToolbar = useCallback(() => {
     setPosition(DEFAULT_POSITION);
     setMode(DEFAULT_MODE);
-    setOpened(false);
   }, []);
 
   return { opened, position, mode, showToolbar, hideToolbar };

38a3c72dbcb702ea8821839243b18e3f9b60ea75
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-09-23 19:12:00 +0900

    chore: 주석 구체화

--- app/plugins/withCustomAndroidConfig.js
+++ app/plugins/withCustomAndroidConfig.js
@@ -5,13 +5,22 @@ const { withAndroidManifest } = require('@expo/config-plugins');
  * - application 태그에 usesCleartextTraffic, networkSecurityConfig 속성 주입
  */
 function withCustomAndroidConfig(config) {
+  // withAndroidManifest: Expo가 AndroidManifest.xml을 파싱한 결과(modResults)를 수정할 수 있게 해주는 래퍼
+  // 콜백에서 수정된 config를 반환하면, prebuild 시점에 해당 변경이 반영됨
   return withAndroidManifest(config, (config) => {
+    // AndroidManifest.xml의 최상위 <manifest> 안에 있는 <application> 배열을 가져옴
+    // 보통 하나만 존재하므로 [0]으로 접근
     const app = config.modResults.manifest.application[0];
 
-    // Application 태그에 속성 주입
+    // Application 태그의 속성 객체(app.$)에 키/값으로 원하는 속성들을 주입
+    // 'android:usesCleartextTraffic' = 'true' : HTTP(클리어텍스트) 트래픽을 허용
     app.$['android:usesCleartextTraffic'] = 'true';
+
+    // 'android:networkSecurityConfig' = '@xml/network_security_config'
+    // → res/xml/network_security_config.xml 파일을 네트워크 보안 설정으로 사용
     app.$['android:networkSecurityConfig'] = '@xml/network_security_config';
 
+    // 수정이 끝난 config를 반환 (필수)
     return config;
   });
 }

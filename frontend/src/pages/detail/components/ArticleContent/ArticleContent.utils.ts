624c0880fadde688bd1258e4adcc86de90d5e1d2
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-09-24 14:33:18 +0900

    refactor: br 태그 추가 로직 제거 및 긱뉴스 절취선 제거 로직 추가

--- frontend/src/pages/detail/components/ArticleContent/ArticleContent.utils.ts
+++ frontend/src/pages/detail/components/ArticleContent/ArticleContent.utils.ts
@@ -1,8 +1,25 @@
 export const processContent = (content: string): string => {
   if (!content) return '';
 
-  return content
-    .replace(/\\r\\n/g, '<br>') // Windows 개행
-    .replace(/\r/g, '<br>') // Mac 개행 (구버전)
-    .replace(/\n/g, '<br>'); // Unix/Linux 개행
+  return (
+    content
+      .replace(/\\r\\n/g, '') // Windows 개행
+      .replace(/\r/g, '') // Mac 개행 (구버전)
+      .replace(/\n/g, '<br>') // Unix/Linux 개행
+      // 긱뉴스 절취선을 제거하기 위해서 추가
+      // * 링크 패턴을 <ul><li><a>로 변환
+      .replace(
+        /-{5,}\s*([\s\S]*?)\s*\*\s*(https?:\/\/\S+)\s*-{5,}/g,
+        '<ul><li><a href="$2" target="_blank" rel="noopener noreferrer">$1</a></li></ul>',
+      )
+      .trim()
+  );
+};
+
+export const extractBodyContent = (content: string) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(content, 'text/html');
+  const bodyContent = doc.body.innerHTML;
+
+  return bodyContent;
 };

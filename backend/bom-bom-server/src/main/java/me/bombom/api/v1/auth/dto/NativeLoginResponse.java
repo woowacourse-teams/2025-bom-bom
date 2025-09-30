ebd8752fea4fdf51da5f7faf4740b23862487cdb
ChoiDongjun <adong0808@naver.com>
2025-09-30 18:11:58 +0900

    fix: isRegistered 필수 값으로

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/auth/dto/NativeLoginResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/auth/dto/NativeLoginResponse.java
@@ -1,6 +1,10 @@
 package me.bombom.api.v1.auth.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record NativeLoginResponse(
+
+        @NotNull
         boolean isRegistered,
         String email,
         String password

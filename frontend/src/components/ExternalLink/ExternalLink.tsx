588572fd3170f010c2ceb50056e93fc5a26e390b
guesung <gueit214@naver.com>
2025-10-01 17:14:40 +0900

    feat: 외부 링크를 여는 ExternalLink 컴포넌트

---
+++ frontend/src/components/ExternalLink/ExternalLink.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { sendMessageToRN, isWebView } from '@/libs/webview/webview.utils';
+import type { ReactNode } from 'react';
+
+interface ExternalLinkProps {
+  href: string;
+  children: ReactNode;
+  className?: string;
+}
+
+export const ExternalLink = ({
+  href,
+  children,
+  className,
+}: ExternalLinkProps) => {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (isWebView()) {
+      e.preventDefault();
+      sendMessageToRN({
+        type: 'OPEN_BROWSER',
+        payload: { url: href },
+      });
+    }
+  };
+
+  return (
+    <a href={href} onClick={handleClick} className={className}>
+      {children}
+    </a>
+  );
+};

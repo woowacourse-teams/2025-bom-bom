3728bbca92b66a7fc61fa832eb131d3a8d70fd6f
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-10-02 11:18:03 +0900

    [FE][BOM-532] fix: 안드로이드 빌드 오류 해결 (#418)
    * fix: network security config 설정 삭제
    
    * refactor: wake awake 활성화

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,12 +1,24 @@
+import { activateKeepAwakeAsync, deactivateKeepAwake } from 'expo-keep-awake';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Slot } from 'expo-router';
+import { Platform } from 'react-native';
 import { AuthProvider } from '../contexts/AuthContext';
 import { EmotionThemeProvider } from '../contexts/ThemeContext';
 import { WebViewProvider } from '../contexts/WebViewContext';
 
 export default function RootLayout() {
+  useEffect(() => {
+    if (Platform.OS === 'web') return;
+
+    activateKeepAwakeAsync();
+
+    return () => {
+      deactivateKeepAwake();
+    };
+  }, []);
+
   return (
     <EmotionThemeProvider>
       <WebViewProvider>

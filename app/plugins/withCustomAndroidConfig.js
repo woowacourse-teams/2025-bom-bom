09a92f4c2e5109e4b799869fbcefdd9ed887f89e
guesung <gueit214@naver.com>
2025-10-03 17:03:08 +0900

    chore: withCustomAndroidConfig 복구

--- app/plugins/withCustomAndroidConfig.js
+++ app/plugins/withCustomAndroidConfig.js
@@ -1,21 +1,16 @@
-import { withAndroidManifest } from '@expo/config-plugins';
-import { ConfigContext, ExpoConfig } from 'expo/config';
+const { withAndroidManifest } = require('@expo/config-plugins');
 
 /**
  * AndroidManifest.xml 수정 플러그인
  * - application 태그에 usesCleartextTraffic, networkSecurityConfig 속성 주입
  */
-function withCustomAndroidConfig(config: ConfigContext) {
+function withCustomAndroidConfig(config) {
   // withAndroidManifest: Expo가 AndroidManifest.xml을 파싱한 결과(modResults)를 수정할 수 있게 해주는 래퍼
   // 콜백에서 수정된 config를 반환하면, prebuild 시점에 해당 변경이 반영됨
-  return withAndroidManifest(config.config as ExpoConfig, (config) => {
+  return withAndroidManifest(config, (config) => {
     // AndroidManifest.xml의 최상위 <manifest> 안에 있는 <application> 배열을 가져옴
     // 보통 하나만 존재하므로 [0]으로 접근
-    const app = config.modResults.manifest.application?.[0];
-
-    if (!app) {
-      return config;
-    }
+    const app = config.modResults.manifest.application[0];
 
     // Application 태그의 속성 객체(app.$)에 키/값으로 원하는 속성들을 주입
     // 'android:usesCleartextTraffic' = 'true' : HTTP(클리어텍스트) 트래픽을 허용
@@ -26,4 +21,4 @@ function withCustomAndroidConfig(config: ConfigContext) {
   });
 }
 
-export default withCustomAndroidConfig;
+module.exports = withCustomAndroidConfig;

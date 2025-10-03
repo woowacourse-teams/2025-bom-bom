819337792007f2dc892978de528ef4612ad4bdcd
guesung <gueit214@naver.com>
2025-10-03 11:03:53 +0900

    chore: 앱 버전을 체크해서, 최신이라면 인앱브라우저 열도록 수정

--- app/constants/webview.ts
+++ app/constants/webview.ts
@@ -1,2 +1,1 @@
-export const WEBVIEW_USER_AGENT =
-  'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/96.0.4664.116 Mobile/15E148 Safari/604.1';
+export const WEBVIEW_USER_AGENT = 'bombom/1.0.2';

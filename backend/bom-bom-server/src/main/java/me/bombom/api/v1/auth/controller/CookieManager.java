193df31b372dd1f8001bbc713058932d9c11210c
ChoiDongjun <adong0808@naver.com>
2025-10-06 22:37:40 +0900

    feat: CookieManager로 쿠키 관리 클래스 추가

---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/auth/controller/CookieManager.java
@@ -0,0 +1,26 @@
+package me.bombom.api.v1.auth.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * 추후에 JWT로 변경 시를 대비해 미리 추가
+ */
+@Component
+public class CookieManager {
+
+    public void delete(HttpServletResponse response, String name) {
+        Cookie cookie = new Cookie(name, "");
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+
+        response.addCookie(cookie);
+        response.addHeader(
+                "Set-Cookie",
+                name + "=; Max-Age=0; Path=/; Secure; HttpOnly; SameSite=None"
+        );
+    }
+}

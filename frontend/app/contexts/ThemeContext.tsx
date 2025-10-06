a401175c51b4dc6d31c869ea4ba9a14166dbe7f6
guesung <gueit214@naver.com>
2025-10-06 17:41:34 +0900

    fix: fix the import

--- frontend/app/contexts/ThemeContext.tsx
+++ frontend/app/contexts/ThemeContext.tsx
@@ -1,6 +1,6 @@
+import { theme } from '@bom-bom/shared';
 import { ThemeProvider } from '@emotion/react';
 import React, { PropsWithChildren } from 'react';
-import { theme } from '../styles/theme';
 
 export const EmotionThemeProvider = ({ children }: PropsWithChildren) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;

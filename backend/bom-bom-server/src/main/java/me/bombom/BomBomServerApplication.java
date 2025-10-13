19cef2ba5ab39e40a1b471d25beb1c41638fd3f2
ryan-dia <cjfdnjs158@naver.com>
2025-10-13 21:13:47 +0900

    feat: 애플리케이션 내 비동기 활성화

--- backend/bom-bom-server/src/main/java/me/bombom/BomBomServerApplication.java
+++ backend/bom-bom-server/src/main/java/me/bombom/BomBomServerApplication.java
@@ -4,8 +4,10 @@ import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication

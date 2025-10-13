a31a9d9142ccd302d5645753f64070685920c5b7
ChoiDongjun <adong0808@naver.com>
2025-10-13 21:02:35 +0900

    refactor: 사용하지 않는 메서드 삭제

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/dto/response/PreviousArticleDetailResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/dto/response/PreviousArticleDetailResponse.java
@@ -3,9 +3,6 @@ package me.bombom.api.v1.article.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import me.bombom.api.v1.article.domain.Article;
-import me.bombom.api.v1.newsletter.domain.Category;
-import me.bombom.api.v1.newsletter.domain.Newsletter;
 import me.bombom.api.v1.newsletter.dto.NewsletterBasicResponse;
 
 public record PreviousArticleDetailResponse(
@@ -25,14 +22,4 @@ public record PreviousArticleDetailResponse(
         @NotNull
         NewsletterBasicResponse newsletter
 ) {
-
-    public static PreviousArticleDetailResponse of(Article article, Newsletter newsletter, Category category) {
-        return new PreviousArticleDetailResponse(
-                article.getTitle(),
-                article.getContents(),
-                article.getArrivedDateTime(),
-                article.getExpectedReadTime(),
-                NewsletterBasicResponse.of(newsletter, category)
-        );
-    }
 }

7aa2e696075240e9dbbbaaa15a75b04f69b1b9ee
ryan-dia <cjfdnjs158@naver.com>
2025-10-13 21:12:52 +0900

    feat: webclient 보일러플레이트 코드 추가

---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/config/WebClientConfig.java
@@ -0,0 +1,14 @@
+package me.bombom.api.v1.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.build();
+    }
+}

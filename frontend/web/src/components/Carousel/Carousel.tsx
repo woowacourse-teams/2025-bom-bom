b5480cdc24989e5ede48756ce223507f065a2c61
Jeongeun Lee <leeju1009@gmail.com>
2025-10-15 14:04:31 +0900

    chore: 서버에서 데이터를 받아오는 경우를 고려하여, Carousel의 에러 throw 로직 제거

--- frontend/web/src/components/Carousel/Carousel.tsx
+++ frontend/web/src/components/Carousel/Carousel.tsx
@@ -35,18 +35,8 @@ const Carousel = ({
   const hasMultipleSlides = slideCount > 1;
 
   if (process.env.NODE_ENV === 'development') {
-    if (originSlides.length === 0) {
-      throw new Error(
-        'Carousel 컴포넌트에 최소 한 개 이상의 child가 필요합니다.',
-      );
-    }
-
     if (autoPlay && autoPlaySpeedMs < 100) {
       throw new Error('timer 주기는 100ms 이상이어야 합니다.');
-    }
-
-    if (autoPlay && originSlides.length === 1) {
-      throw new Error('슬라이드가 1개일 경우, 자동 재생을 할 수 없습니다.');
     }
   }
 

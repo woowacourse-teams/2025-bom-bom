76d4b851aafe5d04e354538ce487c362ccb223ad
ryan-dia <cjfdnjs158@naver.com>
2025-10-16 22:57:57 +0900

    feat: fcm 서버에 알림 요청 로직 추가

---
+++ backend/bom-bom-server/src/main/java/me/bombom/fcm/service/FcmService.java
@@ -0,0 +1,26 @@
+package me.bombom.fcm.service;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class FcmService {
+
+    public void sendNotification(String targetToken, String title, String body) throws FirebaseMessagingException {
+        Message message = Message.builder()
+                .setToken(targetToken)
+                .setNotification(Notification.builder()
+                        .setTitle(title)
+                        .setBody(body)
+                        .build())
+                .build();
+
+        String response = FirebaseMessaging.getInstance().send(message);
+        log.debug("✅ FCM 발송 성공: " + response);
+    }
+}

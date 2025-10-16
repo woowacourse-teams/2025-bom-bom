5cb6fcfa0cba0dc9c004f6560e5603ac05c2ca14
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-10-16 10:33:33 +0900

    refactor: 이미지 추가

--- frontend/web/src/pages/recommend/components/NewsletterDetail/PreviousTab.tsx
+++ frontend/web/src/pages/recommend/components/NewsletterDetail/PreviousTab.tsx
@@ -5,6 +5,8 @@ import PreviousArticleListItem from './PreviousArticleListItem';
 import Button from '@/components/Button/Button';
 import { openExternalLink } from '@/utils/externalLink';
 import type { GetPreviousArticlesResponse } from '@/apis/previousArticles';
+import sadBom from '#/assets/avif/sad-bom.avif';
+import subscribeBom from '#/assets/avif/subscribe-bom.avif';
 import OpenIcon from '#/assets/svg/open.svg';
 
 interface PreviousTabProps {
@@ -48,35 +50,44 @@ const PreviousTab = ({
 
   if (previousNewsletterUrl) {
     return (
-      <Container>
-        <Button onClick={openPreviousLetters}>
+      <Container isMobile={isMobile}>
+        <img width={250} src={subscribeBom} alt="empty" />
+        <OpenSubscribeButton onClick={openPreviousLetters}>
           지난 소식 보러가기
-          <OpenIcon fill={theme.colors.white} width={16} height={16} />
-        </Button>
+          <OpenIcon fill={theme.colors.primary} width={16} height={16} />
+        </OpenSubscribeButton>
       </Container>
     );
   }
 
   return (
-    <Container>
-      <EmptyState isMobile={isMobile}>
-        해당 뉴스레터는 지난 뉴스레터가 존재하지 않습니다.
-      </EmptyState>
+    <Container isMobile={isMobile}>
+      <img width={250} src={sadBom} alt="empty" />
+      지난 뉴스레터가 존재하지 않습니다.
     </Container>
   );
 };
 
 export default PreviousTab;
 
-const Container = styled.div`
+const Container = styled.div<{ isMobile: boolean }>`
   padding: 24px 0;
 
   display: flex;
+  gap: 8px;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
-`;
 
-const EmptyState = styled.p<{ isMobile: boolean }>`
   color: ${({ theme }) => theme.colors.textSecondary};
   font: ${({ theme, isMobile }) =>
     isMobile ? theme.fonts.body3 : theme.fonts.body2};
+`;
+
+const OpenSubscribeButton = styled(Button)`
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+
+  background-color: ${({ theme }) => theme.colors.white};
+  color: ${({ theme }) => theme.colors.primary};
+  font: ${({ theme }) => theme.fonts.body2};
 `;

f10f700f175739d0c1fcb2ab1d90f0dbf796bc20
guesung <gueit214@naver.com>
2025-10-16 16:25:57 +0900

    refactor: Divider컴포넌트의 margin props 수정

--- frontend/web/src/components/Divider/Divider.tsx
+++ frontend/web/src/components/Divider/Divider.tsx
@@ -1,19 +1,19 @@
 import styled from '@emotion/styled';
 
 interface DividerProps {
-  margin?: string;
+  margin: number;
 }
 
-const Divider = ({ margin = '8px 0' }: DividerProps) => {
+const Divider = ({ margin = 8 }: DividerProps) => {
   return <StyledDivider margin={margin} />;
 };
 
 export default Divider;
 
-const StyledDivider = styled.div<{ margin: string }>`
+const StyledDivider = styled.div<{ margin: number }>`
   width: 100%;
   height: 1px;
-  margin: ${({ margin }) => margin};
+  margin: ${({ margin }) => margin}px 0;
 
   background-color: ${({ theme }) => theme.colors.dividers};
 `;

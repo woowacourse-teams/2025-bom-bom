b09b116b94143ad64a72f0edfcf72ebe24ffb295
ChoiDongjun <adong0808@naver.com>
2025-10-16 20:00:47 +0900

    fix: category String으로 넘겨주도록 변경

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/newsletter/dto/NewsletterWithDetailResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/newsletter/dto/NewsletterWithDetailResponse.java
@@ -1,6 +1,7 @@
 package me.bombom.api.v1.newsletter.dto;
 
 import jakarta.validation.constraints.NotNull;
+import me.bombom.api.v1.newsletter.domain.Category;
 import me.bombom.api.v1.newsletter.domain.Newsletter;
 import me.bombom.api.v1.newsletter.domain.NewsletterDetail;
 
@@ -16,7 +17,7 @@ public record NewsletterWithDetailResponse(
         String imageUrl,
 
         @NotNull
-        Long categoryId,
+        String category,
 
         @NotNull
         String mainPageUrl,
@@ -31,12 +32,15 @@ public record NewsletterWithDetailResponse(
 
         String subscribeMethod
 ) {
-    public static NewsletterWithDetailResponse of(Newsletter newsletter, NewsletterDetail newsletterDetail) {
+    public static NewsletterWithDetailResponse of(
+            Newsletter newsletter,
+            NewsletterDetail newsletterDetail,
+            Category category) {
         return new NewsletterWithDetailResponse(
                 newsletter.getName(),
                 newsletter.getDescription(),
                 newsletter.getImageUrl(),
-                newsletter.getCategoryId(),
+                category.getName(),
                 newsletterDetail.getMainPageUrl(),
                 newsletterDetail.getSubscribeUrl(),
                 newsletterDetail.getIssueCycle(),

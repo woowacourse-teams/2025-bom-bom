4bfd1777c8a040e0550a1e67dc1a3c2b63050e62
guesung <gueit214@naver.com>
2025-10-17 11:41:19 +0900

    chore: 두께 설정

--- frontend/web/src/components/Divider/Divider.tsx
+++ frontend/web/src/components/Divider/Divider.tsx
@@ -2,17 +2,18 @@ import styled from '@emotion/styled';
 
 interface DividerProps {
   margin?: number;
+  height?: number;
 }
 
-const Divider = ({ margin = 8 }: DividerProps) => {
-  return <StyledDivider margin={margin} />;
+const Divider = ({ margin = 8, height = 1 }: DividerProps) => {
+  return <StyledDivider margin={margin} height={height} />;
 };
 
 export default Divider;
 
-const StyledDivider = styled.div<{ margin: number }>`
+const StyledDivider = styled.div<{ margin: number; height: number }>`
   width: 100%;
-  height: 1px;
+  height: ${({ height }) => height}px;
   margin: ${({ margin }) => margin}px 0;
 
   background-color: ${({ theme }) => theme.colors.dividers};

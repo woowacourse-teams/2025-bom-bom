be405daab5cd18b82b4b6114c9cb31af734740f2
guesung <gueit214@naver.com>
2025-10-18 13:16:41 +0900

    refactor: 불필요한 async 제거

--- frontend/web/src/hooks/useAppInstallPrompt.ts
+++ frontend/web/src/hooks/useAppInstallPrompt.ts
@@ -20,7 +20,7 @@ export function useAppInstallPrompt() {
     setShowModal(true);
   }, [dismissedUntil]);
 
-  const handleInstallClick = async () => {
+  const handleInstallClick = () => {
     downloadApp();
   };
 

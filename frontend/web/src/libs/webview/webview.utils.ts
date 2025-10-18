c412d4a291e921631dd8ec2c2852c28325d0f2d9
guesung <gueit214@naver.com>
2025-10-18 13:16:06 +0900

    fix: 웹뷰인지 체크할 때 `bombom`를 userAgent에서 확인

--- frontend/web/src/libs/webview/webview.utils.ts
+++ frontend/web/src/libs/webview/webview.utils.ts
@@ -5,12 +5,14 @@ import type {
   WindowWithWebkit,
 } from '@bombom/shared/webview';
 
-export const isAndroid = (): boolean => navigator.userAgent.includes('google');
-export const isIOS = (): boolean => navigator.userAgent.includes('Apple');
+export const isAndroid = (): boolean =>
+  navigator.userAgent.includes('bombom') &&
+  navigator.userAgent.includes('google');
+export const isIOS = (): boolean =>
+  navigator.userAgent.includes('bombom') &&
+  navigator.userAgent.includes('Apple');
 
-export const isWebView = (): boolean => {
-  return !!(isAndroid() || isIOS());
-};
+export const isWebView = (): boolean => navigator.userAgent.includes('bombom');
 
 export const isWeb = (): boolean => {
   return !isWebView();

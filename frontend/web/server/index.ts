482ad89d60d765f84620f6f7e96f01355dc664a1
guesung <gueit214@naver.com>
2025-10-19 12:31:47 +0900

    fix: app.use로 수정해서 정적 파일 서빙

--- frontend/web/server/index.ts
+++ frontend/web/server/index.ts
@@ -1,30 +1,21 @@
 import path from 'path';
-// import compression from 'compression';
+import compression from 'compression';
 import express from 'express';
-import { handleSSR } from './render';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 정적 파일 제공
-const distPath = path.join(__dirname, '../../dist/static');
-app.use('/static', express.static(distPath, { index: false }));
+app.use(compression());
 
-// public 폴더 제공
-const publicPath = path.join(__dirname, '../../dist/public');
+const distPath = path.join(__dirname, '../static');
+app.use(express.static(distPath));
+
+const publicPath = path.join(__dirname, '../public');
 app.use('/public', express.static(publicPath));
 
-// SSR이 필요한 경로들
 // const ssrRoutes = ['/'];
 
-// SSR 라우트 핸들러
-// ssrRoutes.forEach((route) => {
-//   app.get(route, handleSSR);
-// });
-
-// 나머지 모든 경로는 SPA fallback (index.html 제공)
-// 이렇게 하면 /my, /bookmark 등 다른 페이지는 CSR로 작동
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 

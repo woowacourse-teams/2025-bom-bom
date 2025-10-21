443c0133693e00ca6a17474b6666025bb6f42720
ryan-dia <cjfdnjs158@naver.com>
2025-10-21 22:41:15 +0900

    chore: 기본 의존성 추가

---
+++ backend/fcm-server/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "news.bombom.fcm"
+version = "0.0.1-SNAPSHOT"
+description = "fcm-server"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // db
+	implementation("mysql:mysql-connector-java:8.0.33")
+
+    // fcm
+	implementation("com.google.firebase:firebase-admin:9.2.0")
+
+    // lombok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+    // test
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}

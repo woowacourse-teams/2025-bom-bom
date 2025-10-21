4b9210a7f80020a6229bd5ec2fd1d75386d1116c
Jeongeun Lee <leeju1009@gmail.com>
2025-10-22 02:22:57 +0900

    refactor: 배너를 반복문을 사용하여 렌더링하도록 코드 개선

--- frontend/web/src/pages/recommend/components/SlideCardList/SlideCardList.tsx
+++ frontend/web/src/pages/recommend/components/SlideCardList/SlideCardList.tsx
@@ -1,25 +1,31 @@
 import styled from '@emotion/styled';
 import Carousel from '@/components/Carousel/Carousel';
 
-const banner1 = '/assets/avif/banner_1.avif';
-const banner2 = '/assets/avif/banner_2.avif';
-const banner3 = '/assets/avif/banner_3.avif';
-const banner4 = '/assets/avif/banner_4.avif';
-
-const BANNER_LABELS = [
-  '디그, 디그와 함께 하세요. 누구보다 쉽게 핵심을 디깅해 드릴게요',
-  '캐릿, 트렌드 당일 배송, 렛츠 캐릿',
-  '데일리 바이트, 놓치면 안 될 비즈니스, 경제 이슈 한 눈에 보기',
-  '부딩, 구독형 부동산 뉴스레터',
+const BANNERS = [
+  {
+    src: '/assets/avif/banner_1.avif',
+    alt: '디그, 디그와 함께 하세요. 누구보다 쉽게 핵심을 디깅해 드릴게요',
+  },
+  {
+    src: '/assets/avif/banner_2.avif',
+    alt: '캐릿, 트렌드 당일 배송, 렛츠 캐릿',
+  },
+  {
+    src: '/assets/avif/banner_3.avif',
+    alt: '데일리 바이트, 놓치면 안 될 비즈니스, 경제 이슈 한 눈에 보기',
+  },
+  {
+    src: '/assets/avif/banner_4.avif',
+    alt: '부딩, 구독형 부동산 뉴스레터',
+  },
 ];
 
 const SlideCardList = () => {
   return (
     <Carousel>
-      <Banner src={banner1} alt={BANNER_LABELS[0]} />
-      <Banner src={banner2} alt={BANNER_LABELS[1]} />
-      <Banner src={banner3} alt={BANNER_LABELS[2]} />
-      <Banner src={banner4} alt={BANNER_LABELS[3]} />
+      {BANNERS.map(({ src, alt }, index) => (
+        <Banner key={`banner-${index}`} src={src} alt={alt} tabIndex={0} />
+      ))}
     </Carousel>
   );
 };

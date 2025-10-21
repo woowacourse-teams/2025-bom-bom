23590628be8d3a9226ca0a30ece7c437cb701163
ryan-dia <cjfdnjs158@naver.com>
2025-10-21 20:41:33 +0900

    feat: 사용하지 않는 클래스 삭제

--- backend/mail-server/src/main/java/news/bombomemail/email/notification/NotificationPublisher.java
+++
@@ -1,8 +0,0 @@
-package news.bombomemail.email.notification;
-
-import news.bombomemail.email.dto.NotificationMessage;
-
-public interface NotificationPublisher {
-
-    void publish(Object body);
-}

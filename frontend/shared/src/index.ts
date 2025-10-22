2a04b998d45f816fa0d0452b82da00f7c1460c23
Jeongeun Lee <leeju1009@gmail.com>
2025-10-22 22:44:41 +0900

    refactor: app api fetcher에도 공통적으로 사용되는 파일들을 shared 리소스로 변경

--- frontend/shared/src/index.ts
+++ frontend/shared/src/index.ts
@@ -2,3 +2,5 @@ export * from './theme';
 export * from './app';
 export type * from './webview';
 export type * from './fetcher';
+export { default as ApiError } from './ApiError';
+export * from './defaultErrorMessage';

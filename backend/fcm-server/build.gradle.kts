ab40798e8f754251a26ffec4f18fb2ef06241fb2
ryan-dia <cjfdnjs158@naver.com>
2025-10-25 14:50:51 +0900

    chore: 의존성 분류

--- backend/fcm-server/build.gradle.kts
+++ backend/fcm-server/build.gradle.kts
@@ -28,11 +28,19 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // db
 	implementation("mysql:mysql-connector-java:8.0.33")
+    runtimeOnly("com.h2database:h2")
+
+    // fcm
 	implementation("com.google.firebase:firebase-admin:9.2.0")
-	runtimeOnly("com.h2database:h2")
+
+    // lombok
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+
+    // test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testCompileOnly("org.projectlombok:lombok")
 	testAnnotationProcessor("org.projectlombok:lombok")

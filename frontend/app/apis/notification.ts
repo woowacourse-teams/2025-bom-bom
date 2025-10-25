0d301de55e87d486fae7bc51164c28ec0b9799b0
Jeongeun Lee <leeju1009@gmail.com>
2025-10-26 01:45:41 +0900

    refactor: 백엔드 api 명세에 맞게 수정

---
+++ frontend/app/apis/notification.ts
@@ -0,0 +1,60 @@
+import { fetcher } from './fetcher';
+
+interface PostTokenParams {
+  memberId: string;
+  deviceUuid: string;
+  token: string;
+}
+
+interface PutTokenParams {
+  memberId: string;
+  deviceUuid: string;
+  token: string;
+}
+
+interface PutNotificationSettingsParams {
+  memberId: string;
+  deviceUuid: string;
+  enabled: boolean;
+}
+
+export const postFCMToken = ({
+  memberId,
+  deviceUuid,
+  token,
+}: PostTokenParams) => {
+  return fetcher.post({
+    path: '/notifications/tokens',
+    body: {
+      memberId,
+      deviceUuid,
+      token,
+    },
+  });
+};
+
+export const putFCMToken = ({
+  memberId,
+  deviceUuid,
+  token,
+}: PutTokenParams) => {
+  return fetcher.put({
+    path: '/notifications/tokens',
+    body: {
+      memberId,
+      deviceUuid,
+      token,
+    },
+  });
+};
+
+export const putNotificationSettings = ({
+  memberId,
+  deviceUuid,
+  enabled,
+}: PutNotificationSettingsParams) => {
+  return fetcher.put({
+    path: `/notifications/tokens/${memberId}/${deviceUuid}/settings`,
+    body: { enabled },
+  });
+};

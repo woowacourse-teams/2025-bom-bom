7782b05b7d9bb51f9619635bf5162b05d755409c
ryan-dia <cjfdnjs158@naver.com>
2025-10-27 03:25:40 +0900

    style: 개행 추가

--- backend/mail-server/src/main/java/news/bombomemail/email/event/ArticleArrivedEvent.java
+++ backend/mail-server/src/main/java/news/bombomemail/email/event/ArticleArrivedEvent.java
@@ -5,6 +5,7 @@ public record ArticleArrivedEvent(
         String newsletterName,
         String articleTitle
 ) {
+
     public static ArticleArrivedEvent of(Long memberId, String newsletterName, String articleTitle) {
         return new ArticleArrivedEvent(memberId, newsletterName, articleTitle);
     }

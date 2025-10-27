872ade4afffdb946662ddf71f023ad71bf083830
ryan-dia <cjfdnjs158@naver.com>
2025-10-27 17:47:51 +0900

    style: 컨벤션에 따라 어노테이션 순서 변경

--- backend/fcm-server/src/main/java/news/bombom/notification/NotificationServerApplication.java
+++ backend/fcm-server/src/main/java/news/bombom/notification/NotificationServerApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
+@SpringBootApplication
 public class NotificationServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServerApplication.class, args);
+    }
 
 }

36d37b257e5250235319b3300a7afa4d26a372d0
Jeongeun Lee <leeju1009@gmail.com>
2025-10-28 12:12:50 +0900

    fix: FCM api 요청 base url 변경

--- frontend/app/constants/env.ts
+++ frontend/app/constants/env.ts
@@ -5,4 +5,10 @@ export const ENV = {
 
   // Web URLs
   webUrl: process.env.EXPO_PUBLIC_WEB_URL ?? '',
+
+  // API
+  baseUrl: process.env.EXPO_PUBLIC_API_BASE_URL,
+
+  // FCM API
+  baseUrlFCM: process.env.EXPO_PUBLIC_API_FCM_BASE_URL,
 } as const;

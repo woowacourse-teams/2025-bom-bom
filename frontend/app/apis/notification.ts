fe17dceb201d85875ee9e0a937229ee7f441185c
Jeongeun Lee <leeju1009@gmail.com>
2025-11-01 17:24:12 +0900

    chore: 불필요한 api 제거

--- frontend/app/apis/notification.ts
+++ frontend/app/apis/notification.ts
@@ -6,33 +6,12 @@ interface PostTokenParams {
   token: string;
 }
 
-interface PutTokenParams {
-  memberId: number;
-  deviceUuid: string;
-  token: string;
-}
-
 export const postFCMToken = ({
   memberId,
   deviceUuid,
   token,
 }: PostTokenParams) => {
   return fetcher.post({
-    path: '/notifications/tokens',
-    body: {
-      memberId,
-      deviceUuid,
-      token,
-    },
-  });
-};
-
-export const putFCMToken = ({
-  memberId,
-  deviceUuid,
-  token,
-}: PutTokenParams) => {
-  return fetcher.put({
     path: '/notifications/tokens',
     body: {
       memberId,

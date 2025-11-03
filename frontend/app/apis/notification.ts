ef7dcd317102f19d818fb1884911834aa652a042
Jeongeun Lee <leeju1009@gmail.com>
2025-11-03 14:43:59 +0900

    refactor: FCM 토큰 요청 메서드를 put으로 수정

--- frontend/app/apis/notification.ts
+++ frontend/app/apis/notification.ts
@@ -1,31 +1,10 @@
 import { fetcher } from '@bombom/shared/apis';
 
-interface PostTokenParams {
-  memberId: number;
-  deviceUuid: string;
-  token: string;
-}
-
 interface PutTokenParams {
   memberId: number;
   deviceUuid: string;
   token: string;
 }
-
-export const postFCMToken = ({
-  memberId,
-  deviceUuid,
-  token,
-}: PostTokenParams) => {
-  return fetcher.post({
-    path: '/notifications/tokens',
-    body: {
-      memberId,
-      deviceUuid,
-      token,
-    },
-  });
-};
 
 export const putFCMToken = ({
   memberId,

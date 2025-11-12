9ad3873c7a0b0ae9a9d9e1a26ea5c6e6188cadef
ryan-dia <cjfdnjs158@naver.com>
2025-11-12 17:35:23 +0900

    test: 클래스 이름과 매개변수 추가에 따른 수정

--- backend/mail-server/src/test/java/news/bombomemail/reading/event/TodayReadingListenerTest.java
+++ backend/mail-server/src/test/java/news/bombomemail/reading/event/TodayReadingListenerTest.java
@@ -1,5 +1,6 @@
 package news.bombomemail.reading.event;
 
+import news.bombomemail.article.event.ArticleArrivedEvent;
 import news.bombomemail.reading.service.TodayReadingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,10 +26,16 @@ class TodayReadingListenerTest {
     @Transactional
     void afterCommit_이벤트_발행후_서비스_호출() {
         // given
+        Long newsletterId = 1L;
+        String newsletterName = "테스트 뉴스레터";
+        Long articleId = 1L;
+        String articleTitle = "테스트 아티클";
         Long memberId = 1L;
+        String unsubscribeUrl = "unsubscribeUrl";
 
         // when
-        eventPublisher.publishEvent(TodayReadingEvent.from(memberId));
+        eventPublisher.publishEvent(ArticleArrivedEvent.of(
+                newsletterId, newsletterName, articleId, articleTitle, memberId, unsubscribeUrl));
 
         TestTransaction.flagForCommit();
         TestTransaction.end();

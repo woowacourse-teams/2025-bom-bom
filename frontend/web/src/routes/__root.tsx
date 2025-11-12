b36c7e9821b6e71671e909a931c148b3a0a7467d
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-11-13 00:17:20 +0900

    feat: 점검중 페이지 리다이렉트 로직 복구

--- frontend/web/src/routes/__root.tsx
+++ frontend/web/src/routes/__root.tsx
@@ -5,14 +5,16 @@ import {
   createRootRouteWithContext,
   HeadContent,
   Outlet,
+  redirect,
 } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { ENV } from '@/apis/env';
 import Toast from '@/components/Toast/Toast';
+import { SERVER_STATUS } from '@/constants/serverStatus';
 import usePageTracking from '@/libs/googleAnalytics/usePageTracking';
 import { useWebViewAuth } from '@/libs/webview/useWebViewAuth';
 import { queryClient } from '@/main';
 import type { QueryClient } from '@tanstack/react-query';
-import type { redirect } from '@tanstack/react-router';
 
 interface BomBomRouterContext {
   queryClient: QueryClient;
@@ -42,7 +44,36 @@ export const Route = createRootRouteWithContext<BomBomRouterContext>()({
       <RootComponent />
     </>
   ),
-  beforeLoad: async (): Promise<void | ReturnType<typeof redirect>> => {
+  beforeLoad: async ({
+    location,
+  }): Promise<void | ReturnType<typeof redirect>> => {
+    const maintenancePath = '/maintenance';
+
+    try {
+      const res = await fetch(ENV.monitoringStatusUrl, {
+        cache: 'no-store',
+      });
+
+      if (!res.ok) {
+        throw new Error(`Failed to fetch status: ${res.status}`);
+      }
+
+      const serverStatus = await res.json();
+      const status =
+        serverStatus?.data?.result?.[0]?.value[1] ?? SERVER_STATUS.off;
+
+      const isServerOn = status === SERVER_STATUS.on;
+
+      if (!isServerOn && location.pathname !== maintenancePath) {
+        return redirect({ to: maintenancePath });
+      }
+    } catch (err) {
+      console.error('Server status check failed:', err);
+      if (location.pathname !== maintenancePath) {
+        return redirect({ to: maintenancePath });
+      }
+    }
+
     return;
   },
 });

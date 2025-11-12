f9de0e68d6e875721a1e342478ce6bdfb9377636
ryan-dia <cjfdnjs158@naver.com>
2025-11-12 17:37:49 +0900

    style: 첫 줄 개행

--- backend/mail-server/src/main/java/news/bombomemail/article/event/ArticleArrivedEvent.java
+++ backend/mail-server/src/main/java/news/bombomemail/article/event/ArticleArrivedEvent.java
@@ -8,12 +8,13 @@ public record ArticleArrivedEvent(
         Long memberId,
         String unsubscribeUrl
 ) {
-    public static ArticleArrivedEvent of(Long newsletterId,
-                                         String newsletterName,
-                                         Long articleId,
-                                         String articleTitle,
-                                         Long memberId,
-                                         String unsubscribeUrl
+    public static ArticleArrivedEvent of(
+            Long newsletterId,
+            String newsletterName,
+            Long articleId,
+            String articleTitle,
+            Long memberId,
+            String unsubscribeUrl
     ) {
         return new ArticleArrivedEvent(newsletterId, newsletterName, articleId, articleTitle, memberId, unsubscribeUrl);
     }

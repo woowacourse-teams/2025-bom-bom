0e4476253e7240fd0ee958b96cc821ee11fb1341
ryan-dia <cjfdnjs158@naver.com>
2025-11-13 22:23:15 +0900

    hotfix: enableJpaAuditing 애너테이션 추가

--- backend/mail-server/src/main/java/news/bombomemail/BomBomEmailServerApplication.java
+++ backend/mail-server/src/main/java/news/bombomemail/BomBomEmailServerApplication.java
@@ -2,7 +2,9 @@ package news.bombomemail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BomBomEmailServerApplication {
 

c48a17219870844c09cf6bcfc8d545de8c1657bb
geonwoo <kysub99@naver.com>
2025-11-16 18:40:28 +0900

    refactor: subscribes 대신 subscriptions 사용

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/controller/SubscribeController.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/controller/SubscribeController.java
@@ -4,14 +4,13 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.bombom.api.v1.common.resolver.LoginMember;
 import me.bombom.api.v1.member.domain.Member;
+import me.bombom.api.v1.subscribe.dto.UnsubscribeResponse;
 import me.bombom.api.v1.subscribe.dto.SubscribedNewsletterResponse;
 import me.bombom.api.v1.subscribe.service.SubscribeService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,18 +21,17 @@ public class SubscribeController implements SubscribeControllerApi {
     private final SubscribeService subscribeService;
 
     @Override
-    @GetMapping("/me/subscribes")
+    @GetMapping("/me/subscriptions")
     public List<SubscribedNewsletterResponse> getSubscribedNewsletters(@LoginMember Member member) {
         return subscribeService.getSubscribedNewsletters(member);
     }
 
     @Override
-    @DeleteMapping("/me/subscribes/{subscribeId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void cancelSubscription(
+    @PostMapping("/me/subscriptions/{subscriptionId}/unsubscribe")
+    public UnsubscribeResponse unsubscribe(
         @LoginMember Member member,
-        @PathVariable Long subscribeId
+        @PathVariable Long subscriptionId
     ) {
-        subscribeService.cancelSubscription(member.getId(), subscribeId);
+        return subscribeService.unsubscribe(member.getId(), subscriptionId);
     }
 }
--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/controller/SubscribeControllerApi.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/controller/SubscribeControllerApi.java
@@ -6,9 +6,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import me.bombom.api.v1.member.domain.Member;
+import me.bombom.api.v1.subscribe.dto.UnsubscribeResponse;
 import me.bombom.api.v1.subscribe.dto.SubscribedNewsletterResponse;
 
-@Tag(name = "Subscribe", description = "구독 관련 API")
+@Tag(name = "Subscription", description = "구독 관련 API")
 public interface SubscribeControllerApi {
 
     @Operation(summary = "구독한 뉴스레터 목록 조회", description = "현재 로그인한 사용자가 구독한 뉴스레터 목록을 조회합니다.")
@@ -18,10 +19,10 @@ public interface SubscribeControllerApi {
     })
     List<SubscribedNewsletterResponse> getSubscribedNewsletters(Member member);
 
-    @Operation(summary = "뉴스레터 구독 취소", description = "뉴스레터를 구독을 취소합니다.")
+    @Operation(summary = "뉴스레터 구독 취소", description = "뉴스레터 구독을 취소합니다. 구독 리스트에서 삭제하고 unsubscribeUrl이 존재하는 경우 반환됩니다.")
     @ApiResponses({
-        @ApiResponse(responseCode = "204", description = "구독 취소 성공"),
+        @ApiResponse(responseCode = "200", description = "구독 취소 성공"),
         @ApiResponse(responseCode = "401", description = "인증 실패")
     })
-    void cancelSubscription(Member member, Long subscribeId);
+    UnsubscribeResponse unsubscribe(Member member, Long subscriptionId);
 }

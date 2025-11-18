b2a2ce13b7521e5b67e36b6c6975021f7fa25918
Jeongeun Lee <leeju1009@gmail.com>
2025-11-18 21:31:17 +0900

    feat: 구독 취소 확인 모달 컨텐츠 생성

---
+++ frontend/web/src/pages/MyPage/NewsletterUnsubscribeConfirmation.tsx
@@ -0,0 +1,89 @@
+import styled from '@emotion/styled';
+import Button from '@/components/Button/Button';
+import { useDevice } from '@/hooks/useDevice';
+
+interface NewsletterUnsubscribeModalProps {
+  onClose: () => void;
+  onUnsubscribe: () => void;
+}
+
+const NewsletterUnsubscribeConfirmation = ({
+  onClose,
+  onUnsubscribe,
+}: NewsletterUnsubscribeModalProps) => {
+  const device = useDevice();
+  const isMobile = device === 'mobile';
+
+  return (
+    <Container isMobile={isMobile}>
+      <Title isMobile={isMobile}>정말 구독을 취소하시겠습니까?</Title>
+
+      <UnsubscribeDescription isMobile={isMobile}>
+        구독을 취소하면 해당 뉴스레터의{'\n'}새로운 소식을 받아볼 수 없습니다.
+      </UnsubscribeDescription>
+
+      <ModalButtonWrapper>
+        <ModalButton
+          isMobile={isMobile}
+          variant="outlined"
+          text="네, 취소할래요"
+          onClick={() => {
+            onUnsubscribe();
+            onClose();
+          }}
+        />
+        <ModalButton
+          isMobile={isMobile}
+          text="아니요, 유지할래요"
+          onClick={onClose}
+        />
+      </ModalButtonWrapper>
+    </Container>
+  );
+};
+
+export default NewsletterUnsubscribeConfirmation;
+
+const Container = styled.div<{ isMobile: boolean }>`
+  width: 100%;
+
+  display: flex;
+  gap: ${({ isMobile }) => (isMobile ? '12px' : '20px')};
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  text-align: center;
+`;
+
+const Title = styled.h2<{ isMobile: boolean }>`
+  color: ${({ theme }) => theme.colors.textPrimary};
+  font: ${({ theme, isMobile }) =>
+    isMobile ? theme.fonts.heading5 : theme.fonts.heading4};
+`;
+
+const UnsubscribeDescription = styled.p<{ isMobile: boolean }>`
+  color: ${({ theme }) => theme.colors.textSecondary};
+  font: ${({ theme, isMobile }) =>
+    isMobile ? theme.fonts.body3 : theme.fonts.body2};
+  white-space: pre-wrap;
+`;
+
+const ModalButtonWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  gap: 12px;
+  justify-content: center;
+`;
+
+const ModalButton = styled(Button)<{ isMobile: boolean }>`
+  height: 48px;
+  min-width: ${({ isMobile }) => (isMobile ? '120px' : '160px')};
+  border-radius: 8px;
+
+  font: ${({ theme, isMobile }) =>
+    isMobile ? theme.fonts.body3 : theme.fonts.body2};
+
+  word-break: keep-all;
+`;

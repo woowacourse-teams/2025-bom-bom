1735c9934fdf219bee64c359e4bef066e4c299eb
geonwoo <kysub99@naver.com>
2025-11-18 16:24:33 +0900

    docs: Schema 어노테이션 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/dto/UnsubscribeResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/dto/UnsubscribeResponse.java
@@ -1,7 +1,12 @@
 package me.bombom.api.v1.subscribe.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UnsubscribeResponse(
+
     String unsubscribeUrl,
+
+    @Schema(required = true)
     boolean hasUnsubscribeUrl
 ) {
 

03a97ec70c2d8b9f23cd4722838be9c9089d42ec
ChoiDongjun <adong0808@naver.com>
2025-11-18 17:52:04 +0900

    feat: 소문자로도 입력가능하도록 구현

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/controller/PreviousArticleController.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/article/controller/PreviousArticleController.java
@@ -2,6 +2,7 @@ package me.bombom.api.v1.article.controller;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
+import java.beans.PropertyEditorSupport;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.bombom.api.v1.article.dto.request.PreviousArticleRequest;
@@ -12,10 +13,13 @@ import me.bombom.api.v1.common.resolver.LoginMember;
 import me.bombom.api.v1.member.domain.Member;
 import me.bombom.api.v1.newsletter.domain.PreviousArticleSource;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
@@ -42,5 +46,15 @@ public class PreviousArticleController implements PreviousArticleControllerApi {
             @RequestParam PreviousArticleSource source
             ) {
         return previousArticleService.getPreviousArticleDetail(id, source, member);
+    }
+
+    @InitBinder
+    private void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(PreviousArticleSource.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                setValue(PreviousArticleSource.valueOf(text.toUpperCase()));
+            }
+        });
     }
 }

16bb91a74d371ffc558cd24392e51504a29eff39
ryan-dia <cjfdnjs158@naver.com>
2025-12-08 23:53:29 +0900

    feat: contentsText를 위해 htmlTagCleaner 적용

--- backend/mail-server/src/main/java/news/bombomemail/article/service/RecentArticleService.java
+++ backend/mail-server/src/main/java/news/bombomemail/article/service/RecentArticleService.java
@@ -10,11 +10,10 @@ import news.bombomemail.article.domain.RecentArticle;
 import news.bombomemail.article.repository.RecentArticleRepository;
 import news.bombomemail.article.util.ReadingTimeCalculator;
 import news.bombomemail.article.util.SummaryGenerator;
-import org.jsoup.Jsoup;
+import news.bombomemail.article.util.html.HtmlTagCleaner;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
 @Slf4j
 @Service
@@ -22,6 +21,7 @@ import org.springframework.util.StringUtils;
 @Transactional(readOnly = true)
 public class RecentArticleService {
 
+    private final HtmlTagCleaner htmlTagCleaner;
     private final RecentArticleRepository recentArticleRepository;
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
@@ -36,23 +36,15 @@ public class RecentArticleService {
             Long memberId,
             Long newsletterId
     ) throws MessagingException {
-        String contentsText = extractTextFromHtml(contents);
         return RecentArticle.builder()
                 .title(message.getSubject())
                 .contents(contents)
-                .contentsText(contentsText)
+                .contentsText(htmlTagCleaner.clean(contents))
                 .expectedReadTime(ReadingTimeCalculator.calculate(contents))
                 .contentsSummary(SummaryGenerator.summarize(contents))
                 .memberId(memberId)
                 .newsletterId(newsletterId)
                 .arrivedDateTime(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
                 .build();
-    }
-
-    private String extractTextFromHtml(String htmlText) {
-        if (!StringUtils.hasText(htmlText)) {
-            return "";
-        }
-        return Jsoup.parse(htmlText).text().strip();
     }
 }

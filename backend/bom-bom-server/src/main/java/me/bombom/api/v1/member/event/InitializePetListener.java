a56ce1275e237b36a6410445d3ac0ad4cb9e8674
rladmstn <oz33177351@gmail.com>
2025-12-11 15:29:32 +0900

    refactor: 회원가입 시 읽기 지표 및 펫 초기화 리스너 분리

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/member/event/InitializePetListener.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/member/event/InitializePetListener.java
@@ -3,25 +3,22 @@ package me.bombom.api.v1.member.event;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.bombom.api.v1.pet.service.PetService;
-import me.bombom.api.v1.reading.service.ReadingService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class MemberSignupListener {
+public class InitializePetListener {
 
-    private final ReadingService readingService;
     private final PetService petService;
 
     @TransactionalEventListener
     public void on(MemberSignupEvent event) {
         try {
-            readingService.initializeReadingInformation(event.memberId());
             petService.createPet(event.memberId());
         } catch (Exception e) {
-            log.error("읽기 정보 초기화에 실패했습니다.");
+            log.error("펫 초기화에 실패했습니다.");
         }
     }
 }

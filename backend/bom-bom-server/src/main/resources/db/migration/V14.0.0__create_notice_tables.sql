de6ebec705a5257d97ca5cd58b1d865f40617241
rladmstn <oz33177351@gmail.com>
2025-12-15 01:33:36 +0900

    fix: flyway sql 오류 수정

--- backend/bom-bom-server/src/main/resources/db/migration/V14.0.0__create_notice_tables.sql
+++ backend/bom-bom-server/src/main/resources/db/migration/V14.0.0__create_notice_tables.sql
@@ -1,17 +1,17 @@
 -- Notice 카테고리 및 공지 테이블 생성
-CREATE TABLE `notice_category` (
+CREATE TABLE notice_category (
     id bigint NOT NULL AUTO_INCREMENT,
     name varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_notice_category_name` (`name`)
 );
 
-CREATE TABLE `notice` (
+CREATE TABLE notice (
     id bigint NOT NULL AUTO_INCREMENT,
     title varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
     content mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
     notice_category_id bigint NOT NULL,
     created_at datetime(6) DEFAULT NULL,
     updated_at datetime(6) DEFAULT NULL,
-    PRIMARY KEY (`id`),
+    PRIMARY KEY (`id`)
 );

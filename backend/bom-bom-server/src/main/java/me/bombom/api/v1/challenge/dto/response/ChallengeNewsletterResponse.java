311566653e6515caf24ec5d71d99d1bb6fea03e9
geonwoo <kysub99@naver.com>
2025-12-29 08:20:45 +0900

    chore: 미사용 import 제거

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/dto/response/ChallengeNewsletterResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/dto/response/ChallengeNewsletterResponse.java
@@ -1,6 +1,5 @@
 package me.bombom.api.v1.challenge.dto.response;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import me.bombom.api.v1.newsletter.domain.Newsletter;
 

6c13e50a253439b7849d9bdb87929ea51d0db446
ChoiDongjun <adong0808@naver.com>
2025-12-30 21:51:34 +0900

    feat: ChallengeProgressController 검증 애노테이션 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/controller/ChallengeProgressController.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/controller/ChallengeProgressController.java
@@ -6,11 +6,13 @@ import me.bombom.api.v1.challenge.dto.MemberChallengeProgressResponse;
 import me.bombom.api.v1.challenge.service.ChallengeProgressService;
 import me.bombom.api.v1.common.resolver.LoginMember;
 import me.bombom.api.v1.member.domain.Member;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/challenges")
@@ -21,8 +23,7 @@ public class ChallengeProgressController {
     @GetMapping("/{id}/progress/me")
     public MemberChallengeProgressResponse getMemberProgress(
             @LoginMember Member member,
-            @PathVariable @Positive(message = "id는 1 이상의 값이어야 합니다.") Long id
-    ) {
+            @PathVariable @Positive(message = "id는 1 이상의 값이어야 합니다.") Long id) {
         return challengeProgressService.getMemberProgress(id, member);
     }
 }

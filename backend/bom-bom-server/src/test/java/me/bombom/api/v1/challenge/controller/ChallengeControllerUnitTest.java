8b6e506bc0aa3e2afff85369d1ba027d59abf003
ChoiDongjun <adong0808@naver.com>
2025-12-30 13:20:38 +0900

    style: 테스트 클래스명 수정

--- backend/bom-bom-server/src/test/java/me/bombom/api/v1/challenge/controller/ChallengeControllerUnitTest.java
+++ backend/bom-bom-server/src/test/java/me/bombom/api/v1/challenge/controller/ChallengeControllerUnitTest.java
@@ -19,7 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @WithMockUser
 @WebMvcTest(ChallengeController.class)
-class ChallengeControllerTest {
+class ChallengeControllerUnitTest {
 
     @Autowired
     private MockMvc mockMvc;

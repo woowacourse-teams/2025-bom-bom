19294e7470882a26d137da9c0caa85a01a11bef0
geonwoo <kysub99@naver.com>
2025-12-31 22:35:48 +0900

    fix: 챌린지 신청 및 취소 상태 코드 변경

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/controller/ChallengeController.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/controller/ChallengeController.java
@@ -10,11 +10,13 @@ import me.bombom.api.v1.challenge.service.ChallengeService;
 import me.bombom.api.v1.common.resolver.LoginMember;
 import me.bombom.api.v1.member.domain.Member;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
@@ -48,6 +50,7 @@ public class ChallengeController implements ChallengeControllerApi {
 
     @Override
     @PostMapping("/{id}/application")
+    @ResponseStatus(HttpStatus.CREATED)
     public void applyChallenge(
             @PathVariable @Positive(message = "id는 1 이상의 값이어야 합니다.") Long id,
             @LoginMember Member member
@@ -57,6 +60,7 @@ public class ChallengeController implements ChallengeControllerApi {
 
     @Override
     @DeleteMapping("/{id}/application")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void cancelChallenge(
             @PathVariable @Positive(message = "id는 1 이상의 값이어야 합니다.") Long id,
             @LoginMember Member member

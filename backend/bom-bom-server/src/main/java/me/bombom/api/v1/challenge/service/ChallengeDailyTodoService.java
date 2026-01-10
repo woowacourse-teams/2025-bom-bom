4f190450e5bb311d972c26f387c1b0c4d074c1ff
ryan-dia <cjfdnjs158@naver.com>
2026-01-10 14:49:45 +0900

    fix: 챌린지에서 주말에는 읽기 체크 되지 않도록 수정

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/service/ChallengeDailyTodoService.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/service/ChallengeDailyTodoService.java
@@ -1,12 +1,17 @@
 package me.bombom.api.v1.challenge.service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.bombom.api.v1.challenge.domain.ChallengeTodoType;
 import me.bombom.api.v1.challenge.repository.ChallengeDailyTodoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -16,7 +21,21 @@ public class ChallengeDailyTodoService {
 
     @Transactional
     public void updateChallengeDailyTodo(Long memberId, Long articleId, LocalDate today) {
+        if (isWeekend(today)) {
+            log.info("오늘은 {}입니다. 주말에는 챌린지를 진행하지 않습니다.", today.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.KOREAN));
+            return;
+        }
+
         challengeDailyTodoRepository.insertTodayReadTodoIfMissing(
-                memberId, articleId, today, ChallengeTodoType.READ.name());
+                memberId,
+                articleId,
+                today,
+                ChallengeTodoType.READ.name()
+        );
+    }
+
+    private boolean isWeekend(LocalDate today) {
+        DayOfWeek dayOfWeek = today.getDayOfWeek();
+        return dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY;
     }
 }

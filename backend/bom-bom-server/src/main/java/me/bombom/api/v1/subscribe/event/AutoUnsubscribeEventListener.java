4406ccec9921e9a895e16b5276689c9abd9dacbd
ChoiDongjun <adong0808@naver.com>
2026-01-14 12:28:04 +0900

    fix: 이벤트 리스너의 Transactional 애노테이션 제거

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/event/AutoUnsubscribeEventListener.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/event/AutoUnsubscribeEventListener.java
@@ -8,7 +8,6 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
@@ -26,19 +25,18 @@ public class AutoUnsubscribeEventListener {
         Long subscribeId = event.subscribeId();
         String unsubscribeUrl = event.unsubscribeUrl();
 
-        log.info("구독 자동 취소 시작 subscribeId: {}", subscribeId);
+        log.info("구독 자동 취소 처리 시작 subscribeId: {}", subscribeId);
         boolean isSuccess = unsubscribeAgent.unsubscribe(unsubscribeUrl);
 
         eventPublisher.publishEvent(AutoUnsubscribeCompletedEvent.of(subscribeId, isSuccess));
-        log.info("구독 자동 취소 처리 완료 subscribeId: {}", subscribeId);
+        log.info("구독 자동 취소 처리 종료 subscribeId: {}", subscribeId);
     }
 
-    @Transactional
     @EventListener
     public void handleUnsubscribeCompleted(AutoUnsubscribeCompletedEvent event) {
         Long subscribeId = event.subscribeId();
-        log.info("구독 자동 취소 완료 처리 시작 subscribeId: {}", subscribeId);
+        log.info("구독 자동 취소 결과 처리 시작 subscribeId: {}", subscribeId);
         subscribeService.handleUnsubscribeResult(subscribeId, event.isSuccess());
-        log.info("구독 자동 취소 완료 처리 종료 subscribeId: {}", subscribeId);
+        log.info("구독 자동 취소 결과 처리 종료 subscribeId: {}", subscribeId);
     }
 }

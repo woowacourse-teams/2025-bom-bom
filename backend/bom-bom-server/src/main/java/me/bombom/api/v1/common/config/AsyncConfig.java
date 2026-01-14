37f76c95efea5e2faf8f29c7af9efb61f7bde7c4
ChoiDongjun <adong0808@naver.com>
2026-01-14 15:43:14 +0900

    feat: 비동기 스레드풀 설정 추가

---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package me.bombom.api.v1.common.config;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("async-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        executor.initialize();
+        return executor;
+    }
+}

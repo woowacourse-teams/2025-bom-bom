8d3844e2f197be0115ce634462b6bf811066dcc3
rladmstn <oz33177351@gmail.com>
2026-01-17 21:41:13 +0900

    feat: 코멘트 좋아요 테이블에 unique 제약 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/domain/ChallengeCommentLike.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/domain/ChallengeCommentLike.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,6 +17,12 @@ import me.bombom.api.v1.common.BaseEntity;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        uniqueConstraints = @UniqueConstraint(
+                name = "uk_challenge_comment_like_participant_comment",
+                columnNames = {"participant_id", "comment_id"}
+        )
+)
 public class ChallengeCommentLike extends BaseEntity {
 
     @Id
---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/dto/response/ChallengeCommentLikeResponse.java
@@ -0,0 +1,10 @@
+package me.bombom.api.v1.challenge.dto.response;
+
+import me.bombom.api.v1.challenge.domain.ChallengeComment;
+
+public record ChallengeCommentLikeResponse(int likeCount) {
+
+    public static ChallengeCommentLikeResponse of(ChallengeComment comment) {
+        return new ChallengeCommentLikeResponse(comment.getLikeCount());
+    }
+}

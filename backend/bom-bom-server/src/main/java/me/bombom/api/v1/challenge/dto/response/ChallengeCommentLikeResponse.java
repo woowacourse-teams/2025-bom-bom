fc97c4a3af3a99a43a3dd5a9ecfb71d52c18e892
rladmstn <oz33177351@gmail.com>
2026-01-18 22:59:29 +0900

    style: dto 메서드 네이밍 규칙에 맞게 수정

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/dto/response/ChallengeCommentLikeResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/dto/response/ChallengeCommentLikeResponse.java
@@ -4,7 +4,7 @@ import me.bombom.api.v1.challenge.domain.ChallengeComment;
 
 public record ChallengeCommentLikeResponse(int likeCount) {
 
-    public static ChallengeCommentLikeResponse of(ChallengeComment comment) {
+    public static ChallengeCommentLikeResponse from(ChallengeComment comment) {
         return new ChallengeCommentLikeResponse(comment.getLikeCount());
     }
 }

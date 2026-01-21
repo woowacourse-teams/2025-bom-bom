0db2a575533fff1a573e14858312fa495aa6e36f
rladmstn <oz33177351@gmail.com>
2026-01-21 19:11:25 +0900

    style: dto 개행 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/dto/response/ChallengeCommentLikeResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/dto/response/ChallengeCommentLikeResponse.java
@@ -2,7 +2,10 @@ package me.bombom.api.v1.challenge.dto.response;
 
 import me.bombom.api.v1.challenge.domain.ChallengeComment;
 
-public record ChallengeCommentLikeResponse(int likeCount) {
+public record ChallengeCommentLikeResponse(
+
+        int likeCount
+) {
 
     public static ChallengeCommentLikeResponse from(ChallengeComment comment) {
         return new ChallengeCommentLikeResponse(comment.getLikeCount());

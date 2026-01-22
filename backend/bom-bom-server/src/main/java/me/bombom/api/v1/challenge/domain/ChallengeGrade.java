d4cf5688ccd1f5bd347f335410b106ff86b31225
geonwoo <kysub99@naver.com>
2026-01-23 03:19:42 +0900

    fix: 챌린지 메달 기준 변경

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/domain/ChallengeGrade.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/domain/ChallengeGrade.java
@@ -2,20 +2,21 @@ package me.bombom.api.v1.challenge.domain;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import me.bombom.api.v1.badge.domain.BadgeGrade;
 
 @Getter
 @RequiredArgsConstructor
 public enum ChallengeGrade {
 
-    GOLD("금메달", 100),
-    SILVER("은메달", 80),
-    BRONZE("동메달", 50),
-    COMPLETE("완료", 0),
-    FAIL("탈락", 0),
+    GOLD("금메달", 100, BadgeGrade.GOLD),
+    SILVER("은메달", 90, BadgeGrade.SILVER),
+    BRONZE("동메달", 80, BadgeGrade.BRONZE),
+    FAIL("탈락", 0, null),
     ;
 
     private final String description;
     private final int minProgress;
+    private final BadgeGrade badgeGrade;
 
     public static ChallengeGrade calculate(int progress, boolean isSurvived) {
         if (!isSurvived) {
@@ -24,12 +25,16 @@ public enum ChallengeGrade {
         if (progress >= GOLD.minProgress) {
             return GOLD;
         }
-        if (progress >= SILVER.minProgress){
+        if (progress >= SILVER.minProgress) {
             return SILVER;
         }
-        if (progress >= BRONZE.minProgress){
+        if (progress >= BRONZE.minProgress) {
             return BRONZE;
         }
-        return COMPLETE;
+        return FAIL;
+    }
+
+    public BadgeGrade toBadge() {
+        return this.badgeGrade;
     }
 }

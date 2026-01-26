7687d12a05f47e128d4b58951b40bd2c5315290a
ryan-dia <cjfdnjs158@naver.com>
2026-01-27 02:05:08 +0900

    feat: 스케줄링 config 추가

---
+++ backend/mail-server/src/main/java/news/bombomemail/common/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package news.bombomemail.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}

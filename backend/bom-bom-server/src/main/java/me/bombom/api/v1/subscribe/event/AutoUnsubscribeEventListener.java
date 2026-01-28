3a93e471518bb1c6a7e84c0d4f4b35b04d0439f4
ChoiDongjun <adong0808@naver.com>
2026-01-28 21:11:07 +0900

    feat: 재시도 불가능 예외에 대해 디스코드 알림 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/event/AutoUnsubscribeEventListener.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/event/AutoUnsubscribeEventListener.java
@@ -2,7 +2,16 @@ package me.bombom.api.v1.subscribe.event;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+<<<<<<< HEAD
 import me.bombom.api.v1.subscribe.service.SubscribeService;
+=======
+import me.bombom.api.v1.common.DiscordWebhookNotifier;
+import me.bombom.api.v1.common.exception.RetryableException;
+import me.bombom.api.v1.subscribe.exception.AutoUnsubscribeFailedException;
+import me.bombom.api.v1.subscribe.service.SubscribeService;
+import me.bombom.api.v1.subscribe.service.UnsubscribeAgent;
+import org.springframework.context.ApplicationEventPublisher;
+>>>>>>> 0080a940 (feat: 재시도 불가능 예외에 대해 디스코드 알림 추가)
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -13,7 +22,14 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 public class AutoUnsubscribeEventListener {
 
+<<<<<<< HEAD
     private final SubscribeService subscribeService;
+=======
+    private final UnsubscribeAgent unsubscribeAgent;
+    private final ApplicationEventPublisher eventPublisher;
+    private final SubscribeService subscribeService;
+    private final DiscordWebhookNotifier discordWebhookNotifier;
+>>>>>>> 0080a940 (feat: 재시도 불가능 예외에 대해 디스코드 알림 추가)
 
     @Async
     @TransactionalEventListener
@@ -22,9 +38,29 @@ public class AutoUnsubscribeEventListener {
         String unsubscribeUrl = event.unsubscribeUrl();
         Long newsletterId = event.newsletterId();
 
+<<<<<<< HEAD
         log.info("구독 자동 취소 처리 시작 subscribeId: {}", subscribeId);
         subscribeService.processUnsubscribe(subscribeId, newsletterId, unsubscribeUrl);
         log.info("구독 자동 취소 처리 종료 subscribeId: {}", subscribeId);
+=======
+        try {
+            log.info("구독 자동 취소 처리 시작 subscribeId: {}", subscribeId);
+            boolean isSuccess = unsubscribeAgent.unsubscribe(unsubscribeUrl, newsletterId);
+            eventPublisher.publishEvent(AutoUnsubscribeCompletedEvent.of(subscribeId, isSuccess));
+            log.info("구독 자동 취소 처리 종료 subscribeId: {}", subscribeId);
+        } catch (RetryableException e) {
+            //재시도
+        } catch (AutoUnsubscribeFailedException e) {
+            discordWebhookNotifier.sendUnsubscribeErrorNotification(
+                    e.getMessage(),
+                    e.getNewsletterId(),
+                    e.getUrl(),
+                    subscribeId
+            );
+        } catch (Exception e) {
+            log.error("예상치 못한 예외가 발생했습니다.", e);
+        }
+>>>>>>> 0080a940 (feat: 재시도 불가능 예외에 대해 디스코드 알림 추가)
     }
 
     @EventListener

5f36b5dd57e6328e18b459d37af134b1e0ac82d9
ChoiDongjun <adong0808@naver.com>
2026-02-02 12:24:20 +0900

    feat: 구독 취소 자동화 AWS Lambda 호출 방식으로 수정

---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/dto/PlaywrightResponse.java
@@ -0,0 +1,8 @@
+package me.bombom.api.v1.subscribe.dto;
+
+public record PlaywrightResponse(
+                Integer statusCode,
+                boolean success,
+                String message,
+                String error) {
+}

494c400b096211f981a153b62bd34d42a5496ec9
ChoiDongjun <adong0808@naver.com>
2026-02-04 12:11:20 +0900

    [BOM-918] fix: 회원 정합성 문제 해결 (#644)
    * fix: LoginMember 애노테이션을 사용할 때마다 DB를 조회하여 최신 데이터를 맞추도록 수정
    
    * test: 실패하는 테스트 수정
    
    * test: LoginMemberArgumentResolver 테스트 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/resolver/LoginMemberArgumentResolver.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/resolver/LoginMemberArgumentResolver.java
@@ -1,21 +1,28 @@
 package me.bombom.api.v1.common.resolver;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.bombom.api.v1.auth.dto.CustomOAuth2User;
 import me.bombom.api.v1.common.exception.ErrorDetail;
 import me.bombom.api.v1.common.exception.UnauthorizedException;
 import me.bombom.api.v1.member.domain.Member;
+import me.bombom.api.v1.member.repository.MemberRepository;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 @Slf4j
+@Component
+@RequiredArgsConstructor
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberRepository memberRepository;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -46,7 +53,9 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
             if (member == null) {
                 throw new UnauthorizedException(ErrorDetail.UNAUTHORIZED);
             }
-            return member;
+
+            return memberRepository.findById(member.getId())
+                    .orElseThrow(() -> new UnauthorizedException(ErrorDetail.UNAUTHORIZED));
         }
         throw new UnauthorizedException(ErrorDetail.INVALID_TOKEN);
     }

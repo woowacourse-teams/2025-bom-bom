5ef5c6c7c19b812ab18a5d30497f176794347c09
ChoiDongjun <adong0808@naver.com>
2026-02-05 23:56:06 +0900

    chore: 로깅에 구독 취소된 방식 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/dto/PlaywrightResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/subscribe/dto/PlaywrightResponse.java
@@ -1,8 +1,9 @@
 package me.bombom.api.v1.subscribe.dto;
 
 public record PlaywrightResponse(
-                Integer statusCode,
-                boolean success,
-                String message,
-                String error) {
+        Integer statusCode,
+        boolean success,
+        String message,
+        String error,
+        String method) {
 }

24efeb5aad2013fb33a1a8348ede08a691ade84d
rladmstn <oz33177351@gmail.com>
2026-02-12 11:29:01 +0900

    feat: 메세지 빌더 추상화 및 구현체 추가

---
+++ backend/fcm-server/src/main/java/news/bombom/notification/service/message/ArticleArrivalMessageBuilder.java
@@ -0,0 +1,34 @@
+package news.bombom.notification.service.message;
+
+import java.util.Map;
+import news.bombom.notification.domain.ArticleArrivalNotification;
+import news.bombom.notification.domain.MemberFcmToken;
+import news.bombom.notification.domain.Notification;
+import news.bombom.notification.domain.NotificationType;
+import news.bombom.notification.dto.NotificationMessage;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArticleArrivalMessageBuilder implements NotificationMessageBuilder {
+
+    @Override
+    public boolean supports(Notification notification) {
+        return notification instanceof ArticleArrivalNotification;
+    }
+
+    @Override
+    public NotificationMessage build(Notification notification, MemberFcmToken token) {
+        ArticleArrivalNotification article = (ArticleArrivalNotification) notification;
+
+        return NotificationMessage.builder()
+                .recipient(token.getFcmToken())
+                .title(article.getNewsletterName())
+                .content(article.getArticleTitle())
+                .type(NotificationType.FCM)
+                .data(Map.of(
+                        "articleId", String.valueOf(article.getArticleId()),
+                        "notificationType", "arrivedArticle"
+                ))
+                .build();
+    }
+}
---
+++ backend/fcm-server/src/main/java/news/bombom/notification/service/message/ChallengeTodoReminderMessageBuilder.java
@@ -0,0 +1,36 @@
+package news.bombom.notification.service.message;
+
+import java.util.Map;
+import news.bombom.notification.domain.ChallengeTodoReminderNotification;
+import news.bombom.notification.domain.MemberFcmToken;
+import news.bombom.notification.domain.Notification;
+import news.bombom.notification.domain.NotificationType;
+import news.bombom.notification.dto.NotificationMessage;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChallengeTodoReminderMessageBuilder implements NotificationMessageBuilder {
+
+    @Override
+    public boolean supports(Notification notification) {
+        return notification instanceof ChallengeTodoReminderNotification;
+    }
+
+    @Override
+    public NotificationMessage build(Notification notification, MemberFcmToken token) {
+        ChallengeTodoReminderNotification remind = (ChallengeTodoReminderNotification) notification;
+
+        String title = remind.getChallengeName() + " 아직 할 일이 남았어요!";
+        String content = "오늘도 기록을 채워볼까요? 지금 참여해서 꾸준하게 이어가요!";
+
+        return NotificationMessage.builder()
+                .recipient(token.getFcmToken())
+                .title(title)
+                .content(content)
+                .type(NotificationType.FCM)
+                .data(Map.of(
+                        "notificationType", "challengeTodoReminder"
+                ))
+                .build();
+    }
+}

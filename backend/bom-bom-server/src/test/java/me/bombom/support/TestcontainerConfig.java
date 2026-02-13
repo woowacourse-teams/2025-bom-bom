16d87b881e5016cf91cc18d9407a644bb4e96732
ChoiDongjun <adong0808@naver.com>
2026-02-13 22:01:25 +0900

    chore: 테스트 컨테이너 설정 복구

--- backend/bom-bom-server/src/test/java/me/bombom/support/TestcontainerConfig.java
+++ backend/bom-bom-server/src/test/java/me/bombom/support/TestcontainerConfig.java
@@ -1,8 +1,6 @@
 package me.bombom.support;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -10,10 +8,14 @@ import org.testcontainers.utility.DockerImageName;
 public class TestcontainerConfig {
 
     private static final String MYSQL_IMAGE = "mysql:8.4.5";
+    private static final MySQLContainer<?> MYSQL_CONTAINER;
 
-    @Bean
-    @ServiceConnection
-    public MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>(DockerImageName.parse(MYSQL_IMAGE));
+    static {
+        MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse(MYSQL_IMAGE));
+        MYSQL_CONTAINER.start();
+
+        System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl());
+        System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
+        System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
     }
 }

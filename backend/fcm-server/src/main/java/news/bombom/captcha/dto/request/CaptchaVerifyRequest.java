fd8793dd87defb60d24e96a5b5583342938af7a6
ChoiDongjun <adong0808@naver.com>
2026-02-15 03:31:48 +0900

    chore: 불필요한 import 제거

--- backend/fcm-server/src/main/java/news/bombom/captcha/dto/request/CaptchaVerifyRequest.java
+++ backend/fcm-server/src/main/java/news/bombom/captcha/dto/request/CaptchaVerifyRequest.java
@@ -1,6 +1,5 @@
 package news.bombom.captcha.dto.request;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record CaptchaVerifyRequest(

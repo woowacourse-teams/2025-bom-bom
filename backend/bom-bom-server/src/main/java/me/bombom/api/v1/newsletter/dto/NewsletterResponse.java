f063fccf88da4d3381471758c46c90c1f0b037b7
ChoiDongjun <adong0808@naver.com>
2026-02-27 02:35:14 +0900

    chore: Swagger 옵션 수정

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/newsletter/dto/NewsletterResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/newsletter/dto/NewsletterResponse.java
@@ -2,6 +2,7 @@ package me.bombom.api.v1.newsletter.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.validation.constraints.NotNull;
 import me.bombom.api.v1.newsletter.domain.NewsletterPublicationStatus;
 
@@ -27,11 +28,11 @@ public record NewsletterResponse(
         @NotNull
         NewsletterPublicationStatus status,
 
-        @Schema(required = true)
+        @Schema(requiredMode = RequiredMode.REQUIRED)
         boolean isSubscribed
 ) {
 
-        @QueryProjection
-        public NewsletterResponse {
-        }
+    @QueryProjection
+    public NewsletterResponse {
+    }
 }

216d9e12d938ea66ac95c4408ca0d03b6abf2f6f
geonwoo <kysub99@naver.com>
2026-03-10 01:56:16 +0900

    refactor: StringUtils 사용

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/domain/ChallengeFilterConverter.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/domain/ChallengeFilterConverter.java
@@ -4,13 +4,14 @@ import me.bombom.api.v1.common.exception.CIllegalArgumentException;
 import me.bombom.api.v1.common.exception.ErrorDetail;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
 public class ChallengeFilterConverter implements Converter<String, ChallengeFilter> {
 
     @Override
     public ChallengeFilter convert(String source) {
-        if (source == null || source.isBlank()) {
+        if (!StringUtils.hasText(source)) {
             return ChallengeFilter.DEFAULT;
         }
         if ("summary".equalsIgnoreCase(source)) {

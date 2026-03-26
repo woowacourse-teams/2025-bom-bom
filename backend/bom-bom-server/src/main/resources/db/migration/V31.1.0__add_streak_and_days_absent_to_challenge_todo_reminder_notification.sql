cdd365e772438a4c8430cbd4087eb0edc84da351
ChoiDongjun <adong0808@naver.com>
2026-03-26 11:20:02 +0900

    chore: 추가한 컬럼들에 대해 flyway 스크립트 작성

--- backend/bom-bom-server/src/main/resources/db/migration/V31.1.0__add_streak_and_days_absent_to_challenge_todo_reminder_notification.sql
+++ backend/bom-bom-server/src/main/resources/db/migration/V31.1.0__add_streak_and_days_absent_to_challenge_todo_reminder_notification.sql
@@ -1,3 +1,4 @@
 ALTER TABLE challenge_todo_reminder_notification
     ADD COLUMN streak      INT        NOT NULL DEFAULT 0,
-    ADD COLUMN is_last_day TINYINT(1) NOT NULL DEFAULT 0;
+    ADD COLUMN is_last_day TINYINT(1) NOT NULL DEFAULT 0,
+    ADD COLUMN days_absent INT NULL;

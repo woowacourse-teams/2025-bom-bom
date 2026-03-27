1b1fc26e4a513f8aa65f2618e6870263ad8f94fa
geonwoo <kysub99@naver.com>
2026-03-28 03:23:50 +0900

    refactor: readOnly 트랜잭션 적용

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/reading/service/ContinueReadingSnapshotMetaService.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/reading/service/ContinueReadingSnapshotMetaService.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ContinueReadingSnapshotMetaService {
 
     private static final Long SINGLE_ID = 1L;

a7d88d3e2288e5a004d307c1575390bd2af49ad3
ryan-dia <cjfdnjs158@naver.com>
2026-04-25 20:25:40 +0900

    feat: 매일메일 발행 스케줄러 추가

---
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/nativenewsletter/maeilmail/scheduler/MaeilMailScheduler.java
@@ -0,0 +1,23 @@
+package me.bombom.api.v1.nativenewsletter.maeilmail.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import me.bombom.api.v1.nativenewsletter.maeilmail.service.MaeilMailIssueService;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MaeilMailScheduler {
+
+    private static final String TIME_ZONE = "Asia/Seoul";
+    private static final String ISSUE_CRON = "0 0 7 * * MON-FRI";
+
+    private final MaeilMailIssueService maeilMailIssueService;
+
+    @Scheduled(cron = ISSUE_CRON, zone = TIME_ZONE)
+    @SchedulerLock(name = "maeil_mail_issue", lockAtLeastFor = "PT5S", lockAtMostFor = "PT10M")
+    public void issueMaeilMail() {
+        maeilMailIssueService.issue();
+    }
+}

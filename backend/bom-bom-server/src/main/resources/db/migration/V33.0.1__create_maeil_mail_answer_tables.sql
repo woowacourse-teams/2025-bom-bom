032ea3e90f60fa19414b4930d18a221efe3bd07a
minSsan <msp1125@hanyang.ac.kr>
2026-04-29 01:01:36 +0900

    refactor: 매일메일 모범답안 엔티티 생성일, 수정일 필드 추가

--- backend/bom-bom-server/src/main/resources/db/migration/V33.0.1__create_maeil_mail_answer_tables.sql
+++ backend/bom-bom-server/src/main/resources/db/migration/V33.0.1__create_maeil_mail_answer_tables.sql
@@ -11,6 +11,8 @@ CREATE TABLE maeil_mail_user_answer
 CREATE TABLE maeil_mail_content_answer
 (
     id         BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    created_at datetime(6)           NULL,
+    updated_at datetime(6)           NULL,
     content_id BIGINT                NOT NULL,
     answer     MEDIUMTEXT            NOT NULL,
 );

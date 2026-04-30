b8b67a01d37065fd836e977c5b2e624540a267d3
jerry.je2@kakaoenterprise.com <jerry.je2@kakaoenterprise.com>
2026-04-30 15:59:08 +0900

    style: Response DTO 검증 어노테이션 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/nativenewsletter/maeilmail/dto/MaeilMailInformationResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/nativenewsletter/maeilmail/dto/MaeilMailInformationResponse.java
@@ -1,9 +1,11 @@
 package me.bombom.api.v1.nativenewsletter.maeilmail.dto;
 
+import jakarta.validation.constraints.NotNull;
 import me.bombom.api.v1.nativenewsletter.maeilmail.domain.MaeilMailIssueHistory;
 
 public record MaeilMailInformationResponse(
 
+        @NotNull
         Long contentId
 ) {
 

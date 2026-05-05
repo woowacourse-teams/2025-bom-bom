5df53878bd37a1e2c2b2f7e5ea42896b6f37d8a2
ChoiDongjun <adong0808@naver.com>
2026-05-05 23:03:20 +0900

    refactor: 구독 취소 url 파싱 실패 알림 로직을 두개의 클래스로 분리

---
+++ backend/mail-server/src/main/java/news/bombomemail/subscribe/alert/UnsubscribeUrlMissingEventListener.java
@@ -0,0 +1,19 @@
+package news.bombomemail.subscribe.alert;
+
+import lombok.RequiredArgsConstructor;
+import news.bombomemail.subscribe.alert.service.UnsubscribeUrlAlertService;
+import news.bombomemail.subscribe.event.UnsubscribeUrlMissingEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class UnsubscribeUrlMissingEventListener {
+
+    private final UnsubscribeUrlAlertService unsubscribeUrlAlertService;
+
+    @TransactionalEventListener
+    public void on(UnsubscribeUrlMissingEvent event) {
+        unsubscribeUrlAlertService.record(event);
+    }
+}

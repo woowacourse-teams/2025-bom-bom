76ed6bc77d37c88c350752887792f21f1b2b8892
ryan-dia <cjfdnjs158@naver.com>
2026-05-06 16:15:48 +0900

    feat: 오늘 전체 읽기 횟수 필드 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/reading/domain/TodayReading.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/reading/domain/TodayReading.java
@@ -19,9 +19,12 @@ public class TodayReading extends BaseEntity {
 
     private static final int INITIAL_TOTAL_COUNT = 3;
     private static final int INITIAL_CURRENT_COUNT = 0;
+    private static final int INITIAL_READ_COUNT = 0;
     private static final int RESET_TOTAL_COUNT = 0;
     private static final int RESET_CURRENT_COUNT = 0;
+    private static final int RESET_READ_COUNT = 0;
     private static final int INCREASE_CURRENT_COUNT = 1;
+    private static final int INCREASE_READ_COUNT = 1;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,23 +33,31 @@ public class TodayReading extends BaseEntity {
     @Column(nullable = false, unique = true)
     private Long memberId;
 
+    // 오늘 도착한 뉴스레터 개수
     @Column(nullable = false, columnDefinition = "TINYINT")
     private int totalCount;
 
+    // 오늘 도착한 뉴스레터 중 읽은 개수
     @Column(nullable = false, columnDefinition = "TINYINT")
     private int currentCount;
+
+    // 안 읽은 뉴스레터 중 읽은 개수
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private int readCount;
 
     @Builder
     public TodayReading(
             Long id,
             @NonNull Long memberId,
             int totalCount,
-            int currentCount
+            int currentCount,
+            int readCount
     ) {
         this.id = id;
         this.memberId = memberId;
         this.totalCount = totalCount;
         this.currentCount = currentCount;
+        this.readCount = readCount;
     }
 
     public static TodayReading create(Long memberId) {
@@ -54,15 +65,21 @@ public class TodayReading extends BaseEntity {
                 .memberId(memberId)
                 .totalCount(INITIAL_TOTAL_COUNT)
                 .currentCount(INITIAL_CURRENT_COUNT)
+                .readCount(INITIAL_READ_COUNT)
                 .build();
     }
 
     public void resetCount() {
         this.totalCount = RESET_TOTAL_COUNT;
         this.currentCount = RESET_CURRENT_COUNT;
+        this.readCount = RESET_READ_COUNT;
     }
 
     public void increaseCurrentCount() {
         this.currentCount += INCREASE_CURRENT_COUNT;
+    }
+
+    public void increaseReadCount() {
+        this.readCount += INCREASE_READ_COUNT;
     }
 }

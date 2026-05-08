c5f6d287c07876655cab0be13faaf5db7ef5f131
ChoiDongjun <adong0808@naver.com>
2026-05-08 21:28:25 +0900

    chore: rate limit 설정값에 기본값 적용

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/ReadRateLimitProperties.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/ReadRateLimitProperties.java
@@ -1,20 +1,15 @@
 package me.bombom.api.v1.common;
 
-import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
-@Validated
 @ConfigurationProperties("bom-bom.rate-limit.read-count")
 public class ReadRateLimitProperties {
 
-    @Min(1)
-    private int bucketCapacity; //TODO: 검증 실패하면 그냥 기본값 사용하도록?
+    private int bucketCapacity = 3;
 
-    @Min(1)
-    private int refillSeconds;
+    private int refillSeconds = 50;
 }

46747422cdfed09f71d3803fc78fecea62e3df9c
ChoiDongjun <adong0808@naver.com>
2026-05-11 17:36:18 +0900

    refactor: MaeilMailSubscriptionTrack deleteByMemberId 벌크 삭제로 변경

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/nativenewsletter/maeilmail/repository/MaeilMailSubscriptionTrackRepository.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/nativenewsletter/maeilmail/repository/MaeilMailSubscriptionTrackRepository.java
@@ -3,10 +3,17 @@ package me.bombom.api.v1.nativenewsletter.maeilmail.repository;
 import java.util.List;
 import me.bombom.api.v1.nativenewsletter.maeilmail.domain.MaeilMailSubscriptionTrack;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MaeilMailSubscriptionTrackRepository extends JpaRepository<MaeilMailSubscriptionTrack, Long> {
 
     List<MaeilMailSubscriptionTrack> findByMemberId(Long memberId);
 
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("""
+            DELETE FROM MaeilMailSubscriptionTrack mt
+            WHERE mt.memberId = :memberId
+    """)
     void deleteByMemberId(Long memberId);
 }

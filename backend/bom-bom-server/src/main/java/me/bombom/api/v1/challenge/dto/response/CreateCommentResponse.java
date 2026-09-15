2a78ee5b26e88db12b93e0ce26f7114506d916e6
geonwoo <kysub99@naver.com>
2026-09-16 01:34:29 +0900

    feat: 챌린지 코멘트 작성 응답에 수료 기준 도달 여부 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/dto/response/CreateCommentResponse.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/challenge/dto/response/CreateCommentResponse.java
@@ -2,10 +2,11 @@ package me.bombom.api.v1.challenge.dto.response;
 
 public record CreateCommentResponse(
 
-        boolean isFirstCompletion
+        boolean isFirstCompletion,
+        boolean isChallengeCompleted
 ) {
 
-    public static CreateCommentResponse from(boolean isFirstCompletion) {
-        return new CreateCommentResponse(isFirstCompletion);
+    public static CreateCommentResponse of(boolean isFirstCompletion, boolean isChallengeCompleted) {
+        return new CreateCommentResponse(isFirstCompletion, isChallengeCompleted);
     }
 }

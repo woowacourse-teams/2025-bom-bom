bdb7e775a5829a40086898633ae6953a86c4ebea
시원 <cnc4750@inha.edu>
2026-09-17 15:02:45 +0900

    feat: s3 업로드 과정 상세 에러 처리 추가

--- backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/S3ImageUploader.java
+++ backend/bom-bom-server/src/main/java/me/bombom/api/v1/common/S3ImageUploader.java
@@ -1,5 +1,6 @@
 package me.bombom.api.v1.common;
 
+import io.awspring.cloud.s3.S3Exception;
 import io.awspring.cloud.s3.S3Resource;
 import io.awspring.cloud.s3.S3Template;
 import java.io.IOException;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import me.bombom.api.v1.common.exception.CServerErrorException;
 import me.bombom.api.v1.common.exception.ErrorDetail;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.core.exception.SdkException;
 
 @Component
 @RequiredArgsConstructor
@@ -16,12 +18,24 @@ public class S3ImageUploader {
     private final S3Template s3Template;
 
     public String upload(String bucketName, String key, InputStream inputStream) {
-        S3Resource uploaded = s3Template.upload(bucketName, key, inputStream);
         try {
+            S3Resource uploaded = s3Template.upload(bucketName, key, inputStream);
             return uploaded.getURL().toString();
+        } catch (S3Exception e) {
+            throw new CServerErrorException(ErrorDetail.INTERNAL_SERVER_ERROR)
+                    .addContext("operation", "s3Upload")
+                    .addContext("bucketName", bucketName)
+                    .addContext("key", key)
+                    .addContext("message", e.getMessage());
+        } catch (SdkException e) {
+            throw new CServerErrorException(ErrorDetail.INTERNAL_SERVER_ERROR)
+                    .addContext("operation", "s3GetImageUrl")
+                    .addContext("bucketName", bucketName)
+                    .addContext("key", key)
+                    .addContext("message", e.getMessage());
         } catch (IOException e) {
             throw new CServerErrorException(ErrorDetail.INTERNAL_SERVER_ERROR)
-                    .addContext("operation", "imageUpload")
+                    .addContext("operation", "s3GetImageUrl")
                     .addContext("bucketName", bucketName)
                     .addContext("key", key);
         }
